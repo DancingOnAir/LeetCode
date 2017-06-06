0394b8125ed6646e953ac5d0bf099ee146d04c97
DancingOnAir <A0135925@u.nus.edu>
2017-06-06 17:08:32 +0800

    updated solution for question 605

--- Array/Array/605_CanPlaceFlowers.cpp
+++ Array/Array/605_CanPlaceFlowers.cpp
@@ -4,32 +4,55 @@
 
 using namespace std;
 
-bool canPlaceFlowers(vector<int>& flowerbed, int n) 
-{
-	vector<int> flagList = {0};
-	int i = 0;
-	for (auto val : flowerbed)
-	{
-		if (val == 1)
-			flagList.push_back(i);
-
-		++i;
-	}
-
-	int sum = 0;
-	if (flagList.size() > 1)
-	{
-		for (int j = 1; j < flagList.size(); ++j)
-		{
-			if (flagList.at(j) - flagList.at(j - 1) != 0)
-				sum += (flagList.at(j) - flagList.at(j - 1) - 1) / 2;
-		}
-	}
-	
-	if (sum == n)
-		return true;
-
-	return false;
+bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+    vector<int> flagList;
+    int i = 0;
+    for (auto val : flowerbed)
+    {
+    	if (val == 1)
+    		flagList.push_back(i);
+    
+    	++i;
+    }
+    
+    int sum = 0;
+    	
+    if(flagList.size() > 0)
+    {
+    	for(int j = 0; j < flagList.size(); ++j)
+    	{
+    	    if(0 != flagList.at(j) && 0 == j)
+    	    {
+    	        sum += flagList.at(j) / 2;
+    	    }
+    	        
+    	    if(flowerbed.size() - 1 != flagList.at(j) && flagList.size() - 1 == j)
+    	    {
+    	        sum += (flowerbed.size() - 1 - flagList.at(j)) / 2;
+    	    }
+    	        
+    	    if(j > 0)
+    	    {
+    	        sum += (flagList.at(j) - flagList.at(j - 1) - 2) / 2;
+    	    }
+    	}
+    	    
+    }else
+    {
+    	if(0 == flowerbed.size()%2)
+    	{
+    	    sum = flowerbed.size()/2;
+    	}else
+    	{
+    	    sum = flowerbed.size()/2 + 1;
+    	}
+    }
+    	
+    
+    if (sum == n)
+    	return true;
+    
+    return false;
 }
 
 int main()
