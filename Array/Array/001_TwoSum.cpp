0434787b2893f3679fd9bede150471bdc4bb4dac
DancingOnAir <A0135925@u.nus.edu>
2017-06-15 22:05:49 +0800

    added accepted solution for q004

--- Array/Array/001_TwoSum.cpp
+++ Array/Array/001_TwoSum.cpp
@@ -1,50 +1,50 @@
-#include <iostream>
-#include <stdlib.h>
-#include <vector>
-
-using namespace std;
-
-vector<int> twoSum(vector<int>& nums, int target) {
-
-	cout << target << endl;
-
-	vector<int> tempVec;
-	int number = 0;
-
-
-		
-	for (int i = 0; i < nums.size(); ++i)
-	{
-		number = target - nums.at(i);
-		for (int j = 0; j < nums.size(); ++j)
-		{
-			if (nums.at(j) == number && j != i)
-			{
-				tempVec.push_back(i);
-				tempVec.push_back(j);
-
-				return tempVec;
-			}
-		}
-	}
-
-
-	return tempVec;
-}
-
-int main(void)
-{
-	int pArray[4] = { 2, 7, 11, 15 };
-	vector<int> vec(pArray, pArray + 4);
-
-	//twoSum(vec, vec.at(0) + vec.at(1));
-
-	for (auto value : twoSum(vec, vec.at(0) + vec.at(1)))
-	{
-		cout << value << endl;
-	}
-
-
-	system("pause");
-	return 0;
-}
+//#include <iostream>
+//#include <stdlib.h>
+//#include <vector>
+//
+//using namespace std;
+//
+//vector<int> twoSum(vector<int>& nums, int target) {
+//
+//	cout << target << endl;
+//
+//	vector<int> tempVec;
+//	int number = 0;
+//
+//
+//		
+//	for (int i = 0; i < nums.size(); ++i)
+//	{
+//		number = target - nums.at(i);
+//		for (int j = 0; j < nums.size(); ++j)
+//		{
+//			if (nums.at(j) == number && j != i)
+//			{
+//				tempVec.push_back(i);
+//				tempVec.push_back(j);
+//
+//				return tempVec;
+//			}
+//		}
+//	}
+//
+//
+//	return tempVec;
+//}
+//
+//int main(void)
+//{
+//	int pArray[4] = { 2, 7, 11, 15 };
+//	vector<int> vec(pArray, pArray + 4);
+//
+//	//twoSum(vec, vec.at(0) + vec.at(1));
+//
+//	for (auto value : twoSum(vec, vec.at(0) + vec.at(1)))
+//	{
+//		cout << value << endl;
+//	}
+//
+//
+//	system("pause");
+//	return 0;
+//}
---
+++ Array/Array/004_MedianofTwoSortedArrays.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+	double median = 0;
+
+	nums1.insert(nums1.end(), nums2.begin(), nums2.end());
+	
+	int size = nums1.size();
+	if (size % 2 != 0)
+	{	
+
+		nth_element(nums1.begin(), nums1.begin() + size / 2, nums1.end());
+		median = nums1.at(size / 2);
+	}
+	else
+	{
+		nth_element(nums1.begin(), nums1.begin() + size / 2 - 1, nums1.end());
+		median = nums1.at(size / 2 - 1);
+		nth_element(nums1.begin(), nums1.begin() + size / 2, nums1.end());
+		median = (median + nums1.at(size / 2))/2;
+	}
+
+	return median;
+}
+
+int main(void)
+{
+	int a[] = { 1, 2 };
+	int b[] = { 3, 4};
+	vector<int> nums1(a, a + 2);
+	vector<int> nums2(b, b + 2);
+
+	cout << findMedianSortedArrays(nums1, nums2) << endl;
+
+	system("pause");
+
+	return 0;
+}
