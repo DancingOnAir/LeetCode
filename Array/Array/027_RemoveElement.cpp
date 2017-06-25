520f8dadcb6295e41289a034c44fb40352846c07
DancingOnAir <A0135925@u.nus.edu>
2017-06-25 23:14:30 +0800

    added accepted solution for q027

---
+++ Array/Array/027_RemoveElement.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
+
+using namespace std;
+
+int removeElement(vector<int>& nums, int val) 
+{
+	for (vector<int>::iterator iter = nums.begin(); iter != nums.end();)
+	{
+		if (*iter == val)
+			iter = nums.erase(iter);
+		else
+			++iter;
+	}
+
+	for (auto val : nums)
+	{
+		cout << val << endl;
+	}
+
+	return nums.size();
+}
+
+int removeElement2(vector<int>& nums, int val) {
+	int cnt = 0;
+	for (int i = 0; i < nums.size(); ++i) {
+		if (nums[i] == val)
+			cnt++;
+		else
+			nums[i - cnt] = nums[i];
+	}
+
+	for (auto val : nums)
+	{
+		cout << val << endl;
+	}
+	cout << "end of vector" << endl;
+	return nums.size() - cnt;
+}
+
+int main(void)
+{
+	int a[] = { 3, 2, 2, 3, 4, 3 };
+	int size = sizeof(a) / sizeof(int);
+	int target = 3;
+	vector<int> vec(a, a + size);
+
+	//cout << removeElement(vec, target) << endl;
+	cout << removeElement2(vec, target) << endl;
+	system("pause");
+	return 0;
+}
