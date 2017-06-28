6ee5e46e427cc40c81a84cafc153d0b0f78acefe
DancingOnAir <A0135925@u.nus.edu>
2017-06-28 21:51:38 +0800

    added accepted solution for q033

---
+++ Array/Array/033_SearchInRotatedSortedArray.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
+
+using namespace std;
+
+int search(vector<int>& nums, int target) 
+{
+	for (int i = 0; i < nums.size(); ++i)
+	{
+		if (nums[i] == target)
+			return i;
+	}
+
+	return -1;
+}
+
+int main(void)
+{
+	int a[] = { 0, 1, 2, 4, 5, 6, 7 };
+	int target = 4;
+	int size = sizeof(a) / sizeof(int);
+	vector<int> vec(a, a + size);
+
+	cout << search(vec, target) << endl;
+	system("pause");
+	return 0;
+}
