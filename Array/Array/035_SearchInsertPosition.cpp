1d850c187aa03ede516d1b69b5b54e5b359c3254
DancingOnAir <A0135925@u.nus.edu>
2017-07-02 16:27:44 +0800

    added accepted solution for q039

--- Array/Array/035_SearchInsertPosition.cpp
+++ Array/Array/035_SearchInsertPosition.cpp
@@ -1,36 +1,36 @@
-#include <iostream>
-#include <stdlib.h>
-#include <vector>
-
-using namespace std;
-int searchInsert(vector<int>& nums, int target) 
-{
-	int i = 0;
-	int j = nums.size() - 1;
-
-	while (i < j)
-	{
-		int mid = (i + j) / 2;
-		if (nums[mid] < target)
-			i = mid + 1;
-		else
-			j = mid;
-	}
-
-	if (nums[i] >= target)
-		return i;
-	else
-		return i + 1;
-}
-
-int main(void)
-{
-	int a[] = { 1, 3, 5, 6 };
-	int target = 0;
-	vector<int> vec(a, a + 4);
-
-	cout << searchInsert(vec, target) << endl;
-
-	system("pause");
-	return 0;
-}
+//#include <iostream>
+//#include <stdlib.h>
+//#include <vector>
+//
+//using namespace std;
+//int searchInsert(vector<int>& nums, int target) 
+//{
+//	int i = 0;
+//	int j = nums.size() - 1;
+//
+//	while (i < j)
+//	{
+//		int mid = (i + j) / 2;
+//		if (nums[mid] < target)
+//			i = mid + 1;
+//		else
+//			j = mid;
+//	}
+//
+//	if (nums[i] >= target)
+//		return i;
+//	else
+//		return i + 1;
+//}
+//
+//int main(void)
+//{
+//	int a[] = { 1, 3, 5, 6 };
+//	int target = 0;
+//	vector<int> vec(a, a + 4);
+//
+//	cout << searchInsert(vec, target) << endl;
+//
+//	system("pause");
+//	return 0;
+//}
