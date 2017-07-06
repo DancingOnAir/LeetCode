7eeb6bf28adc8b4ff663941d7f54ba25fe11360d
DancingOnAir <A0135925@u.nus.edu>
2017-07-06 22:13:34 +0800

    added accepted solution for q005

--- Array/String/003_LongestSubstringWithoutRepeatingCharacters.cpp
+++ Array/String/003_LongestSubstringWithoutRepeatingCharacters.cpp
@@ -1,44 +1,69 @@
-#include <iostream>
-#include <stdlib.h>
-#include <string>
-
-using namespace std;
-int lengthOfLongestSubstring(string s) 
-{
-	int maxLength = 0;
-	int tempLength = 0;
-	int position = 0;
-	for (int i = 0; i < s.size(); ++i)
-	{
-		for (int j = position; j < i; ++j)
-		{
-			if (s[i] == s[j])
-			{
-				if(tempLength > maxLength)
-					maxLength = tempLength;
-				
-				tempLength = tempLength -  (j + 1 - position);
-				position = j + 1;
-				break;
-			}
-				
-		}
-		++tempLength;
-	}
-
-	if (maxLength < tempLength)
-		maxLength = tempLength;
-	return maxLength;
-}
-
-int main(void)
-{
-	string a = "abcabcbb";
-	string b = "bbtablud";
-	string c = "pwwkew";
-
-	cout << lengthOfLongestSubstring(b) << endl;
-
-	system("pause");
-	return 0;
-}
+//#include <iostream>
+//#include <stdlib.h>
+//#include <string>
+//#include <vector>
+//#include <algorithm>
+//
+//using namespace std;
+////method 1
+////int lengthOfLongestSubstring(string s) 
+////{
+////	int maxLength = 0;
+////	int tempLength = 0;
+////	int position = 0;
+////	for (int i = 0; i < s.size(); ++i)
+////	{
+////		for (int j = position; j < i; ++j)
+////		{
+////			if (s[i] == s[j])
+////			{
+////				if(tempLength > maxLength)
+////					maxLength = tempLength;
+////				
+////				tempLength = tempLength -  (j + 1 - position);
+////				position = j + 1;
+////				break;
+////			}
+////				
+////		}
+////		++tempLength;
+////	}
+////
+////	if (maxLength < tempLength)
+////		maxLength = tempLength;
+////	return maxLength;
+////}
+//
+////method 2
+//int lengthOfLongestSubstring(string s)
+//{
+//	int maxLength = 0;
+//	int position = -1;
+//	vector<int> vec(256, -1);
+//
+//	for (int i = 0; i < s.size(); ++i)
+//	{
+//		if (vec[s[i]] > position)
+//		{
+//			position = vec[s[i]];
+//		}
+//		vec[s[i]] = i;
+//
+//		maxLength = max(maxLength, i - position);
+//	}
+//
+//
+//	return maxLength;
+//}
+//
+//int main(void)
+//{
+//	string a = "abcabcbb";
+//	string b = "bbtablud";
+//	string c = "pwwkew";
+//
+//	cout << lengthOfLongestSubstring(b) << endl;
+//
+//	system("pause");
+//	return 0;
+//}
---
+++ Array/String/005_LongestPalindromicSubstring.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+//string longestPalindrome(string s) 
+//{
+//	vector<int> dict(256, -1);
+//	int position = -1;
+//	int end = -1;
+//	int start = -1;
+//	int maxLength = 0;
+//	string result = "";
+//
+//	for (int i = 0; i < s.size(); ++i)
+//	{
+//		if (dict[s[i]] > position)
+//		{
+//			//if (i - end == 1 && start != -1)
+//			//	start = position;
+//			//else
+//			//	start = dict[s[i]];
+//			position = dict[s[i]];
+//			end = i;
+//		}
+//
+//		dict[s[i]] = i;
+//		maxLength = max(maxLength, end - position);
+//	}
+//
+//	if (position == -1)
+//		return s;
+//
+//	result = s.substr(position, maxLength + 1);
+//	return result;
+//}
+
+string longestPalindrome(string s)
+{
+	if (s.empty()) return "";
+	if (s.size() == 1) return s;
+	int min_start = 0, max_len = 1;
+	for (int i = 0; i < s.size();) {
+		int j = i, k = i;
+		while (k < s.size() - 1 && s[k + 1] == s[k]) 
+			++k; // Skip duplicate characters.
+		i = k + 1;
+		while (k < s.size() - 1 && j > 0 && s[k + 1] == s[j - 1]) 
+		{	
+			++k; 
+			--j; 
+		} // Expand.
+		int new_len = k - j + 1;
+		if (new_len > max_len) 
+		{ min_start = j; max_len = new_len; }
+	}
+	return s.substr(min_start, max_len);
+}
+
+int main(void)
+{
+	string s = "abba";
+
+	string result = longestPalindrome(s);
+
+	cout << result << endl;
+	system("pause");
+	return 0;
+}
