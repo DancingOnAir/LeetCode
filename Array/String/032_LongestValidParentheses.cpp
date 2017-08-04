be6a7b4c5052172c09316f30b5e8403dc64fc852
DancingOnAir <A0135925@u.nus.edu>
2017-08-04 22:56:46 +0800

    added accepted solution for q032

--- Array/String/032_LongestValidParentheses.cpp
+++ Array/String/032_LongestValidParentheses.cpp
@@ -2,36 +2,59 @@
 #include <stdlib.h>
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <stack>
 
 using namespace std;
-int longestValidParentheses(string s) 
+//solution 1
+//int longestValidParentheses(string s) 
+//{
+//	if (s.size() < 2)
+//		return 0;
+//	int n = s.size();
+//	int i = 0;
+//	int j = 0;
+//	vector<char> record;
+//
+//	while (i < n)
+//	{
+//		switch (s[i])
+//		{
+//		case '(':
+//			record.push_back(s[i]);
+//			break;
+//		case ')':
+//			if (record.size() != 0)
+//				record.pop_back();
+//			else
+//				++j;
+//			break;
+//		}
+//
+//		++i;
+//	}
+//
+//	return n - record.size() - j;
+//}
+
+int longestValidParentheses(string s)
 {
-	if (s.size() < 2)
-		return 0;
-	int n = s.size();
-	int i = 0;
-	int j = 0;
-	vector<char> record;
+	stack<int> stk;
+	stk.push(-1);
+	int maxL = 0;
 
-	while (i < n)
+	for (int i = 0; i < s.size(); ++i)
 	{
-		switch (s[i])
+		int temp = stk.top();
+		if (temp != -1 && s[i] == ')' && s[temp] == '(')
 		{
-		case '(':
-			record.push_back(s[i]);
-			break;
-		case ')':
-			if (record.size() != 0)
-				record.pop_back();
-			else
-				++j;
-			break;
+			stk.pop();
+			maxL = max(maxL, i - stk.top());
 		}
-
-		++i;
+		else
+			stk.push(i);
 	}
-
-	return n - record.size() - j;
+	return maxL;
 }
 
 int main()
