a00afa00b0999b4fdd9176392bccf99522da07f4
DancingOnAir <A0135925@u.nus.edu>
2017-08-10 23:53:20 +0800

    added alterative solution for q094

--- Array/Tree/094_BinaryTreeInorderTraversal.cpp
+++ Array/Tree/094_BinaryTreeInorderTraversal.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <stdlib.h>
 #include <vector>
-
+#include <stack>
 using namespace std;
 
 struct TreeNode
@@ -12,20 +12,50 @@ struct TreeNode
 	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-void inorder(TreeNode *root, vector<int> &nodes)
-{
-	if (!root)
-		return;
+//void inorder(TreeNode *root, vector<int> &nodes)
+//{
+//	if (!root)
+//		return;
+//
+//	inorder(root->left, nodes);
+//	nodes.push_back(root->val);
+//	inorder(root->right, nodes);
+//}
+//
+//vector<int> inorderTraversal(TreeNode* root) 
+//{
+//	vector<int> results;
+//	inorder(root, results);
+//
+//	return results;
+//}
 
-	inorder(root->left, nodes);
-	nodes.push_back(root->val);
-	inorder(root->right, nodes);
-}
-
-vector<int> inorderTraversal(TreeNode* root) 
+vector<int> inorderTraversal(TreeNode *root)
 {
 	vector<int> results;
-	inorder(root, results);
+	if (!root)
+		return results;
+
+	stack<TreeNode *> reserve;
+	reserve.push(root);
+
+	while (!reserve.empty())
+	{
+		TreeNode *p = reserve.top();
+		if (root->left)
+		{
+			reserve.push(p->left);
+			p->left = NULL;
+		}
+		else
+		{
+			results.push_back(p->val);
+			reserve.pop();
+
+			if (p->right)
+				reserve.push(p->right);
+		}
+	}
 
 	return results;
 }
