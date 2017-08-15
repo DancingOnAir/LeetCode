d3e57252b516768195e6f77a6f0ee6e7aebca078
DancingOnAir <A0135925@u.nus.edu>
2017-08-15 21:56:24 +0800

    added accepted solution for q096

--- Array/Tree/095_UniqueBinarySearchTreesII.cpp
+++ Array/Tree/095_UniqueBinarySearchTreesII.cpp
@@ -1,82 +1,82 @@
-#include <iostream>
-#include <stdlib.h>
-#include <vector>
-
-using namespace std;
-struct TreeNode
-{
-	int val;
-	TreeNode *left;
-	TreeNode *right;
-	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-};
-
-vector<TreeNode *> generateTrees(int start, int end)
-{
-	if (start > end)
-		return vector<TreeNode *>{nullptr};
-
-
-	vector<TreeNode *> result;
-	if (start == end)
-	{
-		TreeNode *p = new TreeNode(start);
-		result.push_back(p);
-		return result;
-	}
-	
-	for (int i = start; i <= end; ++i)
-	{
-		vector<TreeNode *> left = generateTrees(start, i - 1);
-		vector<TreeNode *> right = generateTrees(i + 1, end);
-
-		for (auto lnode : left)
-		{
-			for (auto rnode : right)
-			{
-				TreeNode *root = new TreeNode(i);
-				root->left = lnode;
-				root->right = rnode;
-				result.push_back(root);
-			}
-		}
-	}
-
-	return result;
-}
-
-vector<TreeNode*> generateTrees(int n) 
-{
-	if (n < 1)
-		return vector<TreeNode *>();
-
-	return generateTrees(1, n);
-}
-
-void showTree(TreeNode *root)
-{
-	if (nullptr == root)
-	{
-		cout << "null" << " ";
-		return;
-	}
-	cout << root->val << " ";
-
-	showTree(root->left);
-	showTree(root->right);
-}
-
-int main(void)
-{
-	int numOfNodes = 3;
-	vector<TreeNode *> result = generateTrees(numOfNodes);
-
-	for (auto tree : result)
-	{
-		showTree(tree);
-		cout << endl;
-	}
-
-	system("pause");
-	return 0;
-}
+//#include <iostream>
+//#include <stdlib.h>
+//#include <vector>
+//
+//using namespace std;
+//struct TreeNode
+//{
+//	int val;
+//	TreeNode *left;
+//	TreeNode *right;
+//	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//};
+//
+//vector<TreeNode *> generateTrees(int start, int end)
+//{
+//	if (start > end)
+//		return vector<TreeNode *>{nullptr};
+//
+//
+//	vector<TreeNode *> result;
+//	if (start == end)
+//	{
+//		TreeNode *p = new TreeNode(start);
+//		result.push_back(p);
+//		return result;
+//	}
+//	
+//	for (int i = start; i <= end; ++i)
+//	{
+//		vector<TreeNode *> left = generateTrees(start, i - 1);
+//		vector<TreeNode *> right = generateTrees(i + 1, end);
+//
+//		for (auto lnode : left)
+//		{
+//			for (auto rnode : right)
+//			{
+//				TreeNode *root = new TreeNode(i);
+//				root->left = lnode;
+//				root->right = rnode;
+//				result.push_back(root);
+//			}
+//		}
+//	}
+//
+//	return result;
+//}
+//
+//vector<TreeNode*> generateTrees(int n) 
+//{
+//	if (n < 1)
+//		return vector<TreeNode *>();
+//
+//	return generateTrees(1, n);
+//}
+//
+//void showTree(TreeNode *root)
+//{
+//	if (nullptr == root)
+//	{
+//		cout << "null" << " ";
+//		return;
+//	}
+//	cout << root->val << " ";
+//
+//	showTree(root->left);
+//	showTree(root->right);
+//}
+//
+//int main(void)
+//{
+//	int numOfNodes = 3;
+//	vector<TreeNode *> result = generateTrees(numOfNodes);
+//
+//	for (auto tree : result)
+//	{
+//		showTree(tree);
+//		cout << endl;
+//	}
+//
+//	system("pause");
+//	return 0;
+//}
---
+++ Array/Tree/096_UniqueBinarySearchTrees.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
+
+using namespace std;
+
+int numTrees(int n)
+{
+	vector<int> result(n + 1, 0);
+	result[0] = 1;
+	result[1] = 1;
+
+	for (int i = 2; i < n + 1; ++i)
+	{
+		for (int j = 1; j <= i; ++j)
+			result[i] += result[j - 1] * result[i - j];
+	}
+	
+	return result[n];
+}
+
+int main(void)
+{
+	int n = 3;
+	cout << numTrees(n) << endl;
+
+	system("pause");
+	return 0;
+}
