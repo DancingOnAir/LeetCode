73e3160292250178621e9f07289f20377648615e
DancingOnAir <taotong1984@hotmail.com>
2018-08-20 08:54:48 +0800

    accepted solution for q134

---
+++ Array/Array/134_GasStation.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <cassert>
+
+int canCompleteCircuit(std::vector<int>& gas, std::vector<int>& cost) 
+{
+    assert(gas.size() == cost.size());
+
+    int pos = 0;
+    int len = gas.size();
+    int res = 0;
+    int count = 0;
+
+    for (pos = 0; pos < gas.size(); ++pos)
+    {
+        count = 0;
+        res = 0;
+        for (int i = 0; i < gas.size(); ++i)
+        {
+            res += gas[(pos + i) % len];
+            res -= cost[(pos + i) % len];
+
+            if (res < 0)
+                break;
+
+            ++count;
+        }
+
+        if (count == len)
+            return pos;
+    }
+
+    return -1;
+}
+
+int canCompleteCircuit2(std::vector<int> &gas, std::vector<int> &cost) {
+    int start(0), total(0), tank(0);
+    //if car fails at 'start', record the next station
+    for (int i = 0; i < gas.size(); i++)
+    {
+        if ((tank = tank + gas[i] - cost[i])<0)
+        {
+            start = i + 1;
+            total += tank;
+            tank = 0;
+        }
+    }
+
+    return (total + tank < 0) ? -1 : start;
+}
+
+int main()
+{
+    std::vector<int> gas = { 1, 2, 3, 4, 5 };
+    std::vector<int> cost = { 3, 4, 5, 1, 2 };
+    std::vector<int> gas2 = { 2, 3, 4 }, cost2 = {3, 4, 3};
+    std::cout << canCompleteCircuit2(gas, cost) << std::endl;
+
+    system("pause");
+    return 0;
+}
--- Array/Array/299_BullsAndCows.cpp
+++ Array/Array/299_BullsAndCows.cpp
@@ -1,46 +1,46 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <cassert>
-#include <algorithm>
-
-std::string getHint(std::string secret, std::string guess)
-{
-    assert(secret.size() == guess.size());
-
-    std::string result;
-    int bull = 0;
-    int cow = 0;
-
-    std::vector<int> vsecret(256, 0);
-    std::vector<int> vguess(256, 0);
-
-    for (int i = 0; i < secret.size(); ++i)
-    {
-        if (secret[i] == guess[i])
-            ++bull;
-
-        ++vsecret[secret[i]];
-        ++vguess[guess[i]];
-    }
-
-    for (int i = 0; i < 256; ++i)
-    {
-        if (vsecret[i] != 0 && vguess[i] != 0)
-            cow += std::min(vsecret[i], vguess[i]);
-    }
-
-    cow -= bull;
-    result = std::to_string(bull) + 'A' + std::to_string(cow) + 'B';
-
-    return result;
-}
-
-int main()
-{
-    std::string secret1 = "1123", guess1 = "0111";
-    std::string secret2 = "1807", guess2 = "7810";
-    std::cout << getHint(secret2, guess2) << std::endl;
-    system("pause");
-    return 0;
-}
+//#include <iostream>
+//#include <vector>
+//#include <string>
+//#include <cassert>
+//#include <algorithm>
+//
+//std::string getHint(std::string secret, std::string guess)
+//{
+//    assert(secret.size() == guess.size());
+//
+//    std::string result;
+//    int bull = 0;
+//    int cow = 0;
+//
+//    std::vector<int> vsecret(256, 0);
+//    std::vector<int> vguess(256, 0);
+//
+//    for (int i = 0; i < secret.size(); ++i)
+//    {
+//        if (secret[i] == guess[i])
+//            ++bull;
+//
+//        ++vsecret[secret[i]];
+//        ++vguess[guess[i]];
+//    }
+//
+//    for (int i = 0; i < 256; ++i)
+//    {
+//        if (vsecret[i] != 0 && vguess[i] != 0)
+//            cow += std::min(vsecret[i], vguess[i]);
+//    }
+//
+//    cow -= bull;
+//    result = std::to_string(bull) + 'A' + std::to_string(cow) + 'B';
+//
+//    return result;
+//}
+//
+//int main()
+//{
+//    std::string secret1 = "1123", guess1 = "0111";
+//    std::string secret2 = "1807", guess2 = "7810";
+//    std::cout << getHint(secret2, guess2) << std::endl;
+//    system("pause");
+//    return 0;
+//}
