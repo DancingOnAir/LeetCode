9c4a5f3dbe1444648d63207287527598673167aa
DancingOnAir <taotong1984@hotmail.com>
2018-08-30 19:44:15 +0800

    accepted solution for q47

--- Array/Backtracking/047_PermutationsII.cpp
+++ Array/Backtracking/047_PermutationsII.cpp
@@ -1,5 +1,7 @@
 //#include <iostream>
 //#include <vector>
+//#include <algorithm>
+//#include <unordered_set>
 //
 //void backtrack(std::vector<int>& nums, std::vector<std::vector<int>>& result, int level)
 //{
@@ -8,18 +10,26 @@
 //        result.push_back(nums);
 //        return;
 //    }
-//
+//    
+//    std::unordered_set<int> s;
 //    for (int i = level; i < nums.size(); ++i)
 //    {
+//        if (s.count(nums[i]))
+//            continue;
+//
+//        s.insert(nums[i]);
+//
 //        std::swap(nums[level], nums[i]);
 //        backtrack(nums, result, level + 1);
 //        std::swap(nums[level], nums[i]);
 //    }
 //}
 //
-//std::vector<std::vector<int>> permute(std::vector<int>& nums) 
+//std::vector<std::vector<int>> permuteUnique(std::vector<int>& nums)
 //{
 //    std::vector<std::vector<int>> result;
+//    std::unordered_set<int> s;
+//
 //    backtrack(nums, result, 0);
 //
 //    return result;
@@ -27,8 +37,8 @@
 //
 //int main()
 //{
-//    std::vector<int> input = { 1, 2, 3 };
-//    std::vector<std::vector<int>> result = permute(input);
+//    std::vector<int> input = { 1, 1, 2 };
+//    std::vector<std::vector<int>> result = permuteUnique(input);
 //
 //    for (auto row : result)
 //    {
--- Array/Backtracking/078_Subsets.cpp
+++ Array/Backtracking/078_Subsets.cpp
@@ -1,7 +1,7 @@
 //#include <iostream>
 //#include <vector>
 //
-//void backtrack(std::vector<int>& nums, std::vector<std::vector<int>>& result, std::vector<int>& temp, int level, int size)
+//void backtrack1(std::vector<int>& nums, std::vector<std::vector<int>>& result, std::vector<int>& temp, int level, int size)
 //{
 //    if (level >= size)
 //    {
@@ -11,9 +11,24 @@
 //
 //    temp.push_back(nums[level]);
 //    ++level;
-//    backtrack(nums, result, temp, level, size);
+//    backtrack1(nums, result, temp, level, size);
 //    temp.pop_back();
-//    backtrack(nums, result, temp, level, size);
+//    backtrack1(nums, result, temp, level, size);
+//}
+//
+//void backtrack2(std::vector<int>& nums, std::vector<std::vector<int>>& result, std::vector<int>& temp, int level, int size)
+//{
+//    result.push_back(temp);
+//
+//    for(int i = level; i < size; ++i)
+//    {
+//        //if (i > level && nums[i] == nums[i - 1])
+//        //    continue;
+//
+//        temp.push_back(nums[i]);
+//        backtrack2(nums, result, temp, i + 1, size);
+//        temp.pop_back();
+//    }
 //}
 //
 //void display(std::vector<std::vector<int>> input)
@@ -30,14 +45,14 @@
 //{
 //    std::vector<std::vector<int>> result;
 //    std::vector<int> temp;
-//    backtrack(nums, result, temp, 0, nums.size());
+//    backtrack2(nums, result, temp, 0, nums.size());
 //
 //    return result;
 //}
 //
 //int main()
 //{
-//    std::vector<int> input = { 1, 2, 3 };
+//    std::vector<int> input = { 1, 2, 3};
 //    std::vector<std::vector<int>> result = subsets(input);
 //
 //    display(result);
