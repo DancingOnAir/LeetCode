d4d70f6fa5679ca5f421a32d466b1ab311d5db59
DancingOnAir <taotong1984@hotmail.com>
2018-09-08 15:03:10 +0800

    accepted solution for q017

--- Array/String/017_LetterCombinationsOfAPhoneNumber.cpp
+++ Array/String/017_LetterCombinationsOfAPhoneNumber.cpp
@@ -1,10 +1,10 @@
-//#include <iostream>
-//#include <stdlib.h>
-//#include <string>
-//#include <vector>
-//
-//using namespace std;
-//
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <vector>
+
+using namespace std;
+
 //vector<string> letterCombinations(string digits)
 //{
 //	if (digits.empty()) 
@@ -35,18 +35,54 @@
 //
 //	return result;
 //}
-//
-//int main(void)
-//{
-//	string digits = "234";
-//
-//	vector<string> result = letterCombinations(digits);
-//
-//	for (auto val : result)
-//		cout << val << ", ";
-//
-//	cout << endl;
-//
-//	system("pause");
-//	return 0;
-//}
+
+void backtrack(vector<string>& alphabets, vector<int>& nums, vector<string>& result, string& current, int k)
+{
+    if (k == nums.size())
+    {
+        result.push_back(current);
+        return;
+    }
+
+    for (int i = 0; i < alphabets[nums[k]].size(); ++i)
+    {
+        current.push_back(alphabets[nums[k]][i]);
+        backtrack(alphabets, nums, result, current, k + 1);
+        current.pop_back();
+    }
+}
+
+vector<string> letterCombinations(string digits) 
+{
+    vector<string> result;
+    if (digits.empty())
+        return result;
+
+    string current;
+    //0 - 9
+    vector<string> alphabets = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+    vector<int> nums;
+    for (auto val : digits)
+    {
+        nums.push_back(val - '0');
+    }
+
+    backtrack(alphabets, nums, result, current, 0);
+
+    return result;
+}
+
+int main(void)
+{
+    string digits = "23";
+
+    vector<string> result = letterCombinations(digits);
+
+    for (auto val : result)
+	    cout << val << ", ";
+
+    cout << endl;
+
+    system("pause");
+    return 0;
+}
