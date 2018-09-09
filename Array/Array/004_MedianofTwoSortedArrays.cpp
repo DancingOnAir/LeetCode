12a4931da02c22d7a99786e8e082d3eca30c6104
DancingOnAir <taotong1984@hotmail.com>
2018-09-09 17:05:20 +0800

    accepted solution for q004

--- Array/Array/004_MedianofTwoSortedArrays.cpp
+++ Array/Array/004_MedianofTwoSortedArrays.cpp
@@ -1,43 +1,74 @@
-//#include <iostream>
-//#include <stdlib.h>
-//#include <vector>
-//#include <algorithm>
-//
-//using namespace std;
-//
-//double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-//	double median = 0;
-//
-//	nums1.insert(nums1.end(), nums2.begin(), nums2.end());
-//	
-//	int size = nums1.size();
-//	if (size % 2 != 0)
-//	{	
-//
-//		nth_element(nums1.begin(), nums1.begin() + size / 2, nums1.end());
-//		median = nums1.at(size / 2);
-//	}
-//	else
-//	{
-//		nth_element(nums1.begin(), nums1.begin() + size / 2 - 1, nums1.end());
-//		median = nums1.at(size / 2 - 1);
-//		nth_element(nums1.begin(), nums1.begin() + size / 2, nums1.end());
-//		median = (median + nums1.at(size / 2))/2;
-//	}
-//
-//	return median;
-//}
-//
-//int main(void)
-//{
-//	int a[] = { 1, 2 };
-//	int b[] = { 3, 4};
-//	vector<int> nums1(a, a + 2);
-//	vector<int> nums2(b, b + 2);
-//
-//	cout << findMedianSortedArrays(nums1, nums2) << endl;
-//
-//	system("pause");
-//
-//	return 0;
-//}
+#include <iostream>
+#include <stdlib.h>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+	double median = 0;
+
+	nums1.insert(nums1.end(), nums2.begin(), nums2.end());
+	
+	int size = nums1.size();
+	if (size % 2 != 0)
+	{	
+
+		nth_element(nums1.begin(), nums1.begin() + size / 2, nums1.end());
+		median = nums1.at(size / 2);
+	}
+	else
+	{
+		nth_element(nums1.begin(), nums1.begin() + size / 2 - 1, nums1.end());
+		median = nums1.at(size / 2 - 1);
+		nth_element(nums1.begin(), nums1.begin() + size / 2, nums1.end());
+		median = (median + nums1.at(size / 2))/2;
+	}
+
+	return median;
+}
+
+double findMedianSortedArrays2(vector<int>& nums1, vector<int>& nums2) 
+{
+    int n = nums1.size();
+    int m = nums2.size();
+
+    if (n > m)
+        return findMedianSortedArrays2(nums2, nums1);
+
+    int L1, L2, R1, R2, c1, c2, lo = 0, hi = 2*n;
+
+    while (lo <= hi)
+    {
+        c1 = (lo + hi) / 2;
+        c2 = m + n - c1;
+
+        L1 = (c1 == 0) ? INT_MIN : nums1[(c1 - 1) / 2];
+        R1 = (c1 == 2 * n) ? INT_MAX : nums1[c1 / 2];
+        L2 = (c2 == 0) ? INT_MIN : nums2[(c2 - 1) / 2];
+        R2 = (c2 == 2 * m) ? INT_MAX : nums2[c2 / 2];
+
+        if (L1 > R2)
+            hi = c1 - 1;
+        else if (L2 > R1)
+            lo = c1 + 1;
+        else
+            break;
+    }
+
+    return (max(L1, L2) + min(R1, R2)) / 2.0;
+}
+
+int main(void)
+{
+	int a[] = { 1, 2 };
+	int b[] = { 3, 4};
+	vector<int> nums1(a, a + 2);
+	vector<int> nums2(b, b + 2);
+
+	cout << findMedianSortedArrays2(nums1, nums2) << endl;
+
+	system("pause");
+
+	return 0;
+}
--- Array/Backtracking/320_GeneralizedAbbreviation.cpp
+++ Array/Backtracking/320_GeneralizedAbbreviation.cpp
@@ -1,70 +1,70 @@
-//Write a function to generate the generalized abbreviations of a word.
+////Write a function to generate the generalized abbreviations of a word.
+////
+////Example:
+////
+////Given word = "word", return the following list(order does not matter) :
+////
+////    ["word", "1ord", "w1rd", "wo1d", "wor1", "2rd", "w2d", "wo2", "1o1d", "1or1", "w1r1", "1o2", "2r1", "3d", "w3", "4"]
 //
-//Example:
+//#include <iostream>
+//#include <vector>
+//#include <string>
 //
-//Given word = "word", return the following list(order does not matter) :
+//void backtrack(std::vector<std::string>& result, std::string& word, int pos, int cnt, std::string path)
+//{
+//    if (pos == word.size())
+//    {
+//        if (cnt > 0)
+//            path += std::to_string(cnt);
 //
-//    ["word", "1ord", "w1rd", "wo1d", "wor1", "2rd", "w2d", "wo2", "1o1d", "1or1", "w1r1", "1o2", "2r1", "3d", "w3", "4"]
-
-#include <iostream>
-#include <vector>
-#include <string>
-
-void backtrack(std::vector<std::string>& result, std::string& word, int pos, int cnt, std::string path)
-{
-    if (pos == word.size())
-    {
-        if (cnt > 0)
-            path += std::to_string(cnt);
-
-        result.push_back(path);
-        return;
-    }
-
-    backtrack(result, word, pos + 1, cnt + 1, path);
-    backtrack(result, word, pos + 1, 0, path + (cnt > 0 ? std::to_string(cnt) : "") + word[pos]);
-}
-
-std::vector<std::string> generateAbbreviations(std::string word)
-{
-    std::vector<std::string> result;
-    std::string path;
-    backtrack(result, word, 0, 0, path);
-
-    return result;
-}
-
-void backtrack2(std::vector<std::string>& result, std::string& word, int pos)
-{
-    for (int i = pos; i < word.size(); ++i)
-    {
-        for (int j = 1; i + j <= word.size(); ++j)
-        {
-            std::string temp = word.substr(0, i);
-            temp += std::to_string(j) + word.substr(i + j);
-            result.push_back(temp);
-            backtrack2(result, temp, i + 1 + std::to_string(j).size());
-        }
-    }
-}
-
-std::vector<std::string> generateAbbreviations2(std::string word)
-{
-    std::vector<std::string> result;
-    std::string path;
-    backtrack2(result, word, 0);
-
-    return result;
-}
-
-int main()
-{
-    std::string input = "word";
-    std::vector<std::string> result = generateAbbreviations2(input);
-
-    for (auto s : result)
-        std::cout << s << std::endl;
-
-    system("pause");
-    return 0;
-}
+//        result.push_back(path);
+//        return;
+//    }
+//
+//    backtrack(result, word, pos + 1, cnt + 1, path);
+//    backtrack(result, word, pos + 1, 0, path + (cnt > 0 ? std::to_string(cnt) : "") + word[pos]);
+//}
+//
+//std::vector<std::string> generateAbbreviations(std::string word)
+//{
+//    std::vector<std::string> result;
+//    std::string path;
+//    backtrack(result, word, 0, 0, path);
+//
+//    return result;
+//}
+//
+//void backtrack2(std::vector<std::string>& result, std::string& word, int pos)
+//{
+//    for (int i = pos; i < word.size(); ++i)
+//    {
+//        for (int j = 1; i + j <= word.size(); ++j)
+//        {
+//            std::string temp = word.substr(0, i);
+//            temp += std::to_string(j) + word.substr(i + j);
+//            result.push_back(temp);
+//            backtrack2(result, temp, i + 1 + std::to_string(j).size());
+//        }
+//    }
+//}
+//
+//std::vector<std::string> generateAbbreviations2(std::string word)
+//{
+//    std::vector<std::string> result;
+//    std::string path;
+//    backtrack2(result, word, 0);
+//
+//    return result;
+//}
+//
+//int main()
+//{
+//    std::string input = "word";
+//    std::vector<std::string> result = generateAbbreviations2(input);
+//
+//    for (auto s : result)
+//        std::cout << s << std::endl;
+//
+//    system("pause");
+//    return 0;
+//}
