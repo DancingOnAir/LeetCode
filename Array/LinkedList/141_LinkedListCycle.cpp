9f128b44ddbc2daad3518cdb5d5457e68b7cc92c
DancingOnAir <taotong1984@hotmail.com>
2018-09-19 21:40:36 +0800

    accepted solution for q024

--- Array/LinkedList/141_LinkedListCycle.cpp
+++ Array/LinkedList/141_LinkedListCycle.cpp
@@ -1,52 +1,52 @@
-#include <iostream>
-
-struct ListNode
-{
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(nullptr) {}
-};
-
-bool hasCycle(ListNode *head) 
-{
-        ListNode *slow = head;
-        ListNode *fast = head;
-
-        while (slow  && fast && fast->next)
-        {
-            slow = slow->next;
-            fast = fast->next->next;
-
-            if (fast == slow)
-                return true;
-        }
-
-        return false;
-}
-
-void testHasCycle()
-{
-    ListNode *head = new ListNode(1);
-    ListNode *node2 = new ListNode(2);
-    ListNode *node3 = new ListNode(3);
-    ListNode *node4 = new ListNode(4);
-    ListNode *node5 = new ListNode(5);
-    ListNode *node6 = new ListNode(6);
-
-    head->next = node2;
-    node2->next = node3;
-    node3->next = node4;
-    node4->next = node5;
-    node5->next = node6;
-    node6->next = node4;
-
-    bool status = hasCycle(head);
-    std::cout << (status ? "has cycle" : "no cycle") << std::endl;
-}
-
-int main()
-{
-    testHasCycle();
-    system("pause");
-    return 0;
-}
+//#include <iostream>
+//
+//struct ListNode
+//{
+//    int val;
+//    ListNode *next;
+//    ListNode(int x) : val(x), next(nullptr) {}
+//};
+//
+//bool hasCycle(ListNode *head) 
+//{
+//        ListNode *slow = head;
+//        ListNode *fast = head;
+//
+//        while (slow  && fast && fast->next)
+//        {
+//            slow = slow->next;
+//            fast = fast->next->next;
+//
+//            if (fast == slow)
+//                return true;
+//        }
+//
+//        return false;
+//}
+//
+//void testHasCycle()
+//{
+//    ListNode *head = new ListNode(1);
+//    ListNode *node2 = new ListNode(2);
+//    ListNode *node3 = new ListNode(3);
+//    ListNode *node4 = new ListNode(4);
+//    ListNode *node5 = new ListNode(5);
+//    ListNode *node6 = new ListNode(6);
+//
+//    head->next = node2;
+//    node2->next = node3;
+//    node3->next = node4;
+//    node4->next = node5;
+//    node5->next = node6;
+//    node6->next = node4;
+//
+//    bool status = hasCycle(head);
+//    std::cout << (status ? "has cycle" : "no cycle") << std::endl;
+//}
+//
+//int main()
+//{
+//    testHasCycle();
+//    system("pause");
+//    return 0;
+//}
