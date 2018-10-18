f2e8b0fab300bc59d06a9774a7fb996f65bf7a6f
DancingOnAir <taotong1984@hotmail.com>
2018-10-18 10:37:29 +0800

    accepted solution for q100

---
+++ Array/Tree/100_SameTree.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+struct TreeNode
+{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x): val(x), left(nullptr), right(nullptr) {}
+};
+
+bool isSameTree(TreeNode* p, TreeNode* q)
+{
+    if (nullptr == p || nullptr == q)
+        return p == q;
+
+    return (p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, p->right));
+}
