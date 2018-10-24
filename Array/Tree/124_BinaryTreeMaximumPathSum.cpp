d0c5a6a40f127d8a1330e8fb3c478e7a9fd2f509
DancingOnAir <taotong1984@hotmail.com>
2018-10-24 19:39:56 +0800

    accepted solution for q337

--- Array/Tree/124_BinaryTreeMaximumPathSum.cpp
+++ Array/Tree/124_BinaryTreeMaximumPathSum.cpp
@@ -1,60 +1,60 @@
-#include <iostream>
-#include <algorithm>
-using namespace std;
-
-struct TreeNode
-{
-    int val;
-    TreeNode* left;
-    TreeNode* right;
-    TreeNode(int x): val(x), left(nullptr), right(nullptr) {}
-};
-
-int maxToRoot(TreeNode* root, int& max)
-{
-    if (nullptr == root)
-        return 0;
-
-    int l = maxToRoot(root->left, max);
-    int r = maxToRoot(root->right, max);
-
-    if (l < 0)
-        l = 0;
-    if (r < 0)
-        r = 0;
-
-    if (l + r + root->val > max)
-        max = l + r + root->val;
-
-    return root->val += std::max(l, r);
-}
-
-int maxPathSum(TreeNode* root)
-{
-    int max = INT_MIN;
-    maxToRoot(root, max);
-    return max;
-}
-
-void testMaxPathSum()
-{
-    TreeNode* node1 = new TreeNode(-10);
-    TreeNode* node2 = new TreeNode(9);
-    TreeNode* node3 = new TreeNode(20);
-    TreeNode* node4 = new TreeNode(15);
-    TreeNode* node5 = new TreeNode(7);
-
-    node1->left = node2;
-    node1->right = node3;
-    node3->left = node4;
-    node3->right = node5;
-
-    cout << maxPathSum(node1) << endl;
-}
-
-int main()
-{
-    testMaxPathSum();
-    system("pause");
-    return 0;
-}
+//#include <iostream>
+//#include <algorithm>
+//using namespace std;
+//
+//struct TreeNode
+//{
+//    int val;
+//    TreeNode* left;
+//    TreeNode* right;
+//    TreeNode(int x): val(x), left(nullptr), right(nullptr) {}
+//};
+//
+//int maxToRoot(TreeNode* root, int& max)
+//{
+//    if (nullptr == root)
+//        return 0;
+//
+//    int l = maxToRoot(root->left, max);
+//    int r = maxToRoot(root->right, max);
+//
+//    if (l < 0)
+//        l = 0;
+//    if (r < 0)
+//        r = 0;
+//
+//    if (l + r + root->val > max)
+//        max = l + r + root->val;
+//
+//    return root->val += std::max(l, r);
+//}
+//
+//int maxPathSum(TreeNode* root)
+//{
+//    int max = INT_MIN;
+//    maxToRoot(root, max);
+//    return max;
+//}
+//
+//void testMaxPathSum()
+//{
+//    TreeNode* node1 = new TreeNode(-10);
+//    TreeNode* node2 = new TreeNode(9);
+//    TreeNode* node3 = new TreeNode(20);
+//    TreeNode* node4 = new TreeNode(15);
+//    TreeNode* node5 = new TreeNode(7);
+//
+//    node1->left = node2;
+//    node1->right = node3;
+//    node3->left = node4;
+//    node3->right = node5;
+//
+//    cout << maxPathSum(node1) << endl;
+//}
+//
+//int main()
+//{
+//    testMaxPathSum();
+//    system("pause");
+//    return 0;
+//}
---
+++ Array/Tree/337_HouseRobberIII.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x): val(x), left(nullptr), right(nullptr) {}
+};
+
+//int getMaxSub(vector<int>& nums)
+//{
+//    if (0 == nums.size())
+//        return 0;
+//
+//    if (1 == nums.size())
+//        return nums[0];
+//
+//    if (2 == nums.size())
+//        return max(nums[0], nums[1]);
+//
+//    if (3 == nums.size())
+//        return max(nums[0] + nums[2], nums[1]);
+//
+//    vector<int> maxSub(nums.size());
+//    maxSub[0] = nums[0];
+//    maxSub[1] = max(nums[0], nums[1]);
+//    maxSub[2] = max(nums[0] + nums[2], nums[1]);
+//    int result = max(max(maxSub[0], maxSub[1]), maxSub[2]);
+//    for (auto i = 3; i < nums.size(); ++i)
+//    {
+//        maxSub[i] = max(maxSub[i - 2], maxSub[i - 3]) + nums[i];
+//        result = max(result, maxSub[i]);
+//    }
+//
+//    return result;
+//}
+
+int robRecursive(TreeNode* root, int& l, int& r)
+{
+    if (nullptr == root)
+        return 0;
+
+    int ll = 0, lr = 0, rl = 0, rr = 0;
+    l = robRecursive(root->left, ll, lr);
+    r = robRecursive(root->right, rl, rr);
+
+    return max(root->val + ll + lr + rl + rr, r + l);
+}
+
+int rob(TreeNode* root)
+{
+    int l = 0, r = 0;
+
+    return robRecursive(root, l, r);
+}
+
+void testRob()
+{
+    //TreeNode* node1 = new TreeNode(3);
+    //TreeNode* node2 = new TreeNode(4);
+    //TreeNode* node3 = new TreeNode(5);
+    //TreeNode* node4 = new TreeNode(1);
+    //TreeNode* node5 = new TreeNode(3);
+    //TreeNode* node6 = new TreeNode(1);
+
+    //node1->left = node2;
+    //node1->right = node3;
+    //node2->left = node4;
+    //node2->right = node5;
+    //node3->right = node6;
+
+    TreeNode* node1 = new TreeNode(4);
+    TreeNode* node2 = new TreeNode(1);
+    TreeNode* node3 = new TreeNode(2);
+    TreeNode* node4 = new TreeNode(3);
+    node1->left = node2;
+    node2->left = node3;
+    node3->left = node4;
+
+    cout << rob(node1) << endl;
+}
+
+int main()
+{
+    testRob();
+    system("pause");
+    return 0;
+}
