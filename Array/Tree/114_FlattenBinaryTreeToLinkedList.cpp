bf991c253aefb60c5f3298734d9620186a9a34f5
DancingOnAir <taotong1984@hotmail.com>
2018-10-27 19:51:29 +0800

    accepted solution for q173

--- Array/Tree/114_FlattenBinaryTreeToLinkedList.cpp
+++ Array/Tree/114_FlattenBinaryTreeToLinkedList.cpp
@@ -1,97 +1,97 @@
-#include <iostream>
-#include <stack>
-
-struct TreeNode
-{
-    int val;
-    TreeNode* left;
-    TreeNode* right;
-    TreeNode(int x): val(x), left(nullptr), right(nullptr) {}
-};
-
-void flatten(TreeNode* root)
-{
-    if (nullptr == root)
-        return;
-
-    TreeNode* cur = root;
-    while (cur)
-    {
-        if (cur->left)
-        {
-            TreeNode* pre = cur->left;
-            while (pre->right)
-                pre = pre->right;
-
-            pre->right = cur->right;
-
-            cur->right = cur->left;
-            cur->left = nullptr;
-        }
-
-        cur = cur->right;
-    }
-}
-
-void flatten1(TreeNode* root)
-{
-    if (nullptr == root)
-        return;
-
-    std::stack<TreeNode*> s;
-    s.emplace(root);
-    TreeNode* cur = nullptr;
-    TreeNode* head = root;
-
-    while (!s.empty())
-    {
-        cur = s.top();
-        s.pop();
-        
-        if (cur->right)
-            s.emplace(cur->right);
-        if (cur->left)
-            s.emplace(cur->left);
-
-        head->left = nullptr;
-        head->right = cur;
-        head = head->right;
-    }
-}
-
-void inorderTraversal(TreeNode* root)
-{
-    if (nullptr == root)
-        return;
-
-    inorderTraversal(root->left);
-    std::cout << root->val << std::endl;
-    inorderTraversal(root->right);
-}
-
-void testFlatten()
-{
-    TreeNode* node1 = new TreeNode(1);
-    TreeNode* node2 = new TreeNode(2);
-    TreeNode* node3 = new TreeNode(3);
-    TreeNode* node4 = new TreeNode(4);
-    TreeNode* node5 = new TreeNode(5);
-    TreeNode* node6 = new TreeNode(6);
-
-    node1->left = node2;
-    node1->right = node5;
-    node2->left = node3;
-    node2->right = node4;
-    node5->right = node6;
-
-    inorderTraversal(node1);
-    flatten(node1);
-    inorderTraversal(node1);
-}
-
-int main()
-{
-    testFlatten();
-    system("pause");
-    return 0;
-}
+//#include <iostream>
+//#include <stack>
+//
+//struct TreeNode
+//{
+//    int val;
+//    TreeNode* left;
+//    TreeNode* right;
+//    TreeNode(int x): val(x), left(nullptr), right(nullptr) {}
+//};
+//
+//void flatten(TreeNode* root)
+//{
+//    if (nullptr == root)
+//        return;
+//
+//    TreeNode* cur = root;
+//    while (cur)
+//    {
+//        if (cur->left)
+//        {
+//            TreeNode* pre = cur->left;
+//            while (pre->right)
+//                pre = pre->right;
+//
+//            pre->right = cur->right;
+//
+//            cur->right = cur->left;
+//            cur->left = nullptr;
+//        }
+//
+//        cur = cur->right;
+//    }
+//}
+//
+//void flatten1(TreeNode* root)
+//{
+//    if (nullptr == root)
+//        return;
+//
+//    std::stack<TreeNode*> s;
+//    s.emplace(root);
+//    TreeNode* cur = nullptr;
+//    TreeNode* head = root;
+//
+//    while (!s.empty())
+//    {
+//        cur = s.top();
+//        s.pop();
+//        
+//        if (cur->right)
+//            s.emplace(cur->right);
+//        if (cur->left)
+//            s.emplace(cur->left);
+//
+//        head->left = nullptr;
+//        head->right = cur;
+//        head = head->right;
+//    }
+//}
+//
+//void inorderTraversal(TreeNode* root)
+//{
+//    if (nullptr == root)
+//        return;
+//
+//    inorderTraversal(root->left);
+//    std::cout << root->val << std::endl;
+//    inorderTraversal(root->right);
+//}
+//
+//void testFlatten()
+//{
+//    TreeNode* node1 = new TreeNode(1);
+//    TreeNode* node2 = new TreeNode(2);
+//    TreeNode* node3 = new TreeNode(3);
+//    TreeNode* node4 = new TreeNode(4);
+//    TreeNode* node5 = new TreeNode(5);
+//    TreeNode* node6 = new TreeNode(6);
+//
+//    node1->left = node2;
+//    node1->right = node5;
+//    node2->left = node3;
+//    node2->right = node4;
+//    node5->right = node6;
+//
+//    inorderTraversal(node1);
+//    flatten(node1);
+//    inorderTraversal(node1);
+//}
+//
+//int main()
+//{
+//    testFlatten();
+//    system("pause");
+//    return 0;
+//}
---
+++ Array/Tree/173_BinarySearchTreeIterator.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+class BSTIterator {
+public:
+    BSTIterator(TreeNode *root)
+    {
+        pushTreeNode(root);
+    }
+
+    /** @return whether we have a next smallest number */
+    bool hasNext()
+    {
+        return !st.empty();
+    }
+
+    /** @return the next smallest number */
+    int next()
+    {
+        TreeNode* temp = st.top();
+        st.pop();
+        pushTreeNode(temp->right);
+        return temp->val;
+        
+    }
+
+    void pushTreeNode(TreeNode* root)
+    {
+        for (; root != nullptr; root = root->left)
+            st.emplace(root);
+    }
+
+private:
+    stack<TreeNode*> st;
+};
+
+void testBSTIterator()
+{
+    TreeNode* node1 = new TreeNode(1);
+    TreeNode* node2 = new TreeNode(2);
+    TreeNode* node3 = new TreeNode(3);
+    TreeNode* node4 = new TreeNode(4);
+    TreeNode* node5 = new TreeNode(5);
+    TreeNode* node6 = new TreeNode(6);
+    TreeNode* node7 = new TreeNode(7);
+    TreeNode* node8 = new TreeNode(8);
+    TreeNode* node9 = new TreeNode(9);
+    
+    node5->left = node2;
+    node5->right = node7;
+    node2->left = node1;
+    node2->right = node3;
+    node7->left = node6;
+    node7->right = node9;
+    node3->right = node4;
+    node9->left = node8;
+
+    BSTIterator bstIterator(node5);
+    bool result = bstIterator.hasNext();
+    cout << (result ? "true" : "false") << endl;
+    cout << bstIterator.next() << endl;
+}
+
+int main()
+{
+
+    system("pause");
+    return 0;
+}
