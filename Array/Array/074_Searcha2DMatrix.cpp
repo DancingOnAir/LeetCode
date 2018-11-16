7f54f9a2fd80257bb9aa8e8a1a18bbcebf908108
DancingOnAir <taotong1984@hotmail.com>
2018-11-16 19:05:18 +0800

    accepted solutionf for q074

--- Array/Array/074_Searcha2DMatrix.cpp
+++ Array/Array/074_Searcha2DMatrix.cpp
@@ -35,13 +35,40 @@ bool searchMatrix(vector<vector<int>>& matrix, int target)
     return false;
 }
 
+bool searchMatrix2(vector<vector<int>>& matrix, int target)
+{
+    int row = matrix.size();
+    if (0 == row)
+        return false;
+
+    int col = matrix[0].size();
+    if (0 == col)
+        return false;
+
+    int start = 0;
+    int end = row * col - 1;
+    while (start <= end)
+    {
+        int mid = start + (end - start) / 2;
+        if (matrix[mid / col][mid % col] == target)
+            return true;
+        else if (matrix[mid/col][mid%col] > target)
+            end = mid - 1;
+        else
+            start = mid + 1;
+
+    }
+
+    return false;
+}
+
 void testSearchMatrix()
 {
     vector<vector<int>> matrix = { {1, 3, 5, 7}, 
                                 {10, 11, 16, 20},
                                 {23, 30, 34, 50} };
-
-    bool result = searchMatrix(matrix, 3);
+    vector<vector<int>> matrix2 = { {1, 1} };
+    bool result = searchMatrix2(matrix2, 2);
     cout << (result ? "true" : "false") << endl;
 }
 
