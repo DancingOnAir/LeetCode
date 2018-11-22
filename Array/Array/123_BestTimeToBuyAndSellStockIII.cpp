f76539a4a2696d17bc1e2f13ad62505a3d0c46b8
DancingOnAir <taotong1984@hotmail.com>
2018-11-22 11:09:28 +0800

    accepted solution for q123

---
+++ Array/Array/123_BestTimeToBuyAndSellStockIII.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int maxProfit(vector<int>& prices)
+{
+    if (prices.empty())
+        return 0;
+
+    int buy1 = INT_MIN, buy2 = INT_MIN, sell1 = 0, sell2 = 0;
+
+    for (int& x : prices)
+    {
+        sell2 = max(sell2, buy2 + x);
+        buy2 = max(buy2, sell1 - x);
+        sell1 = max(sell1, buy1 + x);
+        buy1 = max(buy1, -x);
+    }
+
+    return sell2;
+}
+
+void testMaxProfit()
+{
+    vector<int> prices = { 3, 3, 5, 0, 0, 3, 1, 4 };
+    cout << maxProfit(prices) << endl;
+}
+
+int main()
+{
+    testMaxProfit();
+    getchar();
+}
