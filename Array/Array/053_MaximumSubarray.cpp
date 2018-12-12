dfbf5beb82dc577880a05462cf2b3b2ee5e67aac
DancingOnAir <taotong1984@hotmail.com>
2018-12-12 11:42:38 +0800

    accepted solution for 053

---
+++ Array/Array/053_MaximumSubarray.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int maxSubArray(vector<int>& nums)
+{
+    if (nums.empty())
+        return 0;
+
+    vector<int> dp(nums.size());
+    dp[0] = nums[0];
+    int res = nums[0];
+
+    for (size_t i = 1; i < nums.size(); ++i)
+    {
+        dp[i] = nums[i] + (dp[i - 1] > 0? dp[i - 1]:0);
+        res = max(res, dp[i]);
+    }
+
+    return res;
+}
+
+int maxSubArray2(vector<int>& nums)
+{
+    int sum = 0;
+    int res = std::numeric_limits<int>::min();
+    for (auto i = 0; i < nums.size(); ++i)
+    {
+        sum += nums[i];
+        res = sum > res ? sum : res;
+        sum = sum > 0 ? sum : 0;
+    }
+
+    return res;
+}
+
+void testMaxSubArray()
+{
+    vector<int> nums = { -2,1,-3,4,-1,2,1,-5,4 };
+    cout << maxSubArray2(nums) << endl;
+}
+
+int main()
+{
+    testMaxSubArray();
+    getchar();
+}
--- Array/Array/062_UniquePaths.cpp
+++ Array/Array/062_UniquePaths.cpp
@@ -1,56 +1,56 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-int uniquePaths(int m, int n)
-{
-    if (1 == m || 1 == n)
-        return 1;
-
-    vector<vector<int>> matrix(n, vector<int>(m));
-    for (int i = 0; i < m; ++i)
-        matrix[0][i] = 1;
-
-    for (int i = 0; i < n; ++i)
-        matrix[i][0] = 1;
-
-    for (int i = 1; i < n; ++i)
-    {
-        for (int j = 1; j < m; ++j)
-        {
-            matrix[i][j] = matrix[i - 1][j] + matrix[i][j - 1];
-        }
-    }
-
-    return matrix[n - 1][m - 1];
-}
-
-int uniquePaths2(int m, int n)
-{
-    if (m > n)
-        uniquePaths2(n, m);
-
-    vector<int> cur(m, 1);
-    for (int i = 1; i < n; ++i)
-    {
-        for (int j = 1; j < m; ++j)
-            cur[j] += cur[j - 1];
-    }
-
-    return cur[m - 1];
-}
-
-void testUniquePaths()
-{
-    int m = 7;
-    int n = 3;
-
-    cout << uniquePaths2(m, n) << endl;
-}
-
-int main()
-{
-    testUniquePaths();
-    getchar();
-}
+//#include <iostream>
+//#include <vector>
+//
+//using namespace std;
+//
+//int uniquePaths(int m, int n)
+//{
+//    if (1 == m || 1 == n)
+//        return 1;
+//
+//    vector<vector<int>> matrix(n, vector<int>(m));
+//    for (int i = 0; i < m; ++i)
+//        matrix[0][i] = 1;
+//
+//    for (int i = 0; i < n; ++i)
+//        matrix[i][0] = 1;
+//
+//    for (int i = 1; i < n; ++i)
+//    {
+//        for (int j = 1; j < m; ++j)
+//        {
+//            matrix[i][j] = matrix[i - 1][j] + matrix[i][j - 1];
+//        }
+//    }
+//
+//    return matrix[n - 1][m - 1];
+//}
+//
+//int uniquePaths2(int m, int n)
+//{
+//    if (m > n)
+//        uniquePaths2(n, m);
+//
+//    vector<int> cur(m, 1);
+//    for (int i = 1; i < n; ++i)
+//    {
+//        for (int j = 1; j < m; ++j)
+//            cur[j] += cur[j - 1];
+//    }
+//
+//    return cur[m - 1];
+//}
+//
+//void testUniquePaths()
+//{
+//    int m = 7;
+//    int n = 3;
+//
+//    cout << uniquePaths2(m, n) << endl;
+//}
+//
+//int main()
+//{
+//    testUniquePaths();
+//    getchar();
+//}
