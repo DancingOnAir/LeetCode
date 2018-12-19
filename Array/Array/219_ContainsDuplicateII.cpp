809e24ad8d51cf755c2ed972f166b055e2adac08
DancingOnAir <taotong1984@hotmail.com>
2018-12-19 10:48:25 +0800

    accpeted solution for q216

---
+++ Array/Array/219_ContainsDuplicateII.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <unordered_map>
+using namespace std;
+
+bool containsNearbyDuplicate(vector<int>& nums, int k)
+{
+    if (nums.empty() || k <= 0)
+        return false;
+
+    set<int> candidates;
+
+    for (int i = 0; i < nums.size(); ++i)
+    {
+        if (i > k)
+            candidates.erase(nums[i - k - 1]);
+        if (!candidates.insert(nums[i]).second)
+            return true;
+    }
+
+    return false;
+}
+
+bool containsNearbyDuplicate2(vector<int>& nums, int k)
+{
+    if (nums.empty() || k <= 0)
+        return false;
+
+    unordered_map<int, int> m;
+    for (int i = 0; i < nums.size(); ++i)
+    {
+        if (m.find(nums[i]) != m.end() && i - m[nums[i]] <= k)
+            return true;
+
+        m[nums[i]] = i;
+    }
+
+    return false;
+}
+
+void testContainNearbyDuplicate()
+{
+    vector<int> nums = { 1,2,3,1 };
+    vector<int> nums2 = { 1,2,3,1,2,3 };
+    int k = 3;
+
+    cout << (containsNearbyDuplicate(nums, k) ? "true" : "false") << endl;
+    cout << (containsNearbyDuplicate(nums2, 2) ? "true" : "false") << endl;
+}
+
+int main()
+{
+    testContainNearbyDuplicate();
+    getchar();
+}
