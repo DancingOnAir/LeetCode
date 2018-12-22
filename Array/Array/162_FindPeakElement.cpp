df5b48dc7deb36a38d029cfa2303da0e95721bc1
DancingOnAir <taotong1984@hotmail.com>
2018-12-22 14:11:36 +0800

    accepted solutionf for q162

---
+++ Array/Array/162_FindPeakElement.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int findPeakElement(vector<int>& nums)
+{
+    for (int i = 1; i < nums.size(); ++i)
+    {
+        if (nums[i] < nums[i - 1])
+            return i - 1;
+    }
+
+
+    return nums.size() - 1;
+}
+
+int helper(const vector<int>& nums, int left, int right)
+{
+    if (left == right)
+        return left;
+    else
+    {
+        int mid1 = left + (right - left) / 2;
+        int mid2 = mid1 + 1;
+
+        if (nums[mid1] > nums[mid2])
+            return helper(nums, left, mid1);
+        else
+            return helper(nums, mid2, right);
+    }
+}
+
+int findPeakElement2(vector<int>& nums)
+{
+    if (nums.size() < 2)
+        return nums.size() - 1;
+
+    return helper(nums, 0, nums.size() - 1);
+}
+
+void testFindPeakElement()
+{
+    vector<int> nums = { 1,2,1,3,5,6,4 };
+    vector<int> nums2 = {};
+    cout << findPeakElement(nums) << endl;
+}
+
+int main()
+{
+    testFindPeakElement();
+    getchar();
+    return 0;
+}
--- Array/Array/287_FindtheDuplicateNumber.cpp
+++ Array/Array/287_FindtheDuplicateNumber.cpp
@@ -1,79 +1,79 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-using namespace std;
-
-int findDuplicate(vector<int>& nums)
-{
-    sort(nums.begin(), nums.end());
-    for (unsigned int i = 0; i < nums.size() - 1; ++i)
-    {
-        if (nums[i] == nums[i + 1])
-            return nums[i];
-    }
-
-    return -1;
-}
-
-int findDuplicate2(vector<int>& nums)
-{
-    if (nums.size() > 1)
-    {
-        int slow = nums[0];
-        int fast = nums[slow];
-
-        while (slow != fast)
-        {
-            slow = nums[slow];
-            fast = nums[nums[fast]];
-        }
-
-        fast = 0;
-        while (slow != fast)
-        {
-            slow = nums[slow];
-            fast = nums[fast];
-        }
-
-        return slow;
-    }
-
-    return -1;
-}
-
-int findDuplicate3(vector<int>& nums)
-{
-    int n = nums.size() - 1;
-    int low = 1;
-    int high = n;
-    int count = 0, mid = 0;
-    while (low < high)
-    {
-        mid = low + (high - low) / 2;
-        count = 0;
-
-        for (auto& x : nums)
-            if (x <= mid)
-                ++count;
-
-        if (count > mid)
-            high = mid;
-        else
-            low = mid + 1;
-    }
-
-    return low;
-}
-
-void testFindDuplicate()
-{
-    vector<int> nums = { 1,3,4,2,2 };
-    cout << findDuplicate3(nums) << endl;
-}
-
-int main()
-{
-    testFindDuplicate();
-    getchar();
-    return 0;
-}
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//using namespace std;
+//
+//int findDuplicate(vector<int>& nums)
+//{
+//    sort(nums.begin(), nums.end());
+//    for (unsigned int i = 0; i < nums.size() - 1; ++i)
+//    {
+//        if (nums[i] == nums[i + 1])
+//            return nums[i];
+//    }
+//
+//    return -1;
+//}
+//
+//int findDuplicate2(vector<int>& nums)
+//{
+//    if (nums.size() > 1)
+//    {
+//        int slow = nums[0];
+//        int fast = nums[slow];
+//
+//        while (slow != fast)
+//        {
+//            slow = nums[slow];
+//            fast = nums[nums[fast]];
+//        }
+//
+//        fast = 0;
+//        while (slow != fast)
+//        {
+//            slow = nums[slow];
+//            fast = nums[fast];
+//        }
+//
+//        return slow;
+//    }
+//
+//    return -1;
+//}
+//
+//int findDuplicate3(vector<int>& nums)
+//{
+//    int n = nums.size() - 1;
+//    int low = 1;
+//    int high = n;
+//    int count = 0, mid = 0;
+//    while (low < high)
+//    {
+//        mid = low + (high - low) / 2;
+//        count = 0;
+//
+//        for (auto& x : nums)
+//            if (x <= mid)
+//                ++count;
+//
+//        if (count > mid)
+//            high = mid;
+//        else
+//            low = mid + 1;
+//    }
+//
+//    return low;
+//}
+//
+//void testFindDuplicate()
+//{
+//    vector<int> nums = { 1,3,4,2,2 };
+//    cout << findDuplicate3(nums) << endl;
+//}
+//
+//int main()
+//{
+//    testFindDuplicate();
+//    getchar();
+//    return 0;
+//}
