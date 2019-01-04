21b14dc6ab3d17cd42d74e21cdbe2a9ca4e94798
DancingOnAir <taotong1984@hotmail.com>
2019-01-04 14:37:04 +0800

    accepted solution for q560

---
+++ Array/Array/560_SubarraySumEqualsK.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+int subarraySum(vector<int>& nums, int k)
+{
+    if (nums.empty())
+        return 0;
+
+    int res = 0;
+    int sum = 0;
+    for (int i = 0; i < nums.size(); ++i)
+    {
+        sum = 0;
+        for (int j = i; j < nums.size(); ++j)
+        {
+            sum += nums[j];
+
+            if (sum == k)
+            {
+                ++res;
+            }
+            else
+                continue;
+        }
+    }
+
+    return res;
+}
+
+int subarraySum2(vector<int>& nums, int k)
+{
+    int res = 0;
+    int sum = 0;
+    unordered_map<int, int> m;
+    m[0]++;
+
+    for (int i = 0; i < nums.size(); ++i)
+    {
+        sum += nums[i];
+
+        if (m.find(sum - k) != m.end())
+            res += m[sum - k];
+
+        m[sum]++;
+    }
+
+    return res;
+}
+
+void testSubarraySum()
+{
+    vector<int> nums = { 1, 1, 1 };
+    int k = 2;
+
+    cout << subarraySum2(nums, k) << endl;
+}
+
+int main()
+{
+    testSubarraySum();
+
+    getchar();
+    return 0;
+}
--- Array/Array/566_ReshapetheMatrix.cpp
+++ Array/Array/566_ReshapetheMatrix.cpp
@@ -1,79 +1,79 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-vector<vector<int>> matrixReshape(vector<vector<int>>& nums, int r, int c)
-{
-    size_t m = nums.size();
-    size_t n = nums[0].size();
-
-    if (m * n != r * c)
-        return nums;
-
-    vector<vector<int>> res(r, vector<int>(c));
-    int x = 0, y = 0;
-    for (int i = 0; i < m; ++i)
-    {
-        for (int j = 0; j < n; ++j)
-        {
-            x = (i * n + j) / c;
-            y = (i * n + j) % c;
-            res[x][y] = nums[i][j];
-        }
-    }
-
-    return res;
-}
-
-vector<vector<int>> matrixReshape2(vector<vector<int>>& nums, int r, int c)
-{
-    size_t m = nums.size();
-    size_t n = nums[0].size();
-
-    if (m * n != r * c)
-        return nums;
-
-    vector<vector<int>> res(r, vector<int>(c));
-    int x = 0, y = 0;
-    int i1 = 0, j1 = 0;
-    int count = n;
-    for (int i = 0; i < r; ++i)
-    {
-        for (int j = 0; j < c; ++j)
-        {
-            res[i][j] = nums[i1][j1];
-            ++j1;
-
-            if (j1 >= count)
-            {
-                j1 = 0;
-                ++i1;
-            }
-        }
-    }
-
-    return res;
-}
-
-void testMatrixReshape()
-{
-    vector<vector<int>> nums = {{1, 2},
-                                {3, 4}};
-    int r = 1, c = 4;
-    auto res = matrixReshape2(nums, r, c);
-
-    for (auto& row : res)
-    {
-        for (auto& col : row)
-            cout << col << ", ";
-        cout << endl;
-    }
-}
-
-int main()
-{
-    testMatrixReshape();
-    getchar();
-    return 0;
-}
+//#include <iostream>
+//#include <vector>
+//
+//using namespace std;
+//
+//vector<vector<int>> matrixReshape(vector<vector<int>>& nums, int r, int c)
+//{
+//    size_t m = nums.size();
+//    size_t n = nums[0].size();
+//
+//    if (m * n != r * c)
+//        return nums;
+//
+//    vector<vector<int>> res(r, vector<int>(c));
+//    int x = 0, y = 0;
+//    for (int i = 0; i < m; ++i)
+//    {
+//        for (int j = 0; j < n; ++j)
+//        {
+//            x = (i * n + j) / c;
+//            y = (i * n + j) % c;
+//            res[x][y] = nums[i][j];
+//        }
+//    }
+//
+//    return res;
+//}
+//
+//vector<vector<int>> matrixReshape2(vector<vector<int>>& nums, int r, int c)
+//{
+//    size_t m = nums.size();
+//    size_t n = nums[0].size();
+//
+//    if (m * n != r * c)
+//        return nums;
+//
+//    vector<vector<int>> res(r, vector<int>(c));
+//    int x = 0, y = 0;
+//    int i1 = 0, j1 = 0;
+//    int count = n;
+//    for (int i = 0; i < r; ++i)
+//    {
+//        for (int j = 0; j < c; ++j)
+//        {
+//            res[i][j] = nums[i1][j1];
+//            ++j1;
+//
+//            if (j1 >= count)
+//            {
+//                j1 = 0;
+//                ++i1;
+//            }
+//        }
+//    }
+//
+//    return res;
+//}
+//
+//void testMatrixReshape()
+//{
+//    vector<vector<int>> nums = {{1, 2},
+//                                {3, 4}};
+//    int r = 1, c = 4;
+//    auto res = matrixReshape2(nums, r, c);
+//
+//    for (auto& row : res)
+//    {
+//        for (auto& col : row)
+//            cout << col << ", ";
+//        cout << endl;
+//    }
+//}
+//
+//int main()
+//{
+//    testMatrixReshape();
+//    getchar();
+//    return 0;
+//}
