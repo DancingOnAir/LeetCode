63328d41bb6a766a161fad9791f8699e2142f646
DancingOnAir <taotong1984@hotmail.com>
2019-01-11 10:46:16 +0800

    accepted solution for q628

--- Array/Array/621_TaskScheduler.cpp
+++ Array/Array/621_TaskScheduler.cpp
@@ -1,76 +1,76 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <unordered_map>
-using namespace std;
-
-int leastInterval(vector<char>& tasks, int n)
-{
-    if (tasks.size() <= 1)
-        return tasks.size();
-
-    int count = 1;
-    int maxValue = 0;
-    int maxCount = 0;
-    int res = 0;
-
-    sort(tasks.begin(), tasks.end());
-
-    for (int i = 1; i < tasks.size(); ++i)
-    {
-        if (tasks[i - 1] != tasks[i])
-        {
-            count = 1;
-        }
-        else
-            ++count;
-
-        if (count > maxValue)
-        {
-            maxValue = count;
-            maxCount = 1;
-        }
-        else if (count == maxValue)
-        {
-            ++maxCount;
-        }
-    }
-
-    res = maxValue + (maxCount - 1) + n * (maxValue - 1);
-    return res > tasks.size()? res : tasks.size();
-}
-
-int leastInterval2(vector<char>& tasks, int n)
-{
-    unordered_map<char, int> m;
-    int count = 0;
-
-    for (char c : tasks)
-    {
-        m[c]++;
-        count = max(count, m[c]);
-    }
-
-    int res = (count - 1) * (n + 1);
-    for (auto& e : m)
-        if (e.second == count)
-            ++res;
-
-    return max(res, (int)tasks.size());
-}
-
-void testLeastInterval()
-{
-    vector<char> tasks = { 'A','A','A','B','B','B' };
-    vector<char> tasks2 = { 'A','B','A' };
-    int n = 2;
-    cout << leastInterval(tasks2, n) << endl;
-}
-
-int main()
-{
-    testLeastInterval();
-
-    getchar();
-    return 0;
-}
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//#include <unordered_map>
+//using namespace std;
+//
+//int leastInterval(vector<char>& tasks, int n)
+//{
+//    if (tasks.size() <= 1)
+//        return tasks.size();
+//
+//    int count = 1;
+//    int maxValue = 0;
+//    int maxCount = 0;
+//    int res = 0;
+//
+//    sort(tasks.begin(), tasks.end());
+//
+//    for (int i = 1; i < tasks.size(); ++i)
+//    {
+//        if (tasks[i - 1] != tasks[i])
+//        {
+//            count = 1;
+//        }
+//        else
+//            ++count;
+//
+//        if (count > maxValue)
+//        {
+//            maxValue = count;
+//            maxCount = 1;
+//        }
+//        else if (count == maxValue)
+//        {
+//            ++maxCount;
+//        }
+//    }
+//
+//    res = maxValue + (maxCount - 1) + n * (maxValue - 1);
+//    return res > tasks.size()? res : tasks.size();
+//}
+//
+//int leastInterval2(vector<char>& tasks, int n)
+//{
+//    unordered_map<char, int> m;
+//    int count = 0;
+//
+//    for (char c : tasks)
+//    {
+//        m[c]++;
+//        count = max(count, m[c]);
+//    }
+//
+//    int res = (count - 1) * (n + 1);
+//    for (auto& e : m)
+//        if (e.second == count)
+//            ++res;
+//
+//    return max(res, (int)tasks.size());
+//}
+//
+//void testLeastInterval()
+//{
+//    vector<char> tasks = { 'A','A','A','B','B','B' };
+//    vector<char> tasks2 = { 'A','B','A' };
+//    int n = 2;
+//    cout << leastInterval(tasks2, n) << endl;
+//}
+//
+//int main()
+//{
+//    testLeastInterval();
+//
+//    getchar();
+//    return 0;
+//}
---
+++ Array/Array/628_MaximumProductofThreeNumbers.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int maximumProduct(vector<int>& nums)
+{
+    size_t n = nums.size();
+
+    sort(nums.begin(), nums.end());
+
+    return max(nums[0] * nums[1] * nums[n - 1], nums[n - 3] * nums[n - 2] * nums[n - 1]);
+}
+
+int maximumProduct2(vector<int>& nums)
+{
+    vector<int> vecMax = { -1001, -1001, -1001 };
+    vector<int> vecMin = { 1001, 1001 };
+
+    for (auto x : nums)
+    {
+        if (x >= vecMax[0])
+        {
+            vecMax[2] = vecMax[1];
+            vecMax[1] = vecMax[0];
+            vecMax[0] = x;
+        }
+        else if (x > vecMax[1])
+        {
+            vecMax[2] = vecMax[1];
+            vecMax[1] = x;
+        }
+        else if (x > vecMax[2])
+        {
+            vecMax[2] = x;
+        }
+
+        if (x <= vecMin[0])
+        {
+            vecMin[1] = vecMax[0];
+            vecMin[0] = x;
+        }
+        else if (x < vecMin[1])
+        {
+            vecMin[1] = x;
+        }
+    }
+
+    return max(vecMax[1] * vecMax[2], vecMin[0] * vecMin[1]) * vecMax[0];
+}
+
+void testMaximumProduct()
+{
+    vector<int> nums = { 1,2,3,4 };
+    cout << maximumProduct2(nums) << endl;
+}
+
+int main()
+{
+    testMaximumProduct();
+
+    getchar();
+    return 0;
+}
