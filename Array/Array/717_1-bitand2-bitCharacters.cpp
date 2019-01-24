3ff25ea2010cc155fac32620880944f5d769fe12
DancingOnAir <taotong1984@hotmail.com>
2019-01-24 09:45:17 +0800

    accepted solution for q717

---
+++ Array/Array/717_1-bitand2-bitCharacters.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool isOneBitCharacter(vector<int>& bits)
+{
+    int n = bits.size();
+    if (0 == n || 1 == bits[n - 1])
+        return false;
+
+    int pos = 0;
+    while (pos < n)
+    {
+        if (bits[pos] == 1)
+            pos += 2;
+        else
+        {
+            ++pos;
+            if (pos == n)
+                return true;
+        }
+
+    }
+
+    return false;
+}
+
+void testIsOneBitCharacter()
+{
+    vector<int> bits = { 1, 1, 1, 0 };
+    vector<int> bits2 = { 1, 0, 0 };
+    cout << (isOneBitCharacter(bits2)?"True":"False") << endl;
+}
+
+int main()
+{
+    testIsOneBitCharacter();
+
+    getchar();
+    return 0;
+}
