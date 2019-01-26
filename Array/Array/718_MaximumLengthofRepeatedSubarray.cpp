a977ffd8da5c3146d896cbe26502ab038eaa01d6
DancingOnAir <taotong1984@hotmail.com>
2019-01-27 00:24:21 +0800

    accepted 1d vector solution for q718

--- Array/Array/718_MaximumLengthofRepeatedSubarray.cpp
+++ Array/Array/718_MaximumLengthofRepeatedSubarray.cpp
@@ -34,12 +34,40 @@ int findLength(vector<int>& A, vector<int>& B)
     return res;
 }
 
+int findLength2(vector<int>& A, vector<int>& B)
+{
+    int m = A.size();
+    int n = B.size();
+
+    if (!m || !n)
+        return 0;
+
+    int res = 0;
+    vector<int> path(n + 1);
+
+    for (int i = 0; i < m; ++i)
+    {
+        for (int j = n; j >= 1; --j)
+        {
+            if (A[i] != B[j - 1])
+                path[j] = 0;
+            else
+                path[j] = path[j - 1] + 1;
+
+            if (res < path[j])
+                res = path[j];
+        }
+    }
+
+    return res;
+}
+
 void testFindLength()
 {
     vector<int> A = { 1,2,3,2,1 };
     vector<int> B = { 3,2,1,4,7 };
 
-    cout << findLength(A, B) << endl;
+    cout << findLength2(A, B) << endl;
 }
 
 int main()
