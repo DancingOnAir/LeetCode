485c6db2b44af039e7236fb691069e245a9b61e1
DancingOnAir <taotong1984@hotmail.com>
2019-04-03 10:36:26 +0800

    feat<354_RussianDollEnvelopes.cpp

--- Array/DynamicProgramming/343_IntegerBreak.cpp
+++ Array/DynamicProgramming/343_IntegerBreak.cpp
@@ -1,45 +1,45 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
-using namespace std;
-
-int integerBreak(int n)
-{
-    if (n < 1)
-        return 0;
-    if (n < 3)
-        return 1;
-    if (n == 3)
-        return 2;
-
-    vector<int> dp(n + 1);
-    dp[1] = 1;
-    dp[2] = 2;
-    dp[3] = 3;
-
-    for (int i = 4; i <= n; ++i)
-    {
-        for (int j = 1; j <= i / 2; ++j)
-        {
-
-            dp[i] = max(dp[i], dp[i - j] * dp[j]);
-        }
-
-    }
-
-    return dp[n];
-}
-
-void testIntegerBreak()
-{
-    int num = 10;
-    cout << integerBreak(num) << endl;
-}
-
-int main()
-{
-    testIntegerBreak();
-    getchar();
-    return 0;
-}
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//
+//using namespace std;
+//
+//int integerBreak(int n)
+//{
+//    if (n < 1)
+//        return 0;
+//    if (n < 3)
+//        return 1;
+//    if (n == 3)
+//        return 2;
+//
+//    vector<int> dp(n + 1);
+//    dp[1] = 1;
+//    dp[2] = 2;
+//    dp[3] = 3;
+//
+//    for (int i = 4; i <= n; ++i)
+//    {
+//        for (int j = 1; j <= i / 2; ++j)
+//        {
+//
+//            dp[i] = max(dp[i], dp[i - j] * dp[j]);
+//        }
+//
+//    }
+//
+//    return dp[n];
+//}
+//
+//void testIntegerBreak()
+//{
+//    int num = 10;
+//    cout << integerBreak(num) << endl;
+//}
+//
+//int main()
+//{
+//    testIntegerBreak();
+//    getchar();
+//    return 0;
+//}
