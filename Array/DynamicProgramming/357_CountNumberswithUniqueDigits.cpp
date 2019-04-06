7a06ae0822a35959421e78b91d2363917a0007d4
DancingOnAir <taotong1984@hotmail.com>
2019-04-06 11:22:25 +0800

    feat<363_MaxSumofRectangleNoLargerThanK.cpp>:accepted solution for q363

--- Array/DynamicProgramming/357_CountNumberswithUniqueDigits.cpp
+++ Array/DynamicProgramming/357_CountNumberswithUniqueDigits.cpp
@@ -1,57 +1,49 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <cmath>
-using namespace std;
-
-int fact(int x)
-{
-    if (1 == x || 0 == x)
-        return 1;
-    else
-        return x * fact(x - 1);
-}
-
-int countNumbersWithUniqueDigits(int n)
-{
-    if (n < 0)
-        return 0;
-
-    vector<int> dp(n + 1);
-    dp[0] = 1;
-    int increase = 0;
-
-    for (int i = 1; i <= n; ++i)
-    {
-        for (int j = 0; j < i; ++j)
-        {
-            if (0 == j)
-                increase = 9;
-            else
-            {
-                if (10 - j < 1)
-                    break;
-
-                increase *= (10 - j);
-            }
-        }
-        dp[i] = dp[i - 1] + increase;
-    }
-
-    return dp[n];
-}
-
-void testCountNumberWithUniqueDigits()
-{
-    int n = 3;
-    cout << countNumbersWithUniqueDigits(2) << endl;
-    cout << countNumbersWithUniqueDigits(n) << endl;
-}
-
-int main()
-{
-    testCountNumberWithUniqueDigits();
-
-    getchar();
-    return 0;
-}
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//#include <cmath>
+//using namespace std;
+//
+//int countNumbersWithUniqueDigits(int n)
+//{
+//    if (n < 0)
+//        return 0;
+//
+//    vector<int> dp(n + 1);
+//    dp[0] = 1;
+//    int increase = 0;
+//
+//    for (int i = 1; i <= n; ++i)
+//    {
+//        for (int j = 0; j < i; ++j)
+//        {
+//            if (0 == j)
+//                increase = 9;
+//            else
+//            {
+//                if (10 - j < 1)
+//                    break;
+//
+//                increase *= (10 - j);
+//            }
+//        }
+//        dp[i] = dp[i - 1] + increase;
+//    }
+//
+//    return dp[n];
+//}
+//
+//void testCountNumberWithUniqueDigits()
+//{
+//    int n = 3;
+//    cout << countNumbersWithUniqueDigits(2) << endl;
+//    cout << countNumbersWithUniqueDigits(n) << endl;
+//}
+//
+//int main()
+//{
+//    testCountNumberWithUniqueDigits();
+//
+//    getchar();
+//    return 0;
+//}
