55c67911ce6d480eeb28efcbea4b8c5c3c0824d9
DancingOnAir <taotong1984@hotmail.com>
2019-05-08 13:29:06 +0800

    feat<466>:accepted solution for q466

--- Array/DynamicProgramming/464_CanIWin.cpp
+++ Array/DynamicProgramming/464_CanIWin.cpp
@@ -1,68 +1,57 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <unordered_set>
-using namespace std;
-
-unordered_set<int> win, lose;
-
-bool canWin(int maxChoosableInteger, int desiredTotal, int usedBits)
-{
-    if (desiredTotal <= 0)
-        return false;
-
-    if (win.find(usedBits) != win.end())
-        return true;
-
-    if (lose.find(usedBits) != lose.end())
-        return false;
-
-    for (int i = 1; i <= maxChoosableInteger; ++i)
-    {
-        int bit = 1 << i;
-        if (usedBits & bit)
-            continue;
-
-        if (!canWin(maxChoosableInteger, desiredTotal - i, usedBits | bit))
-        {
-            win.insert(usedBits);
-            return true;
-        }
-    }
-
-    lose.insert(usedBits);
-    return false;
-}
-
-bool canIWin(int maxChoosableInteger, int desiredTotal)
-{
-
-    int sum = (maxChoosableInteger + 1) * maxChoosableInteger >> 1;
-    
-    if (maxChoosableInteger >= desiredTotal)
-        return true;
-
-    if (sum < desiredTotal)
-        return false;
-
-    if (sum == desiredTotal && maxChoosableInteger & 1)
-        return true;
-
-    return canWin(maxChoosableInteger, desiredTotal, 0);
-}
-
-void testCanIWin()
-{
-    int maxChoosableInteger = 10;
-    int desiredTotal = 11;
-
-    cout << (canIWin(maxChoosableInteger, desiredTotal) ? "True" : "False") << endl;
-}
-
-int main()
-{
-    testCanIWin();
-
-    getchar();
-    return 0;
-}
+//#include <iostream>
+//#include <vector>
+//#include <algorithm>
+//#include <unordered_set>
+//#include <unordered_map>
+//using namespace std;
+//
+//bool canWin(int maxInt, int tot, unordered_map<int, bool>& memo, int usedBits)
+//{
+//    if (tot <= 0)
+//        return false;
+//
+//    if (memo.find(usedBits) != memo.end())
+//        return memo[usedBits];
+//
+//    for (int i = 1; i <= maxInt; ++i)
+//    {
+//        int bit = 1 << i;
+//        if (usedBits & bit)
+//            continue;
+//
+//        if (!canWin(maxInt, tot - i, memo, usedBits | bit))
+//        {
+//            return memo[usedBits] = true;
+//        }
+//    }
+//
+//    return memo[usedBits] = false;
+//}
+//
+//bool canIWin(int maxChoosableInteger, int desiredTotal)
+//{
+//    if (desiredTotal < 2)
+//        return true;
+//
+//    if ((maxChoosableInteger + 1) * maxChoosableInteger >> 1 < desiredTotal)
+//        return false;
+//
+//    unordered_map<int, bool> memo;
+//    return canWin(maxChoosableInteger, desiredTotal, memo, 0);
+//}
+//
+//void testCanIWin()
+//{
+//    int maxChoosableInteger = 10;
+//    int desiredTotal = 11;
+//
+//    cout << (canIWin(maxChoosableInteger, desiredTotal) ? "True" : "False") << endl;
+//}
+//
+//int main()
+//{
+//    testCanIWin();
+//
+//    getchar();
+//    return 0;
+//}
