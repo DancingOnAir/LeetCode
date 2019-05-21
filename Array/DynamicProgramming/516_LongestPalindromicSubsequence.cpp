9a4dfa63879a0119567c74af661261838b09495f
DancingOnAir <taotong1984@hotmail.com>
2019-05-21 11:31:09 +0800

    feat<516>:accepted dp solution for q516

--- Array/DynamicProgramming/516_LongestPalindromicSubsequence.cpp
+++ Array/DynamicProgramming/516_LongestPalindromicSubsequence.cpp
@@ -7,11 +7,25 @@ using namespace std;
 
 int longestPalindromeSubseq(string s)
 {
-    if (s.empty())
-        return 0;
+    int n = s.size();
+    if (n < 2)
+        return n;
 
-    int res = 0;
-    return res;
+    vector<vector<int>> dp(n, vector<int>(n));
+
+    for (int i = n - 1; i >= 0; --i)
+    {
+        dp[i][i] = 1;
+        for (int j = i + 1; j < n; ++j)
+        {
+            if (s[j] == s[i])
+                dp[i][j] = dp[i + 1][j - 1] + 2;
+            else
+                dp[i][j] = max(dp[i + 1][j], dp[i][j - 1]);
+        }
+    }
+
+    return dp[0][n - 1];
 }
 
 void testLongestPalindromeSubseq()
