35c7a3d31c07b5f06309de940cb505d05b753458
DancingOnAir <taotong1984@hotmail.com>
2019-05-23 18:57:36 +0800

    feat<517>:accepted dp solution for 517

--- Array/DynamicProgramming/517_SuperWashingMachines.cpp
+++ Array/DynamicProgramming/517_SuperWashingMachines.cpp
@@ -6,10 +6,35 @@ using namespace std;
 
 int findMinMoves(vector<int>& machines)
 {
-    if (machines.size() < 2)
+    int n = machines.size();
+    if (n < 2)
         return 0;
 
+    vector<int> sum(n + 1);
+    for (int i = 0; i < n; ++i)
+        sum[i + 1] += sum[i] + machines[i];
+
+    if (sum[n] % n)
+        return -1;
+
+    int avg = sum[n] / n;
+    int l = 0, r = 0;
     int res = 0;
+    for (int i = 0; i < n; ++i)
+    {
+        l = i * avg - sum[i];
+        r = (n - i - 1) * avg - (sum[n] - sum[i] - machines[i]);
+
+        if (l > 0 && r > 0)
+        {
+            res = max(res, abs(l) + abs(r));
+        }
+        else
+        {
+            res = max(res, max(abs(l), abs(r)));
+        }
+    }
+
     return res;
 }
 
