1cfbeb3a417fbf10b2bb21f6a94de5462f718d21
DancingOnAir <taotong1984@hotmail.com>
2019-05-25 12:45:02 +0800

    feat<516>:accpted memoriazation solution for q516

--- Array/DynamicProgramming/523_ContinuousSubarraySum.cpp
+++ Array/DynamicProgramming/523_ContinuousSubarraySum.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
-
+#include <unordered_set>
 using namespace std;
 
 bool checkSubarraySum(vector<int>& nums, int k)
@@ -36,6 +36,37 @@ bool checkSubarraySum(vector<int>& nums, int k)
     return false;
 }
 
+bool checkSubarraySum2(vector<int>& nums, int k)
+{
+    int n = nums.size();
+    for (int i = 0; i < n - 1; ++i)
+    {
+        if (nums[i] == 0 && nums[i + 1] == 0)
+            return true;
+    }
+
+    if (0 == k)
+        return false;
+
+    if (k < 0)
+        k = -k;
+
+    unordered_set<int> m;
+    int sum = 0, pre = 0;
+    for (int i = 0; i < n; ++i)
+    {
+        sum += nums[i];
+        int temp = k == 0 ? 0 : (sum % k);
+        if (m.count(temp))
+            return true;
+
+        m.insert(pre);
+        pre = temp;
+    }
+
+    return false;
+}
+
 void testCheckSubarraySum()
 {
     vector<int> nums1 = { 23, 2, 4, 6, 7 };
@@ -44,8 +75,8 @@ void testCheckSubarraySum()
     vector<int> nums2 = { 23, 6, 9 };
     int k2 = 6;
 
-    cout << (checkSubarraySum(nums1, k1) ? "T" : "F") << endl;
-    cout << (checkSubarraySum(nums2, k2) ? "T" : "F") << endl;
+    cout << (checkSubarraySum2(nums1, k1) ? "T" : "F") << endl;
+    cout << (checkSubarraySum2(nums2, k2) ? "T" : "F") << endl;
 }
 
 int main()
