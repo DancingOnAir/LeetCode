eaa2988c53b701194b9aeed53d090f06cc91db60
DancingOnAir <taotong1984@hotmail.com>
2019-05-25 12:40:02 +0800

    feat<516>:accpted brute force solution for q516

--- Array/DynamicProgramming/523_ContinuousSubarraySum.cpp
+++ Array/DynamicProgramming/523_ContinuousSubarraySum.cpp
@@ -6,24 +6,46 @@ using namespace std;
 
 bool checkSubarraySum(vector<int>& nums, int k)
 {
-    int sum = 0;
-    for (int x : nums)
-        sum += x;
+    int n = nums.size();
+    for (int i = 0; i < n - 1; ++i)
+    {
+        if (nums[i] == 0 && nums[i + 1] == 0)
+            return true;
+    }
 
-    if (sum < k)
+    if (0 == k)
         return false;
 
-    bool res = true;
+    if (k < 0)
+        k = -k;
 
-    return true;
+    int sum = 0;
+    vector<vector<int>> dp(n, vector<int>(n));
+    for (int i = 0; i < n; ++i)
+    {
+        sum += nums[i];
+        dp[i][i] = nums[i];
+        for (int j = i + 1; j < n; ++j)
+        {
+            dp[i][j] = dp[i][j - 1] + nums[j];
+            if (dp[i][j] % k == 0)
+                return true;
+        }
+
+    }
+    return false;
 }
 
 void testCheckSubarraySum()
 {
-    vector<int> nums = { 23, 2, 4, 6, 7 };
-    int k = 6;
+    vector<int> nums1 = { 23, 2, 4, 6, 7 };
+    int k1 = 6;
 
-    cout << (checkSubarraySum(nums, k) ? "T" : "F") << endl;
+    vector<int> nums2 = { 23, 6, 9 };
+    int k2 = 6;
+
+    cout << (checkSubarraySum(nums1, k1) ? "T" : "F") << endl;
+    cout << (checkSubarraySum(nums2, k2) ? "T" : "F") << endl;
 }
 
 int main()
