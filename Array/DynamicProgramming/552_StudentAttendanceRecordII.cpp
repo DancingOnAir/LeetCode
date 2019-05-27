e1b862b5ebd2ea33fab55c7a2a2d1557cd1187b3
DancingOnAir <taotong1984@hotmail.com>
2019-05-27 21:58:42 +0800

    feat<552>:init q552

---
+++ Array/DynamicProgramming/552_StudentAttendanceRecordII.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int checkRecord(int n)
+{
+    if (n < 0)
+        return 0;
+
+    int res = 0;
+    return res;
+}
+
+void testCheckRecord()
+{
+    cout << checkRecord(2) << endl;
+}
+
+int main()
+{
+    testCheckRecord();
+
+    getchar();
+    return 0;
+}
