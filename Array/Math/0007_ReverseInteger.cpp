7fe8384e12835b8ca9c376ddedf8d4657ef7f735
DancingOnAir <taotong1984@hotmail.com>
2019-06-02 19:47:25 +0800

    feat<7>:accepted solution for  q7

--- Array/Math/0007_ReverseInteger.cpp
+++ Array/Math/0007_ReverseInteger.cpp
@@ -5,9 +5,19 @@ using namespace std;
 
 int reverse(int x)
 {
+    bool isNegative = false;
+    if (x < 0)
+        isNegative = true;
+
     string s = to_string(x);
-    reverse(s.begin(), s.end());
-    return stoi(s);
+
+    std::reverse(s.begin(), s.end());
+    long long res = isNegative ? -stoll(s) : stoll(s);
+
+    if (res > INT_MAX || res < INT_MIN)
+        return 0;
+
+    return static_cast<int>(res);
 }
 
 void testReverse()
