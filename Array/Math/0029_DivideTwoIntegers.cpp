4133b74d8542486fb3e68b54c659cb4cd453d646
DancingOnAir <taotong1984@hotmail.com>
2019-06-05 19:34:21 +0800

    feat<29>:accepted bit shift solution for q9

--- Array/Math/0029_DivideTwoIntegers.cpp
+++ Array/Math/0029_DivideTwoIntegers.cpp
@@ -5,11 +5,29 @@ using namespace std;
 
 int divide(int dividend, int divisor)
 {
-    if (0 == divisor || 0 == dividend)
-        return 0;
+    if (-1 == divisor || INT_MIN == dividend)
+        return INT_MAX;
 
-    int res = 0;
-    return res;
+    int sign = dividend > 0 ^ divisor > 0 ? -1 : 1;
+
+    long long res = 0;
+    long long dvd = llabs(dividend);
+    long long dvs = llabs(divisor);
+
+    while (dvd >= dvs)
+    {
+        long long temp = dvs, m = 1;
+        while (temp << 1 <= dvd)
+        {
+            temp <<= 1;
+            m <<= 1;
+        }
+
+        dvd -= temp;
+        res += m;
+    }
+
+    return sign * res;
 }
 
 void testDivide()
