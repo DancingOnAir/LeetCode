bde1d53a7a9e5a696ea59dd6bf01dca9f67f45f0
DancingOnAir <taotong1984@hotmail.com>
2019-06-08 23:20:08 +0800

    feat<65>:accepted iter movement solution for q65

--- Array/Math/0065_ValidNumber.cpp
+++ Array/Math/0065_ValidNumber.cpp
@@ -5,19 +5,53 @@ using namespace std;
 
 bool isNumber(string s)
 {
-    if (s.empty())
+    int n = s.size();
+    if (0 == n)
         return false;
 
-    return true;
+    int i = 0;
+    int countNum = 0, countPoint = 0;
+    while (s[i] == ' ')
+        ++i;
+
+    if (s[i] == '+' || s[i] == '-')
+        ++i;
+
+    while (isdigit(s[i]) || s[i] == '.')
+        s[i++] == '.' ? countPoint++ : countNum++;
+
+    if (countPoint > 1 || countNum < 1)
+        return false;
+
+    if (s[i] == 'e')
+    {
+        ++i;
+        countNum = 0;
+        countPoint = 0;
+        if (s[i] == '+' || s[i] == '-')
+            ++i;
+
+        while (isdigit(s[i]) || s[i] == '.')
+            s[i++] == '.' ? countPoint++ : countNum++;
+
+        if (countPoint > 0 || countNum < 1)
+            return false;
+    }
+
+    while (s[i] == ' ')
+        ++i;
+
+    return n == i;
 }
 
 void testIsNumber()
 {
-    assert(isNumber("0") == true);
-    assert(isNumber("0.1") == true);
-    assert(isNumber("abc") == false);
-    assert(isNumber("1 a") == false);
-    assert(isNumber("2e10") == true);
+    //assert(isNumber(".1") == true);
+    //assert(isNumber("0") == true);
+    //assert(isNumber("0.1") == true);
+    //assert(isNumber("abc") == false);
+    //assert(isNumber("1 a") == false);
+    //assert(isNumber("2e10") == true);
     assert(isNumber(" -90e3   ") == true);
     assert(isNumber(" 1e") == false);
     assert(isNumber("e3") == false);
@@ -27,6 +61,8 @@ void testIsNumber()
     assert(isNumber(" --6") == false);
     assert(isNumber("-+3") == false);
     assert(isNumber("95a54e53") == false);
+
+    return;
 }
 
 int main()
