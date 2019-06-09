c0cf76e0cc0533a162104d6f084906a137e55469
DancingOnAir <taotong1984@hotmail.com>
2019-06-09 15:35:55 +0800

    feat<67>:accepted solution for q67

--- Array/Math/0067_AddBinary.cpp
+++ Array/Math/0067_AddBinary.cpp
@@ -5,15 +5,40 @@ using namespace std;
 
 string addBinary(string a, string b)
 {
-    string res("");
     if (a.empty() || b.empty())
-        return res;
+        return "";
+
+    int l1 = a.size();
+    int l2 = b.size();
+
+    if (l1 < l2)
+        return addBinary(b, a);
+
+    string res("");
+    int carry = 0;
+    for (int i = l2 - 1; i >= 0; --i)
+    {
+        int temp = (a[l1 - l2 + i] - '0') + (b[i] - '0') + carry;
+        res = to_string(temp % 2) + res;
+        carry = temp / 2;
+    }
+
+    for (int i = l1 - l2 - 1; i >= 0; --i)
+    {
+        int temp = (a[i] - '0') + carry;
+        res = to_string(temp % 2) + res;
+        carry = temp / 2;
+    }
+
+    if (carry == 1)
+        res = to_string(carry) + res;
 
     return res;
 }
 
 void testAddBinary()
 {
+    cout << addBinary("100", "110010") << endl;
     cout << addBinary("11", "1") << endl;
     cout << addBinary("1010", "1011") << endl;
 }
