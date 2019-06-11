fdcc2d5751f36c811406301af036f0421e13a7b6
DancingOnAir <taotong1984@hotmail.com>
2019-06-11 09:18:26 +0800

    feat<69>:added binary search method

--- Array/Math/0069_Sqrt(x).cpp
+++ Array/Math/0069_Sqrt(x).cpp
@@ -17,13 +17,32 @@ int mySqrt(int x)
     return res;
 }
 
+int mySqrt2(int x)
+{
+    if (x <= 0)
+        return 0;
+
+    double left = 0.0, right = x;
+    double epsilon = 1e-4;
+    while (true)
+    {
+        double mid = left + (right - left) / 2;
+        if ((mid * mid - x) > epsilon)
+            right = mid;
+        else if (mid * mid - x < -epsilon)
+            left = mid;
+        else
+            return mid;
+    }
+}
+
 void testMySqrt()
 {
 
-    cout << mySqrt(2) << endl;
-    cout << mySqrt(3) << endl;
-    cout << mySqrt(4) << endl;
-    cout << mySqrt(8) << endl;
+    cout << mySqrt2(2) << endl;
+    cout << mySqrt2(3) << endl;
+    cout << mySqrt2(4) << endl;
+    cout << mySqrt2(8) << endl;
 }
 
 int main()
