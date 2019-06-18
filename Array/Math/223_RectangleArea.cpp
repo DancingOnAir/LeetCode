c86fccd66b6d8a6cde8621317f5a3ba9d36a71e0
DancingOnAir <taotong1984@hotmail.com>
2019-06-18 23:35:04 +0800

    feat<223>:init q223

---
+++ Array/Math/223_RectangleArea.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int computeArea(int A, int B, int C, int D, int E, int F, int G, int H)
+{
+
+}
+
+void testComputerArea()
+{
+
+}
+
+int main()
+{
+    testComputerArea();
+
+    getchar();
+    return 0;
+}
