bcf8dcc8f3b0d42e5e71033a86c880ff9dd647fb
DancingOnAir <taotong1984@hotmail.com>
2019-06-19 17:56:31 +0800

    feat<223>:accepted solution for q223

--- Array/Math/223_RectangleArea.cpp
+++ Array/Math/223_RectangleArea.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
-
+#include <algorithm>
 using namespace std;
 
 int computeArea(int A, int B, int C, int D, int E, int F, int G, int H)
 {
+    int s1 = (C - A) * (D - B);
+    int s2 = (G - E) * (H - F);
 
+    int left = max(A, E);
+    int right = max(left, min(C, G));
+    int bottom = max(B, F);
+    int top = max(bottom, min(D, H));
+
+    return s1 + s2 - (right - left) * (top - bottom);
 }
 
 void testComputerArea()
 {
-
+    cout << computeArea(-3, 0, 3, 4, 0, -1, 9, 2) << endl;
 }
 
 int main()
