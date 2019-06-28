63b77ba98208a05cf0d9572a65b04a4f0e509fc5
DancingOnAir <taotong1984@hotmail.com>
2019-06-29 07:56:52 +0800

    feat<326>:accepted solution for q326

--- Array/Math/326_PowerofThree.cpp
+++ Array/Math/326_PowerofThree.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
 
 bool isPowerOfThree(int n)
 {
     if (n < 1)
         return false;
-
 
     while (n % 3 == 0)
     {
@@ -19,12 +18,17 @@ bool isPowerOfThree(int n)
     return false;
 }
 
+bool isPowerOfThree2(int n)
+{
+    return (n > 0) && ((int)pow(3, 15) % n == 0);
+}
+
 void testIsPowerOfThree()
 {
-    cout << (isPowerOfThree(27) ? "True" : "False") << endl;
-    cout << (isPowerOfThree(0) ? "True" : "False") << endl;
-    cout << (isPowerOfThree(9) ? "True" : "False") << endl;
-    cout << (isPowerOfThree(45) ? "True" : "False") << endl;
+    cout << (isPowerOfThree2(27) ? "True" : "False") << endl;
+    cout << (isPowerOfThree2(0) ? "True" : "False") << endl;
+    cout << (isPowerOfThree2(9) ? "True" : "False") << endl;
+    cout << (isPowerOfThree2(45) ? "True" : "False") << endl;
 }
 
 int main()
