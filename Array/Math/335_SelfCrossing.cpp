97958f21a0fc594c0e8e4cf4e31c816ea74e12c8
DancingOnAir <taotong1984@hotmail.com>
2019-07-04 11:47:07 +0800

    feat<335>:accepted solution for q335

--- Array/Math/335_SelfCrossing.cpp
+++ Array/Math/335_SelfCrossing.cpp
@@ -4,10 +4,30 @@
 using namespace std;
 
 bool isSelfCrossing(vector<int>& x) {
-    if ((x[0] < x[2]) || (x[1] > x[3]))
+    int len = x.size();
+    if (len < 4)
         return false;
 
-    return true;
+    for (int i = 3; i < len; ++i)
+    {
+        if (x[i] >= x[i - 2] && x[i - 1] <= x[i - 3])
+            return true;
+
+        if (i >= 4)
+        {
+            if (x[i - 1] == x[i - 3] && x[i] + x[i - 4] >= x[i - 2])
+                return true;
+        }
+
+        if (i >= 5)
+        {
+            if (x[i - 2] > x[i - 4] && x[i] + x[i - 4] >= x[i - 2] &&
+                x[i - 1] < x[i - 3] && x[i - 1] + x[i - 5] >= x[i - 3])
+                return true;
+        }
+    }
+
+    return false;
 }
 
 void testIsSelfCrossing()
