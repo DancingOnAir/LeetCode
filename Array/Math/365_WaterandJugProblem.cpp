8aa3fd0fe7f682614229b948f048159d34cc0f26
DancingOnAir <taotong1984@hotmail.com>
2019-07-06 17:06:23 +0800

    feat<365>:accepted gcd solution for q365

--- Array/Math/365_WaterandJugProblem.cpp
+++ Array/Math/365_WaterandJugProblem.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
 #include <vector>
-
+#include <cmath>
 using namespace std;
+
+int gcd(int lhs, int rhs)
+{
+    return rhs ? gcd(rhs, lhs % rhs) : lhs;
+}
 
 bool canMeasureWater(int x, int y, int z)
 {
+    if (x + y < z)
+        return false;
+    
+    if (x == z || y == z || x + y == z)
+        return true;
 
+    return !(z % gcd(x, y));
 }
 
 void testCanMeasureWater()
