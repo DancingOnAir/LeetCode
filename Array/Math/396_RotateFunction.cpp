a3a650c6e9989c3574b124733a2d8356f10cbf2d
DancingOnAir <taotong1984@hotmail.com>
2019-07-10 23:43:51 +0800

    feat<396>:accepted memorization solution for q396

--- Array/Math/396_RotateFunction.cpp
+++ Array/Math/396_RotateFunction.cpp
@@ -3,7 +3,7 @@
 #include <algorithm>
 using namespace std;
 
-int maxRotateFunction(vector<int>& A)
+int maxRotateFunction2(vector<int>& A)
 {
     if (A.empty())
         return 0;
@@ -18,6 +18,28 @@ int maxRotateFunction(vector<int>& A)
             sum += j * A[((-i + len) % len + j) % len];
         }
         res = max(res, sum);
+    }
+
+    return res;
+}
+
+int maxRotateFunction(vector<int>& A)
+{
+    if (A.empty())
+        return 0;
+
+    int f0 = 0, sum = 0;
+    for (int i = 0; i < A.size(); ++i)
+    {
+        f0 += i * A[i];
+        sum += A[i];
+    }
+
+    int res = f0, temp = f0;
+    for (int i = 1; i < A.size(); ++i)
+    {
+        temp += sum - A.size() * A[A.size() - i];
+        res = max(res, temp);
     }
 
     return res;
