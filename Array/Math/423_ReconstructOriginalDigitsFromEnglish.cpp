e571af31186cdfe6f7198caf0f2992156d2e1634
DancingOnAir <taotong1984@hotmail.com>
2019-07-13 14:48:30 +0800

    feat<423>:accepted another solution for q423

--- Array/Math/423_ReconstructOriginalDigitsFromEnglish.cpp
+++ Array/Math/423_ReconstructOriginalDigitsFromEnglish.cpp
@@ -15,7 +15,7 @@ void getSpecificNumber(map<char, int>& m, map<int, int>& res, vector<string>& nu
     }
 }
 
-string originalDigits(string s)
+string originalDigits2(string s)
 {
     vector<string> nums= { "zero", "one", "two", "three", "four", 
         "five", "six", "seven", "eight", "nine" };
@@ -37,7 +37,6 @@ string originalDigits(string s)
     getSpecificNumber(m, res, nums, 5, 'v');
     getSpecificNumber(m, res, nums, 3, 'h');
 
-
     // 3rd check
     getSpecificNumber(m, res, nums, 9, 'i');
     getSpecificNumber(m, res, nums, 1, 'e');
@@ -50,6 +49,29 @@ string originalDigits(string s)
     }
 
     return digits;
+}
+
+string originalDigits(string s) {
+    vector<int> a(10, 0);
+    vector<int> alpha(128, 0);
+    for (char c : s)
+        alpha[c]++;
+    a[0] = alpha['z'];
+    a[2] = alpha['w'];
+    a[4] = alpha['u'];
+    a[6] = alpha['x'];
+    a[8] = alpha['g'];
+    a[3] = alpha['h'] - a[8];
+    a[5] = alpha['f'] - a[4];
+    a[7] = alpha['v'] - a[5];
+    a[1] = alpha['o'] - a[0] - a[2] - a[4];
+    a[9] = alpha['i'] - a[5] - a[6] - a[8];
+    string ans;
+    for (int i = 0; i < 10; i++) {
+        if (a[i] > 0)
+            ans += string(a[i], '0' + i);
+    }
+    return ans;
 }
 
 void testOriginalDigits()
