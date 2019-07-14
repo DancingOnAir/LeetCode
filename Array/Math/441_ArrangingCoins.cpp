7f531275ecad4e648f8da074651129e33d282e86
DancingOnAir <taotong1984@hotmail.com>
2019-07-14 10:50:20 +0800

    feat<441>:accepted solution for q441

--- Array/Math/441_ArrangingCoins.cpp
+++ Array/Math/441_ArrangingCoins.cpp
@@ -2,12 +2,21 @@
 #include <cmath>
 using namespace std;
 
-int arrangeCoins(int n)
+int arrangeCoins2(int n)
 {
     if (n < 1)
         return 0;
 
     return floor(sqrt((double)2 * n + 0.25) - 0.5);
+}
+
+int arrangeCoins(int n)
+{
+    int i = 1;
+    while (n > i)
+        n -= i++;
+
+    return i - 1;
 }
 
 void testArrangeCoins()
