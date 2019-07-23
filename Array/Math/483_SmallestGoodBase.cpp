b8506865622106ecfcef388693c45f9441c00d67
DancingOnAir <taotong1984@hotmail.com>
2019-07-23 15:56:21 +0800

    feat<483>:tried arithmetic progression formula, TLE

--- Array/Math/483_SmallestGoodBase.cpp
+++ Array/Math/483_SmallestGoodBase.cpp
@@ -4,17 +4,37 @@
 
 using namespace std;
 
+int getRemaining(long long num, long long base)
+{
+    int res = log(num) / log(base);
+    return res;
+}
+
 string smallestGoodBase(string n)
 {
     if (n.empty())
         return "";
+
+    long long num = atoll(n.c_str());
+    long long res = 2;
+
+    while (res < num)
+    {
+        long long temp = getRemaining(num, res);
+        if ((pow(res, temp + 1) - 1) / (res - 1) == num)
+            return to_string(res);
+        else
+            ++res;
+    }
+
+    return "";
 }
 
 void testSmallestGoodBase()
 {
     cout << smallestGoodBase("13") << endl;
     cout << smallestGoodBase("4681") << endl;
-    cout << smallestGoodBase("1000000000000000000") << endl;
+    //cout << smallestGoodBase("1000000000000000000") << endl;
 }
 
 int main()
