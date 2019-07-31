5c2fa584f9c8036563ee7d2b47c94ee4105277ca
DancingOnAir <taotong1984@hotmail.com>
2019-07-31 13:41:37 +0800

    feat<592>:accepted split string & gcd, lcm solution for q592

--- Array/Math/592_FractionAdditionandSubtraction.cpp
+++ Array/Math/592_FractionAdditionandSubtraction.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 #include <string>
-
+#include <vector>
 using namespace std;
+
+int gcd(int lhs, int rhs)
+{
+    return rhs == 0 ? lhs : gcd(rhs, lhs % rhs);
+}
+
+int lcm(int lhs, int rhs)
+{
+    return lhs * rhs / gcd(lhs, rhs);
+}
 
 string fractionAddition(string expression)
 {
@@ -9,7 +19,63 @@ string fractionAddition(string expression)
         return "";
 
     string res("");
-    return res;
+
+
+    vector<int> denominators;
+    vector<int> numerators;
+    vector<int> nums;
+    int n = expression.size();
+    int idx = 0;
+    string temp("");
+
+    while (idx < n)
+    {
+        int len = 1;
+        for (; expression[idx + len - 1] != '/'; ++len)
+        {
+            ;
+        }
+
+        temp = expression.substr(idx, len - 1);
+        numerators.emplace_back(stoi(temp));
+
+        idx += len;
+
+
+        len = 1;
+        for (; expression[idx + len - 1] >= '0' &&  expression[idx + len - 1] <= '9'; ++len)
+        {
+
+        }
+
+        temp = expression.substr(idx, len - 1);
+        denominators.emplace_back(stoi(temp));
+        idx += len - 1;
+    }
+
+    int m = numerators.size();
+    if (m == 1)
+        return expression;
+
+    int denominator = 1;
+    for (int i = 0; i < m; ++i)
+    {
+        denominator = lcm(denominator, denominators[i]);
+    }
+
+    int numerator = 0;
+    for (int i = 0; i < m; ++i)
+    {
+        numerator += numerators[i] * denominator / denominators[i];
+    }
+
+    //if (denominator == 0 || numerator == 0)
+    //    return to_string(numerator) + '/' + to_string(denominator);
+
+    int tempGCD = gcd(abs(denominator), abs(numerator));
+    numerator /= tempGCD;
+    denominator /= tempGCD;
+    return to_string(numerator) + '/' + to_string(denominator);
 }
 
 void testFractionAddtion()
@@ -23,8 +89,8 @@ void testFractionAddtion()
     string expression3 = "1/3-1/2";
     cout << fractionAddition(expression3) << endl;
 
-    string expression3 = "5/3+1/3";
-    cout << fractionAddition(expression3) << endl;
+    string expression4 = "5/3+1/3";
+    cout << fractionAddition(expression4) << endl;
 }
 
 int main()
