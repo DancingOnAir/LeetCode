f5268e3869200170774e56542024eef5fbdd35ed
DancingOnAir <taotong1984@hotmail.com>
2019-08-19 16:41:03 +0800

    feat<227>:init q227 & accepted solution for q227

--- Array/String/227_BasicCalculatorII.cpp
+++ Array/String/227_BasicCalculatorII.cpp
@@ -4,52 +4,80 @@
 #include <vector>
 using namespace std;
 
-int calculate(string s) 
+//int calculate2(string s) 
+//{
+//    istringstream iss(s);
+//
+//    vector<int> nums;
+//    vector<char> signs;
+//
+//    while (iss)
+//    {
+//        int num = 0;
+//        char sign = ' ';
+//
+//        iss >> num >> sign;
+//
+//        if (!signs.empty() && (signs.back() == '*' || signs.back() == '/'))
+//        {
+//            int pre = nums.back();
+//            if (signs.back() == '*')
+//            {
+//                num = pre * num;
+//            }
+//            else if (signs.back() == '/')
+//            {
+//                num = pre / num;
+//            }
+//
+//            nums.pop_back();
+//            signs.pop_back();
+//        }
+//
+//        nums.emplace_back(num);
+//        signs.emplace_back(sign);
+//    }
+//
+//    signs.pop_back();
+//
+//    int res = nums[0];
+//    for (int i = 0; i < signs.size(); ++i)
+//    {
+//        if (signs[i] == '+')
+//            res += nums[i + 1];
+//        else
+//            res -= nums[i + 1];
+//    }
+//
+//    return res;
+//}
+
+int calculate(string s)
 {
-    istringstream iss(s);
+    istringstream iss("+" + s + "+");
 
-    vector<int> nums;
-    vector<char> signs;
+    long long total = 0, term = 0, num = 0;
+    char op;
 
-    while (iss)
+    while (iss >> op)
     {
-        int num = 0;
-        char sign = ' ';
-
-        iss >> num >> sign;
-
-        if (!signs.empty() && (signs.back() == '*' || signs.back() == '/'))
+        if (op == '+' || op == '-')
         {
-            int pre = nums.back();
-            if (signs.back() == '*')
-            {
-                num = pre * num;
-            }
-            else if (signs.back() == '/')
-            {
-                num = pre / num;
-            }
-
-            nums.pop_back();
-            signs.pop_back();
+            total += term;
+            iss >> term;
+            term *= 44 - op;
         }
-
-        nums.emplace_back(num);
-        signs.emplace_back(sign);
-    }
-
-    signs.pop_back();
-
-    int res = nums[0];
-    for (int i = 0; i < signs.size(); ++i)
-    {
-        if (signs[i] == '+')
-            res += nums[i + 1];
         else
-            res -= nums[i + 1];
+        {
+            iss >> num;
+            if (op == '*')
+                term *= num;
+            else
+                term /= num;
+        }
     }
 
-    return res;
+    return total;
 }
 
 void testCalculate()
