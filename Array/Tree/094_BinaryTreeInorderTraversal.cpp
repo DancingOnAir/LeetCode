7e9517756194395235ca9a9c4e81bbec181847b1
DancingOnAir <taotong1984@hotmail.com>
2019-09-06 11:13:00 +0800

    fix(094):accepted solution for q094

--- Array/Tree/094_BinaryTreeInorderTraversal.cpp
+++ Array/Tree/094_BinaryTreeInorderTraversal.cpp
@@ -1,61 +1,63 @@
-//#include <iostream>
-//#include <stdlib.h>
-//#include <vector>
-//#include <stack>
-//using namespace std;
-//
-//struct TreeNode
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <string>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr)
+    {
+    }
+};
+
+//void inorder(TreeNode *root, vector<int> &nodes)
 //{
-//	int val;
-//	TreeNode *left;
-//	TreeNode *right;
-//	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-//};
+//	if (!root)
+//		return;
 //
-////void inorder(TreeNode *root, vector<int> &nodes)
-////{
-////	if (!root)
-////		return;
-////
-////	inorder(root->left, nodes);
-////	nodes.push_back(root->val);
-////	inorder(root->right, nodes);
-////}
-////
-////vector<int> inorderTraversal(TreeNode* root) 
-////{
-////	vector<int> results;
-////	inorder(root, results);
-////
-////	return results;
-////}
+//	inorder(root->left, nodes);
+//	nodes.push_back(root->val);
+//	inorder(root->right, nodes);
+//}
+//
+//vector<int> inorderTraversal(TreeNode* root) 
+//{
+//	vector<int> results;
+//	inorder(root, results);
+//
+//	return results;
+//}
 //
 //vector<int> inorderTraversal(TreeNode *root)
 //{
-//	//vector<int> results;
-//	//if (!root)
-//	//	return results;
+//	vector<int> results;
+//	if (!root)
+//		return results;
 //
-//	//stack<TreeNode *> reserve;
-//	//reserve.push(root);
+//	stack<TreeNode *> reserve;
+//	reserve.push(root);
 //
-//	//while (!reserve.empty())
-//	//{
-//	//	TreeNode *p = reserve.top();
-//	//	if (root->left)
-//	//	{
-//	//		reserve.push(p->left);
-//	//		p->left = NULL;
-//	//	}
-//	//	else
-//	//	{
-//	//		results.push_back(p->val);
-//	//		reserve.pop();
+//	while (!reserve.empty())
+//	{
+//		TreeNode *p = reserve.top();
+//		if (root->left)
+//		{
+//			reserve.push(p->left);
+//			p->left = NULL;
+//		}
+//		else
+//		{
+//			results.push_back(p->val);
+//			reserve.pop();
 //
-//	//		if (p->right)
-//	//			reserve.push(p->right);
-//	//	}
-//	//}
+//			if (p->right)
+//				reserve.push(p->right);
+//		}
+//	}
 //
 //	vector<int> results;
 //	stack<TreeNode *> s;
@@ -79,11 +81,87 @@
 //
 //	return results;
 //}
-//
-//int main(void)
-//{
-//
-//
-//	system("pause");
-//	return 0;
-//}
+
+void inorder(TreeNode* root, vector<int>& nums)
+{
+    if (!root)
+        return;
+
+    inorder(root->left, nums);
+    nums.emplace_back(root->val);
+    inorder(root->right, nums);
+}
+
+vector<int> inorderTraversal2(TreeNode* root)
+{
+    if (!root)
+        return vector<int>();
+
+    vector<int> res;
+    inorder(root, res);
+    return res;
+}
+
+vector<int> inorderTraversal(TreeNode* root)
+{
+    if (!root)
+        return vector<int>();
+
+    vector<int> res;
+    stack<TreeNode* > s;
+    TreeNode* pCurrentNode = root;
+    
+    while (!s.empty() || pCurrentNode)
+    {
+        if (pCurrentNode)
+        {
+            s.emplace(pCurrentNode);
+            pCurrentNode = pCurrentNode->left;
+        }
+        else
+        {
+            TreeNode* pTempNode = s.top();
+            s.pop();
+            res.emplace_back(pTempNode->val);
+            pCurrentNode = pTempNode->right;
+        }
+    }
+
+    return res;
+}
+
+TreeNode* createBinaryTree(vector<string>& bt, int pos)
+{
+    if (bt.empty() || bt[pos] == "null")
+    {
+        ++pos;
+        return nullptr;
+    }
+
+    TreeNode* root = new TreeNode(stoi(bt[pos]));
+    return root;
+}
+
+void testInorderTraversal()
+{
+    //vector<string> bt{ "1", "null", "2", "3" };
+    //TreeNode* root = createBinaryTree(bt, 0);
+    TreeNode* root = new TreeNode(1);
+    TreeNode* node1 = new TreeNode(2);
+    TreeNode* node2 = new TreeNode(3);
+    root->right = node1;
+    node1->left = node2;
+
+    vector<int> res = inorderTraversal(root);
+    for (int x : res)
+        cout << x << ", ";
+    cout << endl;
+}
+
+int main()
+{
+    testInorderTraversal();
+
+    getchar();
+    return 0;
+}
--- Array/Tree/508_MostFrequentSubtreeSum.cpp
+++ Array/Tree/508_MostFrequentSubtreeSum.cpp
@@ -1,88 +1,92 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <unordered_map>
-using namespace std;
-
-struct TreeNode
-{
-    TreeNode* left;
-    TreeNode* right;
-    int val;
-
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr)
-    {
-
-    }
-};
-
-int getSum(TreeNode* root, unordered_map<int, int>& counts, int& maxCount)
-{
-    if (!root)
-        return 0;
-
-    int sum = root->val;
-    sum += getSum(root->left, counts, maxCount);
-    sum += getSum(root->right, counts, maxCount);
-
-    ++counts[sum];
-    maxCount = maxCount > counts[sum] ? maxCount : counts[sum];
-
-    return sum;
-}
-
-vector<int> findFrequentTreeSum(TreeNode* root)
-{
-    unordered_map<int, int> counts;
-    int maxCount = INT_MIN;
-    vector<int> res;
-    getSum(root, counts, maxCount);
-
-    for (auto& x : counts)
-    {
-        if (x.second == maxCount)
-            res.emplace_back(x.first);
-    }
-
-    return res;
-}
-
-TreeNode* generateBinaryTree(const vector<string>& bt, int pos)
-{
-    if (bt.empty() || pos >= bt.size() || bt[pos] == "null")
-        return nullptr;
-
-    TreeNode* root = new TreeNode(stoi(bt[pos]));
-    root->left = generateBinaryTree(bt, pos + 1);
-    root->right = generateBinaryTree(bt, pos + 2);
-
-    return root;
-}
-
-void display(const vector<int>& nums)
-{
-    for (int x : nums)
-        cout << x << ", ";
-    cout << endl;
-}
-
-void testFindFrequentTreeSum()
-{
-    vector<string> bt1 = { "5", "2", "-3" };
-    auto root1 = generateBinaryTree(bt1, 0);
-    auto res1 = findFrequentTreeSum(root1);
-    display(res1);
-
-    vector<string> bt2 = { "5", "2", "-5" };
-    auto root2 = generateBinaryTree(bt2, 0);
-    auto res2 = findFrequentTreeSum(root2);
-    display(res2);
-}
-
-int main()
-{
-    testFindFrequentTreeSum();
-
-    getchar();
-    return 0;
-}
+//#include <iostream>
+//#include <vector>
+//#include <string>
+//#include <unordered_map>
+//using namespace std;
+//
+//struct TreeNode
+//{
+//    TreeNode* left;
+//    TreeNode* right;
+//    int val;
+//
+//    TreeNode(int x) : val(x), left(nullptr), right(nullptr)
+//    {
+//
+//    }
+//};
+//
+//int getSum(TreeNode* root, unordered_map<int, int>& counts, int& maxCount)
+//{
+//    if (!root)
+//        return 0;
+//
+//    int sum = root->val;
+//    sum += getSum(root->left, counts, maxCount);
+//    sum += getSum(root->right, counts, maxCount);
+//
+//    ++counts[sum];
+//    maxCount = maxCount > counts[sum] ? maxCount : counts[sum];
+//
+//    return sum;
+//}
+//
+//vector<int> findFrequentTreeSum(TreeNode* root)
+//{
+//    unordered_map<int, int> counts;
+//    int maxCount = INT_MIN;
+//    vector<int> res;
+//    getSum(root, counts, maxCount);
+//
+//    for (auto& x : counts)
+//    {
+//        if (x.second == maxCount)
+//            res.emplace_back(x.first);
+//    }
+//
+//    return res;
+//}
+//
+//int pindex = 0;
+//TreeNode* generateBinaryTree(const vector<string>& bt)
+//{
+//    if (bt[pindex] == "null")
+//    {
+//        ++pindex;
+//        return nullptr;
+//    }
+//
+//    TreeNode* root = new TreeNode(stoi(bt[pindex++]));
+//    root->left = generateBinaryTree(bt);
+//    root->right = generateBinaryTree(bt);
+//
+//    return root;
+//}
+//
+//void display(const vector<int>& nums)
+//{
+//    for (int x : nums)
+//        cout << x << ", ";
+//    cout << endl;
+//}
+//
+//void testFindFrequentTreeSum()
+//{
+//    vector<string> bt1 = { "5", "2", "-3", "null", "null", "null", "null" };
+//    auto root1 = generateBinaryTree(bt1);
+//    auto res1 = findFrequentTreeSum(root1);
+//    display(res1);
+//
+//    vector<string> bt2 = { "5", "2", "-5" };
+//    auto root2 = generateBinaryTree(bt2);
+//    auto res2 = findFrequentTreeSum(root2);
+//    display(res2);
+//}
+//
+//int main()
+//{
+//    testFindFrequentTreeSum();
+//
+//    getchar();
+//    return 0;
+//}
