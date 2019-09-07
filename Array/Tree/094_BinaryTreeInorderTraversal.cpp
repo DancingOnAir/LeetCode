be57d05650b65ce2acf34155aa955c05f8b296c1
DancingOnAir <taotong1984@hotmail.com>
2019-09-07 23:00:23 +0800

    fix(096):accepted solution for q096

--- Array/Tree/094_BinaryTreeInorderTraversal.cpp
+++ Array/Tree/094_BinaryTreeInorderTraversal.cpp
@@ -1,203 +1,130 @@
-#include <iostream>
-#include <vector>
-#include <stack>
-#include <string>
-#include <deque>
-using namespace std;
-
-struct TreeNode
-{
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr)
-    {
-    }
-};
-
-//void inorder(TreeNode *root, vector<int> &nodes)
-//{
-//	if (!root)
-//		return;
+//#include <iostream>
+//#include <vector>
+//#include <stack>
+//#include <string>
+//#include <deque>
+//using namespace std;
 //
-//	inorder(root->left, nodes);
-//	nodes.push_back(root->val);
-//	inorder(root->right, nodes);
+//struct TreeNode
+//{
+//    int val;
+//    TreeNode *left;
+//    TreeNode *right;
+//    TreeNode(int x) : val(x), left(nullptr), right(nullptr)
+//    {
+//    }
+//};
+//
+//void inorder(TreeNode* root, vector<int>& nums)
+//{
+//    if (!root)
+//        return;
+//
+//    inorder(root->left, nums);
+//    nums.emplace_back(root->val);
+//    inorder(root->right, nums);
 //}
 //
-//vector<int> inorderTraversal(TreeNode* root) 
+//vector<int> inorderTraversal2(TreeNode* root)
 //{
-//	vector<int> results;
-//	inorder(root, results);
+//    if (!root)
+//        return vector<int>();
 //
-//	return results;
+//    vector<int> res;
+//    inorder(root, res);
+//    return res;
 //}
 //
-//vector<int> inorderTraversal(TreeNode *root)
+//vector<int> inorderTraversal(TreeNode* root)
 //{
-//	vector<int> results;
-//	if (!root)
-//		return results;
+//    if (!root)
+//        return vector<int>();
 //
-//	stack<TreeNode *> reserve;
-//	reserve.push(root);
+//    vector<int> res;
+//    stack<TreeNode* > s;
+//    TreeNode* pCurrentNode = root;
+//    
+//    while (!s.empty() || pCurrentNode)
+//    {
+//        if (pCurrentNode)
+//        {
+//            s.emplace(pCurrentNode);
+//            pCurrentNode = pCurrentNode->left;
+//        }
+//        else
+//        {
+//            TreeNode* pTempNode = s.top();
+//            s.pop();
+//            res.emplace_back(pTempNode->val);
+//            pCurrentNode = pTempNode->right;
+//        }
+//    }
 //
-//	while (!reserve.empty())
-//	{
-//		TreeNode *p = reserve.top();
-//		if (root->left)
-//		{
-//			reserve.push(p->left);
-//			p->left = NULL;
-//		}
-//		else
-//		{
-//			results.push_back(p->val);
-//			reserve.pop();
-//
-//			if (p->right)
-//				reserve.push(p->right);
-//		}
-//	}
-//
-//	vector<int> results;
-//	stack<TreeNode *> s;
-//	TreeNode *pCurrent = root;
-//
-//	while (!s.empty() || pCurrent)
-//	{
-//		if (pCurrent)
-//		{
-//			s.push(pCurrent);
-//			pCurrent = pCurrent->left;
-//		}
-//		else
-//		{
-//			TreeNode *pNode = s.top();
-//			results.push_back(pNode->val);
-//			s.pop();
-//			pCurrent = pNode->right;
-//		}
-//	}
-//
-//	return results;
+//    return res;
 //}
-
-void inorder(TreeNode* root, vector<int>& nums)
-{
-    if (!root)
-        return;
-
-    inorder(root->left, nums);
-    nums.emplace_back(root->val);
-    inorder(root->right, nums);
-}
-
-vector<int> inorderTraversal2(TreeNode* root)
-{
-    if (!root)
-        return vector<int>();
-
-    vector<int> res;
-    inorder(root, res);
-    return res;
-}
-
-vector<int> inorderTraversal(TreeNode* root)
-{
-    if (!root)
-        return vector<int>();
-
-    vector<int> res;
-    stack<TreeNode* > s;
-    TreeNode* pCurrentNode = root;
-    
-    while (!s.empty() || pCurrentNode)
-    {
-        if (pCurrentNode)
-        {
-            s.emplace(pCurrentNode);
-            pCurrentNode = pCurrentNode->left;
-        }
-        else
-        {
-            TreeNode* pTempNode = s.top();
-            s.pop();
-            res.emplace_back(pTempNode->val);
-            pCurrentNode = pTempNode->right;
-        }
-    }
-
-    return res;
-}
-
-TreeNode* createBinaryTree(const vector<string>& nums)
-{
-    if (nums.empty())
-    {
-        return nullptr;
-    }
-
-    TreeNode* root = new TreeNode(stoi(nums[0]));
-    int nextLevel = 2;
-    int pos = 1;
-    int restNum = nums.size() - 1;
-    deque<TreeNode*> q;
-    TreeNode* pCurrent = nullptr;
-    q.emplace_back(root);
-
-    while (restNum > 0)
-    {
-        for (int i = pos; i < pos + nextLevel; i += 2)
-        {
-            if (i == nums.size())
-                return root;
-
-            pCurrent = q.front();
-            q.pop_front();
-            if (nums[i] != "null")
-            {
-                pCurrent->left = new TreeNode(stoi(nums[i]));
-                q.emplace_back(pCurrent->left);
-            }
-
-            if (i + 1 == nums.size())
-                return root;
-
-            if (nums[i + 1] != "null")
-            {
-                pCurrent->right = new TreeNode(stoi(nums[i + 1]));
-                q.emplace_back(pCurrent->right);
-            }
-        }
-        pos += nextLevel;
-        restNum -= nextLevel;
-        nextLevel = q.size() * 2;
-    }
-
-    return root;
-}
-
-void testInorderTraversal()
-{
-    vector<string> bt{ "1", "null", "2", "3" };
-    TreeNode* root = createBinaryTree(bt);
-    //TreeNode* root = new TreeNode(1);
-    //TreeNode* node1 = new TreeNode(2);
-    //TreeNode* node2 = new TreeNode(3);
-    //root->right = node1;
-    //node1->left = node2;
-
-    vector<int> res = inorderTraversal(root);
-    for (int x : res)
-        cout << x << ", ";
-    cout << endl;
-}
-
-int main()
-{
-    testInorderTraversal();
-
-    getchar();
-    return 0;
-}
+//
+//TreeNode* createBinaryTree(const vector<string>& nums)
+//{
+//    if (nums.empty())
+//    {
+//        return nullptr;
+//    }
+//
+//    TreeNode* root = new TreeNode(stoi(nums[0]));
+//    int nextLevel = 2;
+//    int pos = 1;
+//    int restNum = nums.size() - 1;
+//    deque<TreeNode*> q;
+//    TreeNode* pCurrent = nullptr;
+//    q.emplace_back(root);
+//
+//    while (restNum > 0)
+//    {
+//        for (int i = pos; i < pos + nextLevel; i += 2)
+//        {
+//            if (i == nums.size())
+//                return root;
+//
+//            pCurrent = q.front();
+//            q.pop_front();
+//            if (nums[i] != "null")
+//            {
+//                pCurrent->left = new TreeNode(stoi(nums[i]));
+//                q.emplace_back(pCurrent->left);
+//            }
+//
+//            if (i + 1 == nums.size())
+//                return root;
+//
+//            if (nums[i + 1] != "null")
+//            {
+//                pCurrent->right = new TreeNode(stoi(nums[i + 1]));
+//                q.emplace_back(pCurrent->right);
+//            }
+//        }
+//        pos += nextLevel;
+//        restNum -= nextLevel;
+//        nextLevel = q.size() * 2;
+//    }
+//
+//    return root;
+//}
+//
+//void testInorderTraversal()
+//{
+//    vector<string> bt{ "1", "null", "2", "3" };
+//    TreeNode* root = createBinaryTree(bt);
+//
+//    vector<int> res = inorderTraversal(root);
+//    for (int x : res)
+//        cout << x << ", ";
+//    cout << endl;
+//}
+//
+//int main()
+//{
+//    testInorderTraversal();
+//
+//    getchar();
+//    return 0;
+//}
--- Array/Tree/095_UniqueBinarySearchTreesII.cpp
+++ Array/Tree/095_UniqueBinarySearchTreesII.cpp
@@ -1,16 +1,20 @@
-//#include <iostream>
-//#include <stdlib.h>
-//#include <vector>
-//
-//using namespace std;
-//struct TreeNode
-//{
-//	int val;
-//	TreeNode *left;
-//	TreeNode *right;
-//	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-//};
-//
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr)
+    {
+
+    }
+};
+
 //vector<TreeNode *> generateTrees(int start, int end)
 //{
 //	if (start > end)
@@ -65,18 +69,75 @@
 //	showTree(root->left);
 //	showTree(root->right);
 //}
-//
-//int main(void)
-//{
-//	int numOfNodes = 3;
-//	vector<TreeNode *> result = generateTrees(numOfNodes);
-//
-//	for (auto tree : result)
-//	{
-//		showTree(tree);
-//		cout << endl;
-//	}
-//
-//	system("pause");
-//	return 0;
-//}
+
+vector<TreeNode*> generateTrees(int start, int end)
+{
+    if (start > end)
+        return vector<TreeNode*>{nullptr};
+
+    vector<TreeNode*> res;
+    if (start == end)
+    {
+        TreeNode* p = new TreeNode(start);
+        res.emplace_back(p);
+        return res;
+    }
+
+    for (int i = start; i <= end; ++i)
+    {
+        vector<TreeNode*> left = generateTrees(start, i - 1);
+        vector<TreeNode*> right = generateTrees(i + 1, end);
+
+        for (auto lnode : left)
+        {
+            for (auto rnode : right)
+            {
+                TreeNode* root = new TreeNode(i);
+                root->left = lnode;
+                root->right = rnode;
+
+                res.emplace_back(root);
+            }
+        }
+    }
+
+    return res;
+}
+
+vector<TreeNode*> generateTrees(int n)
+{
+    if (n < 1)
+        return vector<TreeNode*>();
+
+    return generateTrees(1, n);
+}
+
+void preorderTraversal(TreeNode* root)
+{
+    if (!root)
+        return;
+
+    cout << root->val << ", ";
+    preorderTraversal(root->left);
+    preorderTraversal(root->right);
+}
+
+void testGenerateTrees()
+{
+    int numOfNodes = 3;
+    vector<TreeNode*> res = generateTrees(numOfNodes);
+
+    for (auto r : res)
+    {
+        preorderTraversal(r);
+        cout << endl;
+    }
+}
+
+int main()
+{
+    testGenerateTrees();
+
+    getchar();
+    return 0;
+}
