5b608614924c308f8eb8aa86f007240e0f625e2b
DancingOnAir <taotong1984@hotmail.com>
2019-09-08 10:58:51 +0800

    fix(096):accepted solution for q096

--- Array/Tree/095_UniqueBinarySearchTreesII.cpp
+++ Array/Tree/095_UniqueBinarySearchTreesII.cpp
@@ -15,61 +15,6 @@ struct TreeNode
     }
 };
 
-//vector<TreeNode *> generateTrees(int start, int end)
-//{
-//	if (start > end)
-//		return vector<TreeNode *>{nullptr};
-//
-//
-//	vector<TreeNode *> result;
-//	if (start == end)
-//	{
-//		TreeNode *p = new TreeNode(start);
-//		result.push_back(p);
-//		return result;
-//	}
-//	
-//	for (int i = start; i <= end; ++i)
-//	{
-//		vector<TreeNode *> left = generateTrees(start, i - 1);
-//		vector<TreeNode *> right = generateTrees(i + 1, end);
-//
-//		for (auto lnode : left)
-//		{
-//			for (auto rnode : right)
-//			{
-//				TreeNode *root = new TreeNode(i);
-//				root->left = lnode;
-//				root->right = rnode;
-//				result.push_back(root);
-//			}
-//		}
-//	}
-//
-//	return result;
-//}
-//
-//vector<TreeNode*> generateTrees(int n) 
-//{
-//	if (n < 1)
-//		return vector<TreeNode *>();
-//
-//	return generateTrees(1, n);
-//}
-//
-//void showTree(TreeNode *root)
-//{
-//	if (nullptr == root)
-//	{
-//		cout << "null" << " ";
-//		return;
-//	}
-//	cout << root->val << " ";
-//
-//	showTree(root->left);
-//	showTree(root->right);
-//}
-
 vector<TreeNode*> generateTrees(int start, int end)
 {
     if (start > end)
--- Array/Tree/096_UniqueBinarySearchTrees.cpp
+++ Array/Tree/096_UniqueBinarySearchTrees.cpp
@@ -1,8 +1,8 @@
-//#include <iostream>
-//#include <vector>
-//
-//using namespace std;
-//
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
 //int numTrees(int n)
 //{
 //    vector<int> result(n + 1, 0);
@@ -17,12 +17,29 @@
 //    
 //    return result[n];
 //}
-//
-//int main(void)
-//{
-//    int n = 3;
-//    cout << numTrees(n) << endl;
-//
-//    system("pause");
-//    return 0;
-//}
+
+int numTrees(int n)
+{
+    vector<int> res(n + 1);
+    res[0] = res[1] = 1;
+
+    for (int i = 2; i < n + 1; ++i)
+    {
+        for (int j = 1; j <= i; ++j)
+        {
+            res[i] += res[j - 1] * res[i - j];
+        }
+    }
+}
+
+void testNumTrees()
+{
+    int n = 3;
+    cout << numTrees(n) << endl;
+}
+
+int main(void)
+{
+    getchar();
+    return 0;
+}
