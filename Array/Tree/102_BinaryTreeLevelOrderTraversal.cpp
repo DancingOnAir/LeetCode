e02c70a68c5adbf6742a05d6014127be094b2257
DancingOnAir <taotong1984@hotmail.com>
2019-09-14 13:17:41 +0800

    fix(103):accepted solution for q103

--- Array/Tree/102_BinaryTreeLevelOrderTraversal.cpp
+++ Array/Tree/102_BinaryTreeLevelOrderTraversal.cpp
@@ -1,6 +1,8 @@
 //#include <iostream>
 //#include <vector>
+//#include <string>
 //#include <queue>
+//
 //using namespace std;
 //
 //struct TreeNode
@@ -8,213 +10,118 @@
 //    int val;
 //    TreeNode* left;
 //    TreeNode* right;
-//    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//    TreeNode(int x) : val(x), left(nullptr), right(nullptr)
+//    {
+//
+//    }
 //};
 //
 //vector<vector<int>> levelOrder(TreeNode* root)
 //{
-//    vector<vector<int>> result;
-//    if (nullptr == root)
-//        return result;
+//    vector<vector<int>> res;
+//    if (!root)
+//        return res;
 //
-//    queue<TreeNode *> q;
-//    q.push(root);
-//    vector<int> curLevel;
-//    TreeNode* curNode = nullptr;
+//    queue<TreeNode*> q;
+//    q.emplace(root);
+//    TreeNode* pCurrent = nullptr;
 //
 //    while (!q.empty())
 //    {
-//        int l = q.size();
-//        for (auto i = 0; i < l; ++i)
+//        vector<int> nums;
+//        int s = q.size();
+//        while (s > 0)
 //        {
-//            curNode = q.front();
-//            curLevel.push_back(curNode->val);
+//            pCurrent = q.front();
 //            q.pop();
+//            nums.emplace_back(pCurrent->val);
 //
-//            if (curNode->left)
-//                q.push(curNode->left);
-//            if (curNode->right)
-//                q.push(curNode->right);
+//            if (pCurrent->left)
+//                q.emplace(pCurrent->left);
+//
+//            if (pCurrent->right)
+//                q.emplace(pCurrent->right);
+//
+//            --s;
 //        }
 //
-//        result.push_back(curLevel);
-//        curLevel.clear();
+//        res.emplace_back(nums);
 //    }
 //
-//    return result;
+//    return res;
 //}
 //
-//void display(vector<vector<int>>& nums)
+//TreeNode* createBinaryTree(const vector<string>& nums)
 //{
 //    if (nums.empty())
-//        return;
+//        return nullptr;
 //
-//    for (auto row : nums)
+//    TreeNode* root = new TreeNode(stoi(nums[0]));
+//    queue<TreeNode*> q;
+//    q.emplace(root);
+//
+//    TreeNode* pCurrent = nullptr;
+//
+//    int startIndex = 1;
+//    int restIndex = nums.size() - 1;
+//    int nextLevelIndex = 2;
+//
+//    while (restIndex > 0)
 //    {
-//        for (auto col : row)
-//            cout << col << ", ";
+//        for (int i = startIndex; i < startIndex + nextLevelIndex; i += 2)
+//        {
+//            if (i >= nums.size())
+//                return root;
 //
+//            pCurrent = q.front();
+//            q.pop();
+//            if (nums[i] != "null")
+//            {
+//                pCurrent->left = new TreeNode(stoi(nums[i]));
+//                q.emplace(pCurrent->left);
+//            }
+//
+//            if (i + 1 >= nums.size())
+//                return root;
+//
+//            if (nums[i + 1] != "null")
+//            {
+//                pCurrent->right = new TreeNode(stoi(nums[i + 1]));
+//                q.emplace(pCurrent->right);
+//            }
+//        }
+//
+//        startIndex += nextLevelIndex;
+//        restIndex -= nextLevelIndex;
+//        nextLevelIndex = q.size() * 2;
+//    }
+//
+//    return root;
+//}
+//
+//void display(vector<vector<int>>& matrix)
+//{
+//    for (auto& row : matrix)
+//    {
+//        for (auto& col : row)
+//            cout << col << ", ";
 //        cout << endl;
 //    }
 //}
 //
 //void testLevelOrder()
 //{
-//    TreeNode* node1 = new TreeNode(6);
-//    TreeNode* node2 = new TreeNode(2);
-//    TreeNode* node3 = new TreeNode(8);
-//    TreeNode* node4 = new TreeNode(0);
-//    TreeNode* node5 = new TreeNode(4);
-//    TreeNode* node6 = new TreeNode(7);
-//    TreeNode* node7 = new TreeNode(9);
-//    TreeNode* node8 = new TreeNode(3);
-//    TreeNode* node9 = new TreeNode(5);
+//    vector<string> nums1 = { "3", "9", "20", "null", "null", "15", "7" };
+//    TreeNode* root1 = createBinaryTree(nums1);
+//    auto res1 = levelOrder(root1);
+//    display(res1);
 //
-//    node1->left = node2;
-//    node1->right = node3;
-//    node2->left = node4;
-//    node2->right = node5;
-//    node3->left = node6;
-//    node3->right = node7;
-//    node5->left = node8;
-//    node5->right = node9;
-//
-//    vector<vector<int>> result = levelOrder(node1);
-//    display(result);
 //}
 //
 //int main()
 //{
 //    testLevelOrder();
 //
-//    system("pause");
+//    getchar();
 //    return 0;
 //}
-
-#include <iostream>
-#include <vector>
-#include <string>
-#include <queue>
-
-using namespace std;
-
-struct TreeNode
-{
-    int val;
-    TreeNode* left;
-    TreeNode* right;
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr)
-    {
-
-    }
-};
-
-vector<vector<int>> levelOrder(TreeNode* root)
-{
-    vector<vector<int>> res;
-    if (!root)
-        return res;
-
-    queue<TreeNode*> q;
-    q.emplace(root);
-    TreeNode* pCurrent = nullptr;
-
-    while (!q.empty())
-    {
-        vector<int> nums;
-        int s = q.size();
-        while (s > 0)
-        {
-            pCurrent = q.front();
-            q.pop();
-            nums.emplace_back(pCurrent->val);
-
-            if (pCurrent->left != nullptr)
-                q.emplace(pCurrent->left);
-
-            if (pCurrent->right != nullptr)
-                q.emplace(pCurrent->right);
-
-            --s;
-        }
-
-        res.emplace_back(nums);
-    }
-
-    return res;
-}
-
-TreeNode* createBinaryTree(const vector<string>& nums)
-{
-    if (nums.empty())
-        return nullptr;
-
-    TreeNode* root = new TreeNode(stoi(nums[0]));
-    queue<TreeNode*> q;
-    q.emplace(root);
-
-    TreeNode* pCurrent = nullptr;
-
-    int startIndex = 1;
-    int restIndex = nums.size() - 1;
-    int nextLevelIndex = 2;
-
-    while (restIndex > 0)
-    {
-        for (int i = startIndex; i < startIndex + nextLevelIndex; i += 2)
-        {
-            if (i >= nums.size())
-                return root;
-
-            pCurrent = q.front();
-            q.pop();
-            if (nums[i] != "null")
-            {
-                pCurrent->left = new TreeNode(stoi(nums[i]));
-                q.emplace(pCurrent->left);
-            }
-
-            if (i + 1 >= nums.size())
-                return root;
-
-            if (nums[i + 1] != "null")
-            {
-                pCurrent->right = new TreeNode(stoi(nums[i + 1]));
-                q.emplace(pCurrent->right);
-            }
-        }
-
-        startIndex += nextLevelIndex;
-        restIndex -= nextLevelIndex;
-        nextLevelIndex = q.size() * 2;
-    }
-
-    return root;
-}
-
-void display(vector<vector<int>>& matrix)
-{
-    for (auto& row : matrix)
-    {
-        for (auto& col : row)
-            cout << col << ", ";
-        cout << endl;
-    }
-}
-
-void testLevelOrder()
-{
-    vector<string> nums1 = { "3", "9", "20", "null", "null", "15", "7" };
-    TreeNode* root1 = createBinaryTree(nums1);
-    auto res1 = levelOrder(root1);
-    display(res1);
-
-}
-
-int main()
-{
-    testLevelOrder();
-
-    getchar();
-    return 0;
-}
--- Array/Tree/103_BinaryTreeZigzagLevelOrderTraversal.cpp
+++ Array/Tree/103_BinaryTreeZigzagLevelOrderTraversal.cpp
@@ -88,3 +88,136 @@
 //    system("pause");
 //    return 0;
 //}
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr)
+    {
+
+    }
+};
+
+vector<vector<int>> zigzagLevelOrder(TreeNode* root)
+{
+    vector<vector<int>> res;
+    if (!root)
+        return res;
+
+    queue<TreeNode*> s;
+    TreeNode* pCurrent = nullptr;
+    s.emplace(root);
+    bool level = true;
+
+    while (!s.empty())
+    {
+        int len = s.size();
+        vector<int> temp(len);
+        for (int i = 0; i < len; ++i)
+        {
+            pCurrent = s.front();
+            s.pop();
+            
+            if (level)
+                temp[i] = pCurrent->val;
+            else
+                temp[len - i - 1] = pCurrent->val;
+
+            if (pCurrent->left)
+                s.emplace(pCurrent->left);
+            if (pCurrent->right)
+                s.emplace(pCurrent->right);
+        }
+
+        level = !level;
+        res.emplace_back(temp);
+    }
+
+    return res;
+}
+
+TreeNode* createBinaryTree(const vector<string>& nums)
+{
+    if (nums.empty())
+        return nullptr;
+
+    TreeNode* root = new TreeNode(stoi(nums[0]));
+    queue<TreeNode*> q;
+    q.emplace(root);
+    TreeNode* pCurrent = nullptr;
+
+    int startIndex = 1;
+    int nextLevelIndex = 2;
+    int restIndex = nums.size() - 1;
+
+    while (restIndex > 0)
+    {
+        for (int i = startIndex; i < startIndex + nextLevelIndex; i += 2)
+        {
+            if (i == nums.size())
+                return root;
+
+            pCurrent = q.front();
+            q.pop();
+            if (nums[i] != "null")
+            {
+                pCurrent->left = new TreeNode(stoi(nums[i]));
+                q.emplace(pCurrent->left);
+            }
+
+            if (i + 1 == nums.size())
+                return root;
+
+            if (nums[i + 1] != "null")
+            {
+                pCurrent->right = new TreeNode(stoi(nums[i + 1]));
+                q.emplace(pCurrent->right);
+            }
+
+            startIndex += nextLevelIndex;
+            restIndex -= nextLevelIndex;
+            nextLevelIndex = q.size() * 2;
+        }
+    }
+
+    return root;
+}
+
+void display(const vector<vector<int>>& matrix)
+{
+    if (matrix.empty() || matrix[0].empty())
+        return;
+
+    for (auto& row : matrix)
+    {
+        for (auto& col : row)
+            cout << col << ", ";
+        cout << endl;
+    }
+}
+
+void testZigzagLevelOrder()
+{
+    vector<string> nums1 = { "3", "9" ,"20", "null", "null", "15", "7" };
+    auto root1 = createBinaryTree(nums1);
+    auto res1 = zigzagLevelOrder(root1);
+    display(res1);
+}
+
+int main()
+{
+    testZigzagLevelOrder();
+
+    getchar();
+    return 0;
+}
