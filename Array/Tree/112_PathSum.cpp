c3d44d2f33c0e239474e600589c62ac1e20c8c68
DancingOnAir <taotong1984@hotmail.com>
2019-09-22 15:43:04 +0800

    feat(111):accepted solution for q111

--- Array/Tree/112_PathSum.cpp
+++ Array/Tree/112_PathSum.cpp
@@ -86,3 +86,116 @@
 //    system("pause");
 //    return 0;
 //}
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr)
+    {
+
+    }
+};
+
+void helper(TreeNode* root, int target, int& val, vector<int>& res)
+{
+    if (!root->left && !root->right)
+    {
+        res.emplace_back(val + root->val);
+        return;
+    }
+
+    val += root->val;
+    if (root->left)
+        helper(root->left, target, val, res);
+    if (root->right)
+        helper(root->right, target, val, res);
+
+    val -= root->val;
+}
+
+bool hasPathSum(TreeNode* root, int sum)
+{
+    if (!root)
+        return 0 == sum;
+
+    vector<int> res;
+    int val = 0;
+    helper(root, sum, val, res);
+
+    auto iter = find(res.begin(), res.end(), sum);
+    return iter != res.end();
+}
+
+TreeNode* createBinaryTree(const vector<string>& nums)
+{
+    if (nums.empty())
+        return nullptr;
+
+    TreeNode* root = new TreeNode(stoi(nums[0]));
+    queue<TreeNode*> q;
+    q.emplace(root);
+    TreeNode* pCurrent = nullptr;
+
+    int startIndex = 1;
+    int nextLevelIndex = 2;
+    int restIndex = nums.size() - 1;
+
+    while (restIndex > 0)
+    {
+        for (int i = startIndex; i < startIndex + nextLevelIndex; i += 2)
+        {
+            if (i >= nums.size())
+                return root;
+
+            pCurrent = q.front();
+            q.pop();
+
+            if (nums[i] != "null")
+            {
+                pCurrent->left = new TreeNode(stoi(nums[i]));
+                q.emplace(pCurrent->left);
+            }
+
+            if (i + 1 >= nums.size())
+                return root;
+
+            if (nums[i + 1] != "null")
+            {
+                pCurrent->right = new TreeNode(stoi(nums[i + 1]));
+                q.emplace(pCurrent->right);
+            }
+        }
+
+        startIndex += nextLevelIndex;
+        restIndex -= nextLevelIndex;
+        nextLevelIndex = q.size() * 2;
+    }
+
+    return root;
+}
+
+void testHasPathSum()
+{
+    vector<string> nums = { "1", "-2", "-3", "1", "3", "-2", "null", "-1" };
+    vector<string> nums2 = { "5", "4", "8", "11", "null", "13", "4", "7", "2", "null", "null", "null", "1" };
+    TreeNode* root = createBinaryTree(nums);
+    int sum = 22;
+    cout << (hasPathSum(root, sum) ? "True" : "False") << endl;
+}
+
+int main()
+{
+    testHasPathSum();
+
+    getchar();
+    return 0;
+}
