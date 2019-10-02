130cec2bebc9e5b0b03c2c581a3d66c4ed05dd0e
DancingOnAir <taotong1984@hotmail.com>
2019-10-02 23:34:26 +0800

    feat(036):init q036 & accepted solution for q036

--- Array/Tree/617_MergeTwoBinaryTrees.cpp
+++ Array/Tree/617_MergeTwoBinaryTrees.cpp
@@ -18,22 +18,10 @@ struct TreeNode
 
 TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2)
 {
-    if (!t1 && !t2)
-        return nullptr;
+    if (!t1 || !t2)
+        return t1 ? t1 : t2;
 
-    if (t1 && t2)
-    {
-        t1->val += t2->val;
-    }
-    else if (t2)
-    {
-        t1 = new TreeNode(t2->val);
-    }
-    else
-    {
-        t2 = new TreeNode(0);
-    }
-    
+    t1->val += t2->val;
     t1->left = mergeTrees(t1->left, t2->left);
     t1->right = mergeTrees(t1->right, t2->right);
 
