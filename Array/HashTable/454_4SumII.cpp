196e9815728b2803baab94f0b72fc2b6b0b1aaa6
DancingOnAir <taotong1984@hotmail.com>
2019-10-19 13:16:55 +0800

    feat(454): init q454

---
+++ Array/HashTable/454_4SumII.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int fourSumCount(vector<int>& A, vector<int>& B, vector<int>& C, vector<int>& D)
+{
+
+}
+
+void testFourSumCount()
+{
+    vector<int> A = { 1, 2 };
+    vector<int> B = { -2, -1 };
+    vector<int> C = { -1, 2 };
+    vector<int> D = { 0, 2 };
+}
+
+int main()
+{
+    testFourSumCount();
+
+    getchar();
+    return 0;
+}
