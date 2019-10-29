c86ec985381241423cc8d605a08b29c8cc1322c5
DancingOnAir <taotong1984@hotmail.com>
2019-10-29 22:52:28 +0800

    feat(575): accepted bitset solution for q575

--- Array/HashTable/575_DistributeCandies.cpp
+++ Array/HashTable/575_DistributeCandies.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <unordered_set>
+#include <bitset>
+#include <algorithm>
 using namespace std;
 
-int distributeCandies(vector<int>& candies)
+int distributeCandies3(vector<int>& candies)
 {
     if (candies.empty())
         return 0;
@@ -17,6 +20,26 @@ int distributeCandies(vector<int>& candies)
     }
 
     return count.size() > m ? m : count.size();
+}
+
+int distributeCandies2(vector<int>& candies)
+{
+    return min(unordered_set<int>(candies.begin(), candies.end()).size(), candies.size() / 2);
+}
+
+int distributeCandies(vector<int>& candies)
+{
+    bitset<200001> hash;
+    int count = 0;
+    for (int i : candies)
+    {
+        if (!hash.test(i + 100000))
+            ++count;
+        hash.set(i + 100000);
+    }
+
+    int n = candies.size();
+    return min(count, n / 2);
 }
 
 void testDistributeCandies()
