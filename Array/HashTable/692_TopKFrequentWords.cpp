22e5148f1d901506c8e74e803993757f545b7a42
DancingOnAir <taotong1984@hotmail.com>
2019-11-10 14:00:18 +0800

    feat(692): accepted priority queue solution for q692

--- Array/HashTable/692_TopKFrequentWords.cpp
+++ Array/HashTable/692_TopKFrequentWords.cpp
@@ -4,10 +4,11 @@
 #include <unordered_map>
 #include <map>
 #include <set>
+#include <queue>
 
 using namespace std;
 
-vector<string> topKFrequent(vector<string>& words, int k)
+vector<string> topKFrequent2(vector<string>& words, int k)
 {
     unordered_map<string, int> m;
     for (string& word : words)
@@ -33,6 +34,37 @@ vector<string> topKFrequent(vector<string>& words, int k)
     return res;
 }
 
+vector<string> topKFrequent(vector<string>& words, int k)
+{
+    unordered_map<string, int> m;
+    for (auto& word : words)
+        ++m[word];
+
+    auto comp = [&](pair<string, int>& lhs, pair<string, int>& rhs) 
+    {
+        return lhs.second > rhs.second || lhs.second == rhs.second && lhs.first < rhs.first;
+    };
+
+    typedef priority_queue<pair<string, int>, vector<pair<string, int>>, decltype(comp)> my_prioprity_queue_t;
+    my_prioprity_queue_t pq(comp);
+
+    for (auto w : m)
+    {
+        pq.emplace(w.first, w.second);
+        if (pq.size() > k)
+            pq.pop();
+    }
+
+    vector<string> res;
+    while (!pq.empty())
+    {
+        res.insert(res.begin(), pq.top().first);
+        pq.pop();
+    }
+
+    return res;
+}
+
 void display(const vector<string>& words)
 {
     if (words.empty())
@@ -45,15 +77,15 @@ void display(const vector<string>& words)
 
 void testTopKFrequent()
 {
-    //vector<string> words1 = { "i", "love", "leetcode", "i", "love", "coding" };
-    //int k1 = 2;
-    //auto res1 = topKFrequent(words1, k1);
-    //display(res1);
+    vector<string> words1 = { "i", "love", "leetcode", "i", "love", "coding" };
+    int k1 = 3;
+    auto res1 = topKFrequent(words1, k1);
+    display(res1);
 
-    //vector<string> words2 = { "the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is" };
-    //int k2 = 4;
-    //auto res2 = topKFrequent(words2, k2);
-    //display(res2);
+    vector<string> words2 = { "the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is" };
+    int k2 = 4;
+    auto res2 = topKFrequent(words2, k2);
+    display(res2);
 
     vector<string> words3 = {"aaa","aa","a"};
     int k3 = 3;
