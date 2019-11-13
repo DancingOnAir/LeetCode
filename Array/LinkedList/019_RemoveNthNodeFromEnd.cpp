08f5e9b7d8f63146422a72eb16af61f3071837e6
DancingOnAir <taotong1984@hotmail.com>
2019-11-13 11:02:28 +0800

    feat(019):accepted solutionf for q019

--- Array/LinkedList/019_RemoveNthNodeFromEnd.cpp
+++ Array/LinkedList/019_RemoveNthNodeFromEnd.cpp
@@ -79,3 +79,82 @@
 //	system("pause");
 //	return 0;
 //}
+
+#include <iostream>
+
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode* next;
+    ListNode(int x): val(x), next(nullptr) {}
+};
+
+ListNode* removeNthFromEnd(ListNode* head, int n) 
+{
+
+    ListNode* preHead = new ListNode(0);
+    preHead->next = head;
+
+    ListNode* fast = preHead;
+    ListNode* slow = preHead;
+    for (int i = n; i >= 0; --i)
+    {
+        if (!fast)
+            return nullptr;
+
+        fast = fast->next;
+    }
+
+    while (fast)
+    {
+        slow = slow->next;
+        fast = fast->next;
+    }
+
+    slow->next = slow->next->next;
+    return preHead->next;
+}
+
+void display(ListNode* node)
+{
+    if (!node)
+    {
+        cout << endl;
+        return;
+    }
+
+    cout << node->val << "->";
+    display(node->next);
+}
+
+void testRemoveNthFromEnd()
+{
+    ListNode* node6 = new ListNode(1);
+    ListNode* node7 = new ListNode(2);
+    node6->next = node7;
+    auto res2 = removeNthFromEnd(node6, 2);
+    display(res2);
+
+    ListNode* node1 = new ListNode(1);
+    ListNode* node2 = new ListNode(2);
+    ListNode* node3 = new ListNode(3);
+    ListNode* node4 = new ListNode(4);
+    ListNode* node5 = new ListNode(5);
+    node1->next = node2;
+    node2->next = node3;
+    node3->next = node4;
+    node4->next = node5;
+
+    auto res1 = removeNthFromEnd(node1, 2);
+    display(res1);
+}
+
+int main()
+{
+    testRemoveNthFromEnd();
+
+    getchar();
+    return 0;
+}
