bdbec8eee2235fabafa732cca78a8638fdffde94
DancingOnAir <taotong1984@hotmail.com>
2019-12-02 09:57:24 +0800

    feat(203): accepted solution for q203

--- Array/LinkedList/160_IntersectionOfTwoLinkedLists.cpp
+++ Array/LinkedList/160_IntersectionOfTwoLinkedLists.cpp
@@ -1,93 +1,180 @@
+////#include <iostream>
+////
+////struct ListNode
+////{
+////    int val;
+////    ListNode *next;
+////    ListNode(int x) : val(x), next(nullptr) {}
+////};
+////
+////ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) 
+////{
+////    if (nullptr == headA || nullptr == headB)
+////        return nullptr;
+////
+////    ListNode *p1 = headA;
+////    ListNode *p2 = headB;
+////    int m = 0, n = 0;
+////
+////    while (p1 != nullptr || p2 != nullptr)
+////    {
+////        if (p1 != nullptr && p2 != nullptr)
+////        {
+////            ++m;
+////            ++n;
+////
+////            p1 = p1->next;
+////            p2 = p2->next;
+////        }
+////
+////        if (p1 == nullptr && p2 != nullptr)
+////        {
+////            ++n;
+////
+////            p2 = p2->next;
+////        }
+////
+////        if (p1 != nullptr && p2 == nullptr)
+////        {
+////            ++m;
+////            p1 = p1->next;
+////        }
+////    }
+////
+////    int diff = std::abs(m - n);
+////    p1 = headA;
+////    p2 = headB;
+////    if (0 == diff)
+////    {
+////        while (p1 != nullptr && p2 != nullptr)
+////        {
+////            if (p1 == p2)
+////                return p1;
+////
+////            p1 = p1->next;
+////            p2 = p2->next;
+////        }
+////
+////        return nullptr;
+////    }
+////    else
+////    {
+////        if (m > n)
+////        {
+////            while (diff--)
+////                p1 = p1->next;
+////        }
+////        else
+////        {
+////            while (diff--)
+////                p2 = p2->next;
+////        }
+////
+////        while (p1 != nullptr && p2 != nullptr)
+////        {
+////            if (p1 == p2)
+////                return p1;
+////
+////            p1 = p1->next;
+////            p2 = p2->next;
+////        }
+////        return nullptr;
+////    }
+////}
+////
+////ListNode* getIntersectionNode(ListNode* headA, ListNode* headB)
+////{
+////    ListNode *p1 = headA;
+////    ListNode *p2 = headB;
+////
+////    while (p1 != p2)
+////    {
+////        p1 = p1 ? p1->next : headB;
+////        p2 = p2 ? p2->next : headA;
+////    }
+////
+////    return p1;
+////}
+////
+////void testIntersectionNode()
+////{
+////    ListNode *headA = new ListNode(1);
+////    ListNode *node2 = new ListNode(2);
+////    ListNode *node3 = new ListNode(3);
+////    ListNode *node4 = new ListNode(4);
+////    ListNode *node5 = new ListNode(5);
+////
+////    ListNode *headB = new ListNode(6);
+////    ListNode *node7 = new ListNode(7);
+////    ListNode *node8 = new ListNode(8);
+////
+////    headA->next = node2;
+////    node2->next = node3;
+////    node3->next = node4;
+////    node4->next = node5;
+////
+////    headB->next = node7;
+////    node7->next = node8;
+////    node8->next = node3;
+////
+////    ListNode* result = getIntersectionNode(headA, headB);
+////    std::cout << result->val << std::endl;
+////}
+////
+////int main()
+////{
+////    testIntersectionNode();
+////
+////    system("pause");
+////    return 0;
+////}
+//
 //#include <iostream>
+//#include <unordered_set>
+//using namespace std;
 //
 //struct ListNode
 //{
 //    int val;
-//    ListNode *next;
-//    ListNode(int x) : val(x), next(nullptr) {}
+//    ListNode* next;
+//    ListNode(int x): val(x), next(nullptr) {}
 //};
 //
-//ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) 
+//ListNode *getIntersectionNode2(ListNode *headA, ListNode *headB)
 //{
-//    if (nullptr == headA || nullptr == headB)
+//    if (!headA || !headB)
 //        return nullptr;
 //
-//    ListNode *p1 = headA;
-//    ListNode *p2 = headB;
-//    int m = 0, n = 0;
+//    unordered_set<ListNode*> s;
+//    auto head1 = headA;
+//    auto head2 = headB;
 //
-//    while (p1 != nullptr || p2 != nullptr)
+//    while (head1)
 //    {
-//        if (p1 != nullptr && p2 != nullptr)
-//        {
-//            ++m;
-//            ++n;
-//
-//            p1 = p1->next;
-//            p2 = p2->next;
-//        }
-//
-//        if (p1 == nullptr && p2 != nullptr)
-//        {
-//            ++n;
-//
-//            p2 = p2->next;
-//        }
-//
-//        if (p1 != nullptr && p2 == nullptr)
-//        {
-//            ++m;
-//            p1 = p1->next;
-//        }
+//        s.emplace(head1);
+//        head1 = head1->next;
 //    }
 //
-//    int diff = std::abs(m - n);
-//    p1 = headA;
-//    p2 = headB;
-//    if (0 == diff)
+//    while (head2)
 //    {
-//        while (p1 != nullptr && p2 != nullptr)
-//        {
-//            if (p1 == p2)
-//                return p1;
+//        if (s.count(head2))
+//            return head2;
 //
-//            p1 = p1->next;
-//            p2 = p2->next;
-//        }
-//
-//        return nullptr;
+//        head2 = head2->next;
 //    }
-//    else
-//    {
-//        if (m > n)
-//        {
-//            while (diff--)
-//                p1 = p1->next;
-//        }
-//        else
-//        {
-//            while (diff--)
-//                p2 = p2->next;
-//        }
 //
-//        while (p1 != nullptr && p2 != nullptr)
-//        {
-//            if (p1 == p2)
-//                return p1;
-//
-//            p1 = p1->next;
-//            p2 = p2->next;
-//        }
-//        return nullptr;
-//    }
+//    return nullptr;
 //}
 //
-//ListNode* getIntersectionNode(ListNode* headA, ListNode* headB)
+//ListNode *getIntersectionNode(ListNode *headA, ListNode *headB)
 //{
-//    ListNode *p1 = headA;
-//    ListNode *p2 = headB;
+//    if (!headA || !headB)
+//        return nullptr;
 //
-//    while (p1 != p2)
+//    auto p1 = headA;
+//    auto p2 = headB;
+//
+//    while(p1 != p2)
 //    {
 //        p1 = p1 ? p1->next : headB;
 //        p2 = p2 ? p2->next : headA;
@@ -96,17 +183,17 @@
 //    return p1;
 //}
 //
-//void testIntersectionNode()
+//void testGetIntersectionNode()
 //{
-//    ListNode *headA = new ListNode(1);
-//    ListNode *node2 = new ListNode(2);
-//    ListNode *node3 = new ListNode(3);
+//    ListNode *headA = new ListNode(4);
+//    ListNode *node2 = new ListNode(1);
+//    ListNode *node3 = new ListNode(8);
 //    ListNode *node4 = new ListNode(4);
 //    ListNode *node5 = new ListNode(5);
 //
-//    ListNode *headB = new ListNode(6);
-//    ListNode *node7 = new ListNode(7);
-//    ListNode *node8 = new ListNode(8);
+//    ListNode *headB = new ListNode(5);
+//    ListNode *node7 = new ListNode(0);
+//    ListNode *node8 = new ListNode(1);
 //
 //    headA->next = node2;
 //    node2->next = node3;
@@ -123,95 +210,8 @@
 //
 //int main()
 //{
-//    testIntersectionNode();
+//    testGetIntersectionNode();
 //
-//    system("pause");
+//    getchar();
 //    return 0;
 //}
-
-#include <iostream>
-#include <unordered_set>
-using namespace std;
-
-struct ListNode
-{
-    int val;
-    ListNode* next;
-    ListNode(int x): val(x), next(nullptr) {}
-};
-
-ListNode *getIntersectionNode2(ListNode *headA, ListNode *headB)
-{
-    if (!headA || !headB)
-        return nullptr;
-
-    unordered_set<ListNode*> s;
-    auto head1 = headA;
-    auto head2 = headB;
-
-    while (head1)
-    {
-        s.emplace(head1);
-        head1 = head1->next;
-    }
-
-    while (head2)
-    {
-        if (s.count(head2))
-            return head2;
-
-        head2 = head2->next;
-    }
-
-    return nullptr;
-}
-
-ListNode *getIntersectionNode(ListNode *headA, ListNode *headB)
-{
-    if (!headA || !headB)
-        return nullptr;
-
-    auto p1 = headA;
-    auto p2 = headB;
-
-    while(p1 != p2)
-    {
-        p1 = p1 ? p1->next : headB;
-        p2 = p2 ? p2->next : headA;
-    }
-
-    return p1;
-}
-
-void testGetIntersectionNode()
-{
-    ListNode *headA = new ListNode(4);
-    ListNode *node2 = new ListNode(1);
-    ListNode *node3 = new ListNode(8);
-    ListNode *node4 = new ListNode(4);
-    ListNode *node5 = new ListNode(5);
-
-    ListNode *headB = new ListNode(5);
-    ListNode *node7 = new ListNode(0);
-    ListNode *node8 = new ListNode(1);
-
-    headA->next = node2;
-    node2->next = node3;
-    node3->next = node4;
-    node4->next = node5;
-
-    headB->next = node7;
-    node7->next = node8;
-    node8->next = node3;
-
-    ListNode* result = getIntersectionNode(headA, headB);
-    std::cout << result->val << std::endl;
-}
-
-int main()
-{
-    testGetIntersectionNode();
-
-    getchar();
-    return 0;
-}
--- Array/LinkedList/203_RemoveLinkedListElements.cpp
+++ Array/LinkedList/203_RemoveLinkedListElements.cpp
@@ -73,3 +73,80 @@
 //    system("pause");
 //    return 0;
 //}
+
+#include <iostream>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode* next;
+    ListNode(int x): val(x), next(nullptr) {}
+};
+
+ListNode* removeElements(ListNode* head, int val)
+{
+    if (!head)
+        return nullptr;
+
+    ListNode preHead = ListNode(0);
+    preHead.next = head;
+
+    auto pre = &preHead;
+    auto cur = head;
+
+    while (cur)
+    {
+        if (cur->val == val)
+        {
+            pre->next = cur->next;
+            cur = cur->next;
+        }
+        else
+        {
+            pre = pre->next;
+            cur = cur->next;
+        }
+    }
+
+    return preHead.next;
+}
+
+void display(ListNode* head)
+{
+    while (head)
+    {
+        cout << head->val << "->";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+void testRemoveElements()
+{
+    ListNode *head1 = new ListNode(1);
+    ListNode *node2 = new ListNode(2);
+    ListNode *node3 = new ListNode(6);
+    ListNode *node4 = new ListNode(3);
+    ListNode *node5 = new ListNode(4);
+    ListNode *node6 = new ListNode(5);
+    ListNode *node7 = new ListNode(6);
+
+    head1->next = node2;
+    node2->next = node3;
+    node3->next = node4;
+    node4->next = node5;
+    node5->next = node6;
+    node6->next = node7;
+
+    auto res1 = removeElements(head1, 6);
+    display(res1);
+}
+
+int main()
+{
+    testRemoveElements();
+
+    getchar();
+    return 0;
+}
