69c5c180b3616f1cb7af94cc6875df14188e7d88
DancingOnAir <taotong1984@hotmail.com>
2019-12-05 14:23:24 +0800

    feat(445):accepted stack solutioin for q445

--- Array/LinkedList/445_AddTwoNumbersII.cpp
+++ Array/LinkedList/445_AddTwoNumbersII.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <stack>
 using namespace std;
 
 struct ListNode
@@ -29,13 +29,43 @@ ListNode* addTwoNumbers(ListNode* l1, ListNode* l2)
     if (!l2)
         return l1;
 
+    stack<int> s1, s2;
+    for (; l1; l1 = l1->next)
+        s1.emplace(l1->val);
+    for (; l2; l2 = l2->next)
+        s2.emplace(l2->val);
+
+    ListNode* res = new ListNode(0);
+    ListNode* tmp = nullptr;
+    for (int sum = 0; !s1.empty() || !s2.empty(); tmp = new ListNode(sum /= 10), tmp->next = res, res = tmp)
+    {
+        if (!s1.empty())
+            sum += s1.top(), s1.pop();
+
+        if (!s2.empty())
+            sum += s2.top(), s2.pop();
+
+        res->val = sum % 10;
+    }
+
+    return res->val ? res : res->next;
+}
+
+ListNode* addTwoNumbers2(ListNode* l1, ListNode* l2)
+{
+    if (!l1)
+        return l2;
+
+    if (!l2)
+        return l1;
+
     int v1 = 0, v2 = 0, val = 0, extra = 0;
     auto newL1 = reverse(l1);
     auto newL2 = reverse(l2);
     ListNode preHead = ListNode(0);
     auto p = &preHead;
 
-    while (newL1 || newL2)
+    while (newL1 || newL2 || extra)
     {
         v1 = newL1 ? newL1->val : 0;
         v2 = newL2 ? newL2->val : 0;
