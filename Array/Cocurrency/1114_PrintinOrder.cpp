5e67a658105dbef9e899bb1a7148952e6abb341b
DancingOnAir <taotong1984@hotmail.com>
2019-12-08 18:39:02 +0800

    feat(1114):init q1114 & accepted condition_variable solution for q1114

---
+++ Array/Cocurrency/1114_PrintinOrder.cpp
@@ -0,0 +1,108 @@
+#include <functional>
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Foo {
+public:
+    Foo(): count(1)
+    {
+
+    }
+
+    void first(function<void()> printFirst)
+    {
+        unique_lock<mutex> lck(mtx);
+
+        // printFirst() outputs "first". Do not change or remove this line.
+        printFirst();
+        count = 2;
+        cv.notify_all();
+    }
+
+    void second(function<void()> printSecond)
+    {
+        unique_lock<mutex> lck(mtx);
+        while (count != 2)
+            cv.wait(lck);
+        // printSecond() outputs "second". Do not change or remove this line.
+        printSecond();
+        count = 3;
+        cv.notify_all();
+    }
+
+    void third(function<void()> printThird) 
+    {
+        unique_lock<mutex> lck(mtx);
+        while (count != 3)
+            cv.wait(lck);
+        // printThird() outputs "third". Do not change or remove this line.
+        printThird();
+    }
+
+private:
+    mutex mtx;
+    condition_variable cv;
+    int count;
+};
+
+void printFirst()
+{
+    cout << "fisrt" << endl;
+}
+
+void printSecond()
+{
+    for (int i = 0; i < 5; ++i)
+    {
+        cout << "second" << endl;
+    }
+}
+
+void printThird()
+{
+    for (int i = 0; i < 5; ++i)
+    {
+        cout << "third" << endl;
+    }
+}
+
+void test()
+{
+    vector<int> nums = { 1, 3, 2 };
+    Foo foo;
+
+    for (auto num : nums)
+    {
+        if (1 == num)
+        {
+            auto th1 = thread(foo.first, printFirst);
+        }
+
+        if (2 == num)
+        {
+            th2 = thread(printSecond);
+        }
+
+        if (3 == num)
+        {
+            th3 = thread(printThird);
+        }
+    }
+
+    th1.join();
+    th2.join();
+    th3.join();
+    cout << "main" << endl;
+}
+
+int main()
+{
+    test();
+
+    getchar();
+    return 0;
+}
