b569c69b6b84f359b01c4005d96e35c8a19e826c
DancingOnAir <taotong1984@hotmail.com>
2019-12-11 17:32:56 +0800

    feat(1116):accepted promise solution for q1116

--- Array/Cocurrency/1116_PrintZeroEvenOdd.cpp
+++ Array/Cocurrency/1116_PrintZeroEvenOdd.cpp
@@ -5,19 +5,19 @@
 
 using namespace std;
 
-class ZeroEvenOdd {
+class ZeroEvenOdd2 {
 private:
     int n;
     condition_variable cv;
     mutex mtx;
-    bool isZero;
+    int isZero;
     bool isEven;
 
 public:
-    ZeroEvenOdd(int n)
+    ZeroEvenOdd2(int n)
     {
         this->n = n;
-        this->isZero = true;
+        this->isZero = 1;
         this->isEven = false;
     }
 
@@ -29,12 +29,12 @@ public:
         for (int i = 0; i < n; ++i)
         {
             //cv.wait(lck, [&] { return isZero; });
-            while (!isZero)
+            while (isZero != 1)
                 cv.wait(lck);
 
             printNumber(0);
 
-            isZero = false;
+            isZero = 0;
             cv.notify_all();
         }
     }
@@ -46,12 +46,12 @@ public:
         for (int i = 2; 2 <= n; i += 2)
         {
             //cv.wait(lck, [&] { return (!isZero && isEven); });
-            while (isZero || !isEven)
+            while (isZero == 1 || !isEven)
                 cv.wait(lck);
             printNumber(i);
 
             isEven = false;
-            isZero = true;
+            isZero = 1;
             cv.notify_all();
         }
     }
@@ -63,14 +63,71 @@ public:
         for (int i = 1; i <=n; i += 2)
         {
             //cv.wait(lck, [&] { return (!isZero && !isEven); });
-            while (isZero || isEven)
+            while (isZero == 1 || isEven)
                 cv.wait(lck);
 
             printNumber(i);
-            isZero = true;
+            isZero = 1;
             isEven = true;
             cv.notify_all();
         }
+    }
+
+    thread threadZero(function<void(int)> printNumber)
+    {
+        return thread([=] { return zero(printNumber); });
+    }
+
+    thread threadEven(function<void(int)> printNumber)
+    {
+        return thread([=] { return even(printNumber); });
+    }
+
+    thread threadOdd(function<void(int)> printNumber)
+    {
+        return thread([=] {return odd(printNumber); });
+    }
+};
+
+#include <future>
+
+class ZeroEvenOdd {
+private:
+    int n;
+    promise<function<void(int)>> promOdd;
+    promise<function<void(int)>> promEven;
+
+public:
+    ZeroEvenOdd(int n)
+    {
+        this->n = n;
+    }
+
+    // printNumber(x) outputs "x", where x is an integer.
+    void zero(function<void(int)> printNumber)
+    {
+        auto eve = promEven.get_future().get();
+        auto odd = promOdd.get_future().get();
+
+        for (int i = 1; i <= n; ++i)
+        {
+            printNumber(0);
+
+            if (i & 1)
+                odd(i);
+            else
+                eve(i);
+        }
+    }
+
+    void even(function<void(int)> printNumber)
+    {
+        promEven.set_value(printNumber);
+    }
+
+    void odd(function<void(int)> printNumber)
+    {
+        promOdd.set_value(printNumber);
     }
 
     thread threadZero(function<void(int)> printNumber)
@@ -98,7 +155,7 @@ void testZeroEvenOdd()
 {
     function<void(int)> f = printNumber;
     
-    ZeroEvenOdd* p = new ZeroEvenOdd(2);
+    ZeroEvenOdd* p = new ZeroEvenOdd(6);
     thread A, B, C;
     A = p->threadZero(f);
     B = p->threadEven(f);
