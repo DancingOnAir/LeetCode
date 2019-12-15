cd3afe02e446be93c5b121c463798466d75b8615
DancingOnAir <taotong1984@hotmail.com>
2019-12-15 23:06:56 +0800

    feat(1195):condition variable solution for q1195

--- Array/Cocurrency/1195_FizzBuzzMultithreaded.cpp
+++ Array/Cocurrency/1195_FizzBuzzMultithreaded.cpp
@@ -2,52 +2,160 @@
 #include <vector>
 #include <thread>
 #include <future>
-
+#include <mutex>
+#include <condition_variable>
 using namespace std;
+
+//class FizzBuzz {
+//private:
+//    int n;
+//    promise<function<void()>> promFizz, promBuzz, promFizzBuzz;
+//    promise<function<void(int)>> promNumber;
+//
+//public:
+//    FizzBuzz(int n) {
+//        this->n = n;
+//    }
+//
+//    // printFizz() outputs "fizz".
+//    void fizz(function<void()> printFizz) {
+//        promFizz.set_value(printFizz);
+//    }
+//
+//    // printBuzz() outputs "buzz".
+//    void buzz(function<void()> printBuzz) {
+//        promBuzz.set_value(printBuzz);
+//    }
+//
+//    // printFizzBuzz() outputs "fizzbuzz".
+//    void fizzbuzz(function<void()> printFizzBuzz) {
+//        promFizzBuzz.set_value(printFizzBuzz);
+//    }
+//
+//    // printNumber(x) outputs "x", where x is an integer.
+//    void number(function<void(int)> printNumber) {
+//        auto f = promFizz.get_future().get();
+//        auto b = promBuzz.get_future().get();
+//        auto fb = promFizzBuzz.get_future().get();
+//
+//        for (int i = 1; i <= n; ++i)
+//        {
+//            if (i % 5 == 0 && i % 3 == 0)
+//                fb();
+//            else if (i % 3 == 0)
+//                f();
+//            else if (i % 5 == 0)
+//                b();
+//            else
+//                printNumber(i);
+//        }
+//    }
+//
+//    thread threadPrintFizz(function<void()> printFizz)
+//    {
+//        return thread([=] { return fizz(printFizz); });
+//    }
+//
+//    thread threadPrintBuzz(function<void()> printBuzz)
+//    {
+//        return thread([=] { return buzz(printBuzz); });
+//    }
+//
+//    thread threadPrintFizzBuzz(function<void()> printFizzBuzz)
+//    {
+//        return thread([=] {return fizzbuzz(printFizzBuzz); });
+//    }
+//
+//    thread threadPrintNumber(function<void(int)> printNumber)
+//    {
+//        return thread([=] {return number(printNumber); });
+//    }
+//};
 
 class FizzBuzz {
 private:
     int n;
-    promise<function<void()>> promFizz, promBuzz, promFizzBuzz;
-    promise<function<void(int)>> promNumber;
+    int count;
+    mutex mtx;
+    condition_variable cv;
 
 public:
     FizzBuzz(int n) {
         this->n = n;
+        this->count = 1;
     }
 
     // printFizz() outputs "fizz".
     void fizz(function<void()> printFizz) {
-        promFizz.set_value(printFizz);
+        while (true)
+        {
+            unique_lock<mutex> lck(mtx);
+            while (count <= n && (count % 3 != 0 || count % 5 == 0))
+                cv.wait(lck);
+
+            if (count > n)
+                return;
+
+            printFizz();
+            ++count;
+
+            cv.notify_all();
+        }
     }
 
     // printBuzz() outputs "buzz".
     void buzz(function<void()> printBuzz) {
-        promBuzz.set_value(printBuzz);
+        while (true)
+        {
+            unique_lock<mutex> lck(mtx);
+
+            while (count <= n && (count % 5 != 0 || count % 3 == 0))
+                cv.wait(lck);
+
+            if (count > n)
+                return;
+
+            printBuzz();
+            ++count;
+
+            cv.notify_all();
+        }
     }
 
     // printFizzBuzz() outputs "fizzbuzz".
     void fizzbuzz(function<void()> printFizzBuzz) {
-        promFizzBuzz.set_value(printFizzBuzz);
+        while (true)
+        {
+            unique_lock<mutex> lck(mtx);
+            while (count <= n && (count % 3 != 0 || count % 5 != 0))
+                cv.wait(lck);
+
+            if (count > n)
+                return;
+
+            printFizzBuzz();
+            ++count;
+
+            cv.notify_all();
+        }
     }
 
     // printNumber(x) outputs "x", where x is an integer.
     void number(function<void(int)> printNumber) {
-        auto f = promFizz.get_future().get();
-        auto b = promBuzz.get_future().get();
-        auto fb = promFizzBuzz.get_future().get();
 
-        for (int i = 1; i <= n; ++i)
+        while (true)
         {
-            if (i % 5 == 0 && i % 3 == 0)
-                fb();
-            else if (i % 3 == 0)
-                f();
-            else if (i % 5 == 0)
-                b();
-            else
-                printNumber(i);
+            unique_lock<mutex> lck(mtx);
+            while (count <= n && (count % 3 == 0 || count % 5 == 0))
+                cv.wait(lck);
+
+            if (count > n)
+                return;
+                
+            printNumber(count++);
+            cv.notify_all();
         }
+
     }
 
     thread threadPrintFizz(function<void()> printFizz)
