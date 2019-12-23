7e4963062cf9144885150982561ca74da6ec432c
DancingOnAir <taotong1984@hotmail.com>
2019-12-23 10:09:29 +0800

    feat(587):accepted monotone chain solution for q587

--- Array/Geometry/0587_ErecttheFence.cpp
+++ Array/Geometry/0587_ErecttheFence.cpp
@@ -15,29 +15,41 @@ int cross(const vector<int>& o, const vector<int>& a, const vector<int>& b)
     return (a[0] - o[0]) * (b[1] - o[1]) - (a[1] - o[1]) * (b[0] - o[0]);
 }
 
-bool cmp(const vector<int>& lhs, const vector<int>& rhs)
-{
-    return (lhs[0] < rhs[0]) || (lhs[0] == rhs[0] && lhs[1] < rhs[1]);
-}
-
 vector<vector<int>> outerTrees(vector<vector<int>>& points)
 {
     int n = points.size();
     if (n < 4)
     return points;
 
-    sort(points.begin(), points.end(), cmp);
+    sort(points.begin(), points.end(), [&](const vector<int> lhs, const vector<int>& rhs) { 
+        return lhs[0] < rhs[0] || lhs[0] == rhs[0] && lhs[1] < rhs[1]; });
     int count = 0;
     
     vector<vector<int>> res;
     for (int i = 0; i < n; ++i)
     {
-        while (count >= 2 && cross(points[count - 2], points[count - 1], points[i]) <= 0)
-            --count;
-        res[count].emplace_back(points[i]);
+        while (res.size() > 1 && cross(res[res.size() - 2], res.back(), points[i]) < 0)
+        {
+            res.pop_back();
+        }
+
+        res.emplace_back(points[i]);
     }
 
+    if (res.size() == n)
+        return res;
 
+    for (int i = n - 2; i >= 0; --i)
+    {
+        while (res.size() > 1 && cross(res[res.size() - 2], res.back(), points[i]) < 0)
+        {
+            res.pop_back();
+        }
+        res.emplace_back(points[i]);
+    }
+
+    res.pop_back();
+    return res;
 }
 
 void display(const vector<vector<int>>& points)
@@ -51,6 +63,8 @@ void testOuterTrees()
     vector<vector<int>> points1 = {{1, 1}, {2, 2}, {2, 0}, {2, 4}, {3, 3}, {4, 2}};
     auto res1 = outerTrees(points1);
     display(res1);
+
+    cout << endl;
 
     vector<vector<int>> points2 = { {1, 2},{2, 2},{4, 2} };
     auto res2 = outerTrees(points2);
