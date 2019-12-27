87790202b135abbdc0ed5e710cacc4a99248645d
DancingOnAir <taotong1984@hotmail.com>
2019-12-27 15:27:54 +0800

    feat(0005):init q0005 & accepted dp solution for q0005

--- Array/Array/042_TrappingRainWater.cpp
+++ Array/Array/042_TrappingRainWater.cpp
@@ -3,12 +3,16 @@
 //
 //using namespace std;
 //
-//int trap(vector<int>& height)
+//int trap2(vector<int>& height)
 //{
-//    if (height.size() < 3)
+//    int n = height.size();
+//    if (n < 3)
 //        return 0;
 //
-//    int left = 0, right = height.size() - 1, level = 0, lower = 0;
+//    int left = 0;
+//    int right = n - 1;
+//    int lower = 0;
+//    int level = 0;
 //    int res = 0;
 //
 //    while (left < right)
@@ -21,90 +25,50 @@
 //    return res;
 //}
 //
+//int trap(vector<int>& height)
+//{
+//    int n = height.size();
+//    if (n < 3)
+//        return 0;
+//
+//    int sumMap = 0, sumTot = 0;
+//    for (int x : height)
+//        sumMap += x;
+//
+//    int left = 0, right = n - 1;
+//    int maxLeft = 0, maxRight = 0;
+//
+//    while (left <= right)
+//    {
+//        maxLeft = maxLeft > height[left] ? maxLeft : height[left];
+//        maxRight = maxRight > height[right] ? maxRight : height[right];
+//
+//        if (maxLeft <= maxRight)
+//        {
+//            sumTot += maxLeft;
+//            ++left;
+//        }
+//        else
+//        {
+//            sumTot += maxRight;
+//            --right;
+//        }
+//
+//    }
+//
+//    return sumTot - sumMap;
+//}
+//
 //void testTrap()
 //{
-//    vector<int> height = { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
-//    vector<int> height2 = { 5, 2, 1, 2, 1, 5};
-//    cout << trap(height2) << endl;
+//    vector<int> height1 = { 0,1,0,2,1,0,1,3,2,1,2,1 };
+//    cout << trap(height1) << endl;
 //}
 //
 //int main()
 //{
 //    testTrap();
+//
 //    getchar();
+//    return 0;
 //}
-
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-int trap2(vector<int>& height)
-{
-    int n = height.size();
-    if (n < 3)
-        return 0;
-
-    int left = 0;
-    int right = n - 1;
-    int lower = 0;
-    int level = 0;
-    int res = 0;
-
-    while (left < right)
-    {
-        lower = height[height[left] < height[right] ? left++ : right--];
-        level = level > lower ? level : lower;
-        res += level - lower;
-    }
-
-    return res;
-}
-
-int trap(vector<int>& height)
-{
-    int n = height.size();
-    if (n < 3)
-        return 0;
-
-    int sumMap = 0, sumTot = 0;
-    for (int x : height)
-        sumMap += x;
-
-    int left = 0, right = n - 1;
-    int maxLeft = 0, maxRight = 0;
-
-    while (left <= right)
-    {
-        maxLeft = maxLeft > height[left] ? maxLeft : height[left];
-        maxRight = maxRight > height[right] ? maxRight : height[right];
-
-        if (maxLeft <= maxRight)
-        {
-            sumTot += maxLeft;
-            ++left;
-        }
-        else
-        {
-            sumTot += maxRight;
-            --right;
-        }
-
-    }
-
-    return sumTot - sumMap;
-}
-
-void testTrap()
-{
-    vector<int> height1 = { 0,1,0,2,1,0,1,3,2,1,2,1 };
-    cout << trap(height1) << endl;
-}
-
-int main()
-{
-    testTrap();
-
-    getchar();
-    return 0;
-}
---
+++ Array/DP/0005_LongestPalindromicSubstring.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+string longestPalindrome2(string s)
+{
+    int n = s.size();
+    if (n < 2)
+        return s;
+
+    vector<vector<bool>> dp(n, vector<bool>(n, false));
+    int maxLen = 1;
+    int maxStart = 0;
+
+    for (int i = 0; i < n; ++i)
+        dp[i][i] = true;
+
+    for (int i = 0; i < n - 1; ++i)
+    {
+        if (s[i] == s[i + 1])
+        {
+            dp[i][i + 1] = true;
+            maxLen = 2;
+            maxStart = i;
+        }
+    }
+
+    for (int len = 3; len <= n; ++len)
+    {
+        for (int i = 0; i + len - 1 < n; ++i)
+        {
+            int j = i + len - 1;
+            if (s[i] == s[j] && dp[i + 1][j - 1])
+            {
+                dp[i][j] = true;
+                maxLen = len;
+                maxStart = i;
+            }
+        }
+    }
+
+    return s.substr(maxStart, maxLen);
+}
+
+string longestPalindrome(string s)
+{
+    int n = s.size();
+    if (n < 2)
+        return s;
+
+    int maxLen = 0;
+    int maxStart = 0;
+
+    for (int i = 0; i < n;)
+    {
+        if (n - i < maxLen / 2)
+            break;
+
+        int j = i, k = i;
+        while (k < n - 1 && s[k + 1] == s[k])
+            ++k;
+
+        i = k + 1;
+        while (k < n - 1 && j > 0 && s[k + 1] == s[j - 1])
+        {
+            ++k;
+            --j;
+        }
+
+        int newLen = k - j + 1;
+        if (newLen > maxLen)
+        {
+            maxLen = newLen;
+            maxStart = j;
+        }
+
+    }
+
+    return s.substr(maxStart, maxLen);
+}
+
+void testLongestPalindrome()
+{
+    string s1 = "babad";
+    cout << longestPalindrome(s1) << endl;
+
+    string s2 = "cbbd";
+    cout << longestPalindrome(s2) << endl;
+}
+
+int main()
+{
+    testLongestPalindrome();
+
+    getchar();
+    return 0;
+}
