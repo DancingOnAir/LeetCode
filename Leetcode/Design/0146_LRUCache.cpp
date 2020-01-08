5c8e61d07b56803e0d36d75835f9bc6e04c11e2f
DancingOnAir <taotong1984@hotmail.com>
2020-01-08 14:39:54 +0800

    feat(0146):accepted hash table & list solution for q0146

--- Leetcode/Design/0146_LRUCache.cpp
+++ Leetcode/Design/0146_LRUCache.cpp
@@ -2,47 +2,58 @@
 #include <vector>
 #include <unordered_map>
 #include <list>
+#include <queue>
 
 class LRUCache
 {
 public:
-    LRUCache(int capacity): _capacity(capacity)
+    LRUCache(int capacity): capacity_(capacity)
     {
 
     }
 
     int get(int key)
     {
-        if (m.count(key))
-            return m[key];
-        else
+        if (!m_.count(key))
             return -1;
+        
+        list_.splice(list_.begin(), list_, m_[key]);
+        return m_[key]->second;
     }
 
     void put(int key, int value)
     {
-        if (m.count(key) || m.size() < len)
+        if (m_.count(key))
         {
-            m[key] = value;
-            times[key] = 0;
+            list_.splice(list_.begin(), list_, m_[key]);
+            m_[key]->second = value;
+            
             return;
         }
 
-        
-        
-        
-        
+        if (m_.size() >= capacity_)
+        {
+            int toBedeletedKey = list_.back().first;
+            list_.pop_back();
+            m_.erase(toBedeletedKey);
+        }
+
+        list_.emplace_front(std::make_pair(key, value));
+        m_[key] = list_.begin();
     }
 
 private:
-    std::unordered_map<int, std::list<std::pair<int, int>>::iterator> m;
-    std::list<std::pair<int, int>> _list;
-    size_t _capacity;
+    //int - key, pair <int, int> - key, value
+    std::unordered_map<int, std::list<std::pair<int, int>>::iterator> m_;
+    //int - key, int - value
+    std::list<std::pair<int, int>> list_;
+    size_t capacity_;
 };
 
 void test()
 {
     LRUCache cache = LRUCache(2);
+    cache.put(1, 1);
     cache.put(2, 2);
     // returns 1
     std::cout << cache.get(1) << std::endl;
