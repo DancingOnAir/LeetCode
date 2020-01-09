48184999261ae22408b0b40a964e5a9c23ca9c2b
DancingOnAir <taotong1984@hotmail.com>
2020-01-09 10:29:12 +0800

    feat(0155): accepted construct my own stack solution for q0155

--- Leetcode/Design/0155_MinStack.cpp
+++ Leetcode/Design/0155_MinStack.cpp
@@ -40,69 +40,69 @@
 //    std::stack<int> stk2;
 //};
 
-struct Node
-{
-    int min_;
-    int val_;
-    Node* next_;
+//struct Node
+//{
+//    int min_;
+//    int val_;
+//    Node* next_;
+//
+//    Node(int min, int val, Node* next = nullptr): min_(min), val_(val), next_(next) {}
+//};
+//
+//class MinStack {
+//public:
+//    /** initialize your data structure here. */
+//    MinStack()
+//    {
+//        head = new Node(INT_MAX, 0, nullptr);
+//    }
+//
+//    void push(int x)
+//    {
+//        head = new Node(std::min(x, head->min_), x, head);
+//    }
+//
+//    void pop()
+//    {
+//        auto toBeDelete = head;
+//        head = head->next_;
+//        delete toBeDelete;
+//    }
+//
+//    int top()
+//    {
+//        return head->val_;
+//    }
+//
+//    int getMin()
+//    {
+//        return head->min_;
+//    }
+//
+//private:
+//    Node* head;
+//};
 
-    Node(int min, int val, Node* next = nullptr): min_(min), val_(val), next_(next) {}
-};
-
-class MinStack {
-public:
-    /** initialize your data structure here. */
-    MinStack()
-    {
-        head = new Node(INT_MAX, 0, nullptr);
-    }
-
-    void push(int x)
-    {
-        head = new Node(std::min(x, head->min_), x, head);
-    }
-
-    void pop()
-    {
-        auto toBeDelete = head;
-        head = head->next_;
-        delete toBeDelete;
-    }
-
-    int top()
-    {
-        return head->val_;
-    }
-
-    int getMin()
-    {
-        return head->min_;
-    }
-
-private:
-    Node* head;
-};
-
-void test()
-{
-    MinStack minStack = MinStack();
-    minStack.push(-2);
-    minStack.push(0);
-    minStack.push(-3);
-
-    // Returns - 3.
-    std::cout << minStack.getMin() << std::endl;
-    minStack.pop();
-    //Returns 0.
-    std::cout << minStack.top() << std::endl;
-    // Returns - 2.
-    std::cout << minStack.getMin() << std::endl;
-}
-
-int main()
-{
-    test();
-
-    getchar();
-    return 0;
-}
+//void test()
+//{
+//    MinStack minStack = MinStack();
+//    minStack.push(-2);
+//    minStack.push(0);
+//    minStack.push(-3);
+//
+//    // Returns - 3.
+//    std::cout << minStack.getMin() << std::endl;
+//    minStack.pop();
+//    //Returns 0.
+//    std::cout << minStack.top() << std::endl;
+//    // Returns - 2.
+//    std::cout << minStack.getMin() << std::endl;
+//}
+//
+//int main()
+//{
+//    test();
+//
+//    getchar();
+//    return 0;
+//}
