3e4dc5c24ee639e54c7bcc72f7b21ac6049207c3
DancingOnAir <taotong1984@hotmail.com>
2020-01-13 23:11:39 +0800

    feat(0255): accepted double queues solution for q0255

--- Leetcode/Design/0225_ImplementStackusingQueues.h
+++ Leetcode/Design/0225_ImplementStackusingQueues.h
@@ -15,24 +15,42 @@ public:
     /** Push element x onto stack. */
     void push(int x)
     {
+        q1_.emplace(x);
 
+        while (q1_.size() > 1)
+        {
+            q2_.emplace(q1_.front());
+            q1_.pop();
+        }
+        
+        while (!q2_.empty())
+        {
+            q1_.emplace(q2_.front());
+            q2_.pop();
+        }
     }
 
     /** Removes the element on top of the stack and returns that element. */
     int pop()
     {
-        return 0;
+        int temp = q1_.front();
+        q1_.pop();
+        return temp;
     }
 
     /** Get the top element. */
     int top()
     {
-        return 0;
+        return q1_.front();
     }
 
     /** Returns whether the stack is empty. */
     bool empty()
     {
-        return false;
+        return q1_.size() == 0;
     }
+
+private:
+    queue<int> q1_;
+    queue<int> q2_;
 };
