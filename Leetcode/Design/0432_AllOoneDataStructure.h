866366543328c96f3a2179b9977859886d161e76
DancingOnAir <taotong1984@hotmail.com>
2020-01-23 23:23:35 +0800

    feat(0432): accepted o(1) complexity solution for 0432

--- Leetcode/Design/0432_AllOoneDataStructure.h
+++ Leetcode/Design/0432_AllOoneDataStructure.h
@@ -1,8 +1,19 @@
 #pragma once
 #include <string>
 #include <unordered_map>
-#
+#include <list>
 using namespace std;
+
+struct Row
+{
+    int val;
+    list<string> keys;
+
+    Row(int x, const string& s) : val(x), keys({ s })
+    {
+
+    }
+};
 
 class AllOne
 {
@@ -18,62 +29,91 @@ public:
     {
         if (m_.count(key))
         {
-            m_[key]++;
+            auto row = m_[key].first;
+            auto col = m_[key].second;
+            auto prevRow = prev(row);
+
+            if (prevRow == matrix_.end() || prevRow->val != row->val + 1)
+            {
+                auto iter = matrix_.emplace(row, Row(row->val + 1, key));
+                m_[key] = make_pair(iter, iter->keys.begin());
+            }
+            else
+            {
+                prevRow->keys.emplace_front(key);
+                m_[key] = make_pair(prevRow, prevRow->keys.begin());
+            }
+
+            row->keys.erase(col);
+            if (row->keys.empty())
+                matrix_.erase(row);
         }
         else
         {
-            m_[key] = 1;
+            if (matrix_.empty() || matrix_.back().val != 1)
+            {
+                auto iter = matrix_.emplace(matrix_.end(), Row(1, key));
+                m_[key] = make_pair(iter, iter->keys.begin());
+            }
+            else
+            {
+                auto iter = std::prev(matrix_.end());
+                iter->keys.emplace_front(key);
+                m_[key] = make_pair(iter, iter->keys.begin());
+            }
         }
+
     }
 
     /** Decrements an existing key by 1. If Key's value is 1, remove it from the data structure. */
     void dec(string key)
     {
-        if (1 == m_[key])
+        if (!m_.count(key))
+            return;
+
+        auto row = m_[key].first;
+        auto col = m_[key].second;
+
+        if (row->val == 1)
         {
+            row->keys.erase(col);
+            if (row->keys.empty())
+                matrix_.erase(row);
+
             m_.erase(key);
+            return;
+        }
+
+        auto nextRow = next(row);
+        if (nextRow == matrix_.end() || nextRow->val + 1 != row->val)
+        {
+            auto iter = matrix_.emplace(nextRow, Row(row->val - 1, key));
+            m_[key] = make_pair(iter, iter->keys.begin());
         }
         else
         {
-            m_[key]--;
+            nextRow->keys.emplace_front(key);
+            m_[key] = make_pair(nextRow, nextRow->keys.begin());
         }
+
+        row->keys.erase(col);
+        if (row->keys.empty())
+            matrix_.erase(row);
     }
 
     /** Returns one of the keys with maximal value. */
     string getMaxKey()
     {
-        string res("");
-        int maxValue = INT_MIN;
-        for (auto iter = m_.begin(); iter != m_.end(); ++iter)
-        {
-            if (maxValue < iter->second)
-            {
-                maxValue = iter->second;
-                res = iter->first;
-            }
-
-        }
-
-        return res;
+        return matrix_.empty() ? "" : matrix_.front().keys.front();
     }
 
     /** Returns one of the keys with Minimal value. */
     string getMinKey()
     {
-        string res("");
-        int minValue = INT_MAX;
-        for (auto iter = m_.begin(); iter != m_.end(); ++iter)
-        {
-            if (minValue > iter->second)
-            {
-                minValue = iter->second;
-                res = iter->first;
-            }
-        }
-
-        return res;
+        return matrix_.empty() ? "" : matrix_.back().keys.front();
     }
 
 private:
-    unordered_map<string, int> m_;
+    list<Row> matrix_;
+    unordered_map<string, pair<list<Row>::iterator, list<string>::iterator>> m_;
 };
