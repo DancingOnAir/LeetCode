fdcf52a44b8597ad047784f28fdfb4ab875c6f08
DancingOnAir <taotong1984@hotmail.com>
2020-01-24 20:33:18 +0800

    feat(0460): init q0460 & tried matrix list solution for q0460

---
+++ Leetcode/Design/0460_LFUCache.h
@@ -0,0 +1,96 @@
+#pragma once
+#include <unordered_map>
+#include <list>
+
+using namespace std;
+
+class LFUCache
+{
+public:
+    LFUCache(int capacity): capacity_(capacity)
+    {
+
+    }
+
+    int get(int key)
+    {
+        if (!m_.count(key))
+            return -1;
+
+        increaseFrequency(key);
+
+        return m_[key].second->second;
+    }
+
+    void put(int key, int value)
+    {
+        if (m_.count(key))
+        {
+            increaseFrequency(key);
+            m_[key].second->second = value;
+            return;
+        }
+
+        if (m_.size() >= capacity_)
+        {
+            auto LFUkv = matrix_.back().kvs_.back();
+
+            matrix_.back().kvs_.pop_back();
+            if (matrix_.back().kvs_.empty())
+                matrix_.pop_back();
+
+            m_.erase(LFUkv.first);
+        }
+
+        if (matrix_.back().frequent_ == 1)
+        {
+            auto lastRow = prev(matrix_.end());
+            lastRow->kvs_.emplace_front(make_pair(key, value));
+            m_.emplace(key, make_pair(lastRow, lastRow->kvs_.begin()));
+        }
+        else
+        {
+            auto newRow = matrix_.emplace(matrix_.end(), Bucket(1, make_pair(key, value)));
+            m_.emplace(key, make_pair(newRow, newRow->kvs_.begin()));
+        }
+    }
+
+
+private:
+    void increaseFrequency(int key)
+    {
+        auto row = m_[key].first;
+        auto col = m_[key].second;
+        auto prevRow = prev(row);
+
+        if (prevRow == matrix_.end() || prevRow->frequent_ != row->frequent_ + 1)
+        {
+            auto newRow = matrix_.emplace(row, Bucket(row->frequent_ + 1, make_pair(key, col->second)));
+            m_[key] = make_pair(newRow, newRow->kvs_.begin());
+        }
+        else
+        {
+            prevRow->kvs_.emplace_front(make_pair(key, col->second));
+            m_[key] = make_pair(prevRow, prevRow->kvs_.begin());
+
+        }
+
+        row->kvs_.erase(col);
+        if (row->kvs_.empty())
+            matrix_.erase(row);
+    }
+
+    struct Bucket
+    {
+        int frequent_;
+        list<pair<int, int>> kvs_;
+        Bucket(int frequent, pair<int, int> kv) : frequent_(frequent), kvs_({ kv })
+        {
+
+        }
+    };
+
+    int capacity_;
+    list<Bucket> matrix_;
+    unordered_map<int, pair<list<Bucket>::iterator, list<pair<int, int>>::iterator>> m_;
+};
---
+++ Leetcode/DesignTest/0460_LFUCacheTest.cpp
@@ -0,0 +1,11 @@
+#include "pch.h"
+#include "../Design/0460_LFUCache.h"
+
+class LFUCacheTest : public testing::Test
+{
+public:
+
+protected:
+    LFUCache* lfuCache_;
+};
+
