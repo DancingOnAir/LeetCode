4c26188937669c8899eeca0893fd9a4f47b4f6af
DancingOnAir <taotong1984@hotmail.com>
2020-02-13 18:41:30 +0800

    feat(SlidingWindow): accepted solution for q0978

--- Leetcode/SlidingWindow/0978_LongestTurbulentSubarray.h
+++ Leetcode/SlidingWindow/0978_LongestTurbulentSubarray.h
@@ -5,59 +5,35 @@ using namespace std;
 class Solution
 {
 public:
+    int compareValue(int lhs, int rhs)
+    {
+        if (lhs == rhs)
+            return 0;
+
+        return lhs < rhs ? -1 : 1;
+    }
+
     int maxTurbulenceSize(vector<int>& A)
     {
         int n = A.size();
         if (n < 2)
             return n;
 
-        if (n == 2 & A[0] == A[1])
-            return 1;
-
-        int left = 0, right = 2, counter = 0, res = 1;
-
-        if (A[0] < A[1])
+        int res = 1;
+        int anchor = 0;
+        for (int i = 1; i < n; ++i)
         {
-            counter = 1;
-            res = 2;
-        }
-        else if (A[0] > A[1])
-        {
-            counter = -1;
-            res = 2;
-        }
+            int c = compareValue(A[i - 1], A[i]);
 
-        while (right < A.size())
-        {
-            if (A[right - 1] < A[right] && counter == -1)
+            if (i == n - 1 || c * compareValue(A[i], A[i + 1]) != -1)
             {
-                counter = 1;
-            }
-            else if (A[right - 1] > A[right] && counter == 1)
-            {
-                counter = -1;
-            }
-            else
-            {
-                counter = 0;
+                if (c != 0)
+                    res = max(res, i - anchor + 1);
+                anchor = i;
             }
 
-            while (counter == 0 && right < A.size())
-            {
-                res = max(res, right - left);
-                left = right - 1;
-
-                if (A[left] < A[right])
-                    counter = 1;
-                else if (A[left] > A[right])
-                    counter = -1;
-                else
-                    ++right;
-            }
-
-            ++right;
         }
 
-        return max(res, right - left);
+        return res;
     }
 };
