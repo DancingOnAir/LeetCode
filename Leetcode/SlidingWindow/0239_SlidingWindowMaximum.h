312a76ef040cc05af7a974789abe2fd08f7c2a2e
DancingOnAir <taotong1984@hotmail.com>
2020-02-14 23:13:12 +0800

    feat(SlidingWindow): accepted solution for q0480

--- Leetcode/SlidingWindow/0239_SlidingWindowMaximum.h
+++ Leetcode/SlidingWindow/0239_SlidingWindowMaximum.h
@@ -7,7 +7,7 @@ using namespace std;
 class Solution
 {
 public:
-    vector<int> maxSlidingWindow(vector<int>& nums, int k)
+    vector<int> maxSlidingWindow2(vector<int>& nums, int k)
     {
         if (nums.empty() || k < 1 || k > nums.size())
             return vector<int>();
@@ -25,6 +25,51 @@ public:
             dq.emplace_back(i);
             if (i >= k - 1)
                 res.emplace_back(nums[dq.front()]);
+        }
+
+        return res;
+    }
+
+    class MaxQueue
+    {
+    public:
+        void pop(int num)
+        {
+            if (!data_.empty() && data_.front() == num)
+                data_.pop_front();
+        }
+
+        void push(int num)
+        {
+            while (!data_.empty() && data_.back() < num)
+                data_.pop_back();
+            data_.emplace_back(num);
+        }
+
+        int getMax()
+        {
+            return data_.front();
+        }
+
+    private:
+        deque<int> data_;
+    };
+
+    vector<int> maxSlidingWindow(vector<int>& nums, int k)
+    {
+        if (nums.empty() || k < 1 || k > nums.size())
+            return vector<int>();
+
+        vector<int> res;
+        MaxQueue mq;
+        for (int i = 0; i < k - 1; ++i)
+            mq.push(nums[i]);
+
+        for (int i = k - 1; i < nums.size(); ++i)
+        {
+            mq.push(nums[i]);
+            res.emplace_back(mq.getMax());
+            mq.pop(nums[i - k + 1]);
         }
 
         return res;
--- Leetcode/SlidingWindow/0480_SlidingWindowMedian.h
+++ Leetcode/SlidingWindow/0480_SlidingWindowMedian.h
@@ -1,26 +1,61 @@
 #pragma once
 #include <vector>
-#include <deque>
+#include <set>
 using namespace std;
 
 class Solution
 {
 public:
+    class MedianQueue
+    {
+    public:
+        MedianQueue(size_t len) : len_(len)
+        {
+
+        }
+
+        void pop(int num)
+        {
+            data_.erase(data_.lower_bound(num));
+        }
+
+        void push(int num)
+        {
+            data_.emplace(num);
+        }
+
+        double getMedian()
+        {
+            double m1 = *next(data_.begin(), len_ / 2);
+            if (len_ & 1)
+            {
+                return m1;
+            }
+
+            double m2 = *next(data_.begin(), len_ / 2 - 1);
+            return (m1 + m2) * 0.5;
+        }
+
+    private:
+        multiset<int> data_;
+        size_t len_;
+    };
+
     vector<double> medianSlidingWindow(vector<int>& nums, int k)
     {
         if (nums.empty())
             return vector<double>();
 
         vector<double> res;
-        int left = 0, right = 0, counter = 0;
-        deque<int> dq;
+        MedianQueue mq(k);
+        for (int i = 0; i < k - 1; ++i)
+            mq.push(nums[i]);
 
-        for (int i = 0; i < nums.size(); ++i)
+        for (int i = k - 1; i < nums.size(); ++i)
         {
-            if (!dq.empty() && dq.front() == i - k)
-                dq.pop_front();
-
-            dq.emplace_back(i);
+            mq.push(nums[i]);
+            res.emplace_back(mq.getMedian());
+            mq.pop(nums[i - k + 1]);
         }
 
         return res;
