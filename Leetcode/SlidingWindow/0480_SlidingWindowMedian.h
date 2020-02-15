d1cce239580d99cb4d7e0c02822772bf97c44d6d
DancingOnAir <taotong1984@hotmail.com>
2020-02-15 13:57:22 +0800

    feat(SlidingWindow): accepted two heap & hash solution for q0480

--- Leetcode/SlidingWindow/0480_SlidingWindowMedian.h
+++ Leetcode/SlidingWindow/0480_SlidingWindowMedian.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <vector>
 #include <set>
+#include <queue>
+#include <unordered_map>
 using namespace std;
 
 class Solution
@@ -41,7 +43,7 @@ public:
         size_t len_;
     };
 
-    vector<double> medianSlidingWindow(vector<int>& nums, int k)
+    vector<double> medianSlidingWindow2(vector<int>& nums, int k)
     {
         if (nums.empty())
             return vector<double>();
@@ -56,6 +58,94 @@ public:
             mq.push(nums[i]);
             res.emplace_back(mq.getMedian());
             mq.pop(nums[i - k + 1]);
+        }
+
+        return res;
+    }
+
+    vector<double> medianSlidingWindow(vector<int>& nums, int k)
+    {
+        if (nums.empty() || k > nums.size())
+            return vector<double>();
+
+        priority_queue<int> maxHeap;
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+        vector<double> res;
+        int i = 0;
+        while (i < k)
+        {
+            maxHeap.emplace(nums[i++]);
+        }
+
+        for (int j = 0; j < k / 2; ++j)
+        {
+            minHeap.emplace(maxHeap.top());
+            maxHeap.pop();
+        }
+
+        unordered_map<int, int> count;
+
+        while (true)
+        {
+            if (k & 1)
+                res.emplace_back(maxHeap.top());
+            else
+                res.emplace_back(static_cast<double>(maxHeap.top() + minHeap.top()) * 0.5);
+
+            if (i == nums.size())
+                break;
+
+            int m = nums[i - k], n = nums[i++], balance = 0;
+            if (m <= maxHeap.top())
+            {
+                --balance;
+                if (m == maxHeap.top())
+                    maxHeap.pop();
+                else
+                    ++count[m];
+            }
+            else
+            {
+                ++balance;
+                if (m == minHeap.top())
+                    minHeap.pop();
+                else
+                    ++count[m];
+            }
+
+            if (!maxHeap.empty() && n <= maxHeap.top())
+            {
+                ++balance;
+                maxHeap.emplace(n);
+            }
+            else
+            {
+                --balance;
+                minHeap.emplace(n);
+            }
+
+            if (balance < 0)
+            {
+                maxHeap.emplace(minHeap.top());
+                minHeap.pop();
+            }
+            else if (balance > 0)
+            {
+                minHeap.emplace(maxHeap.top());
+                maxHeap.pop();
+            }
+
+            while (!maxHeap.empty() && count[maxHeap.top()])
+            {
+                --count[maxHeap.top()];
+                maxHeap.pop();
+            }
+
+            while (!minHeap.empty() && count[minHeap.top()])
+            {
+                --count[minHeap.top()];
+                minHeap.pop();
+            }
         }
 
         return res;
