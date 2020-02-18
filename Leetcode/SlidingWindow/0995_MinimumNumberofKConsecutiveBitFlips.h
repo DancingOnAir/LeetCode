f4756fad4c73a805faecf37b764cb0d6c3243e17
DancingOnAir <taotong1984@hotmail.com>
2020-02-18 08:40:53 +0800

    feat(SlidingWindow): tried a brute force solution but TLE

--- Leetcode/SlidingWindow/0995_MinimumNumberofKConsecutiveBitFlips.h
+++ Leetcode/SlidingWindow/0995_MinimumNumberofKConsecutiveBitFlips.h
@@ -8,7 +8,21 @@ class Solution
 public:
     int minKBitFlips(vector<int>& A, int K)
     {
-        int left = 0, right = 0, res = 0;
+        int res = 0;
+
+        for (int i = 0; i < A.size(); ++i)
+        {
+            if (A[i] == 0)
+            {
+                if (i + K > A.size())
+                    return -1;
+
+                ++res;
+                for (int j = i; j < i + K; ++j)
+                    A[j] ^= 1;
+            }
+        }
+
         return res;
     }
 };
--- Leetcode/SlidingWindowTest/0995_MinimumNumberofKConsecutiveBitFlipsTest.cpp
+++ Leetcode/SlidingWindowTest/0995_MinimumNumberofKConsecutiveBitFlipsTest.cpp
@@ -17,3 +17,18 @@ TEST_F(AMinimumNumberofKConsecutiveBitFlips, InputArrayWithThreeElementAndKEqOne
     ASSERT_EQ(solution.minKBitFlips(A, K), 2);
 }
 
+TEST_F(AMinimumNumberofKConsecutiveBitFlips, InputArrayWithThreeElementAndKEqTwoReturnMinusOne)
+{
+    vector<int> A = { 1, 1, 0 };
+    int K = 2;
+
+    ASSERT_EQ(solution.minKBitFlips(A, K), -1);
+}
+
+TEST_F(AMinimumNumberofKConsecutiveBitFlips, InputArrayWithEightElementAndKEqThreeReturnThree)
+{
+    vector<int> A = { 0, 0, 0, 1, 0, 1, 1, 0 };
+    int K = 3;
+
+    ASSERT_EQ(solution.minKBitFlips(A, K), 3);
+}
