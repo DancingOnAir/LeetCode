2e81cdfbf8d232c31ffe82038cf8085893f4e9df
DancingOnAir <taotong1984@hotmail.com>
2020-02-22 14:35:38 +0800

    feat(SlidingWindow): accepted solution for q1074

--- Leetcode/SlidingWindow/1074_NumberofSubmatricesThatSumtoTarget.h
+++ Leetcode/SlidingWindow/1074_NumberofSubmatricesThatSumtoTarget.h
@@ -1,20 +1,16 @@
 #pragma once
 #include <vector>
+#include <unordered_map>
 using namespace std;
 
 class Solution
 {
 public:
-    int numSubmatrixSumTarget(vector<vector<int>>& matrix, int target)
+    int numSubmatrixSumTarget2(vector<vector<int>>& matrix, int target)
     {
         int m = matrix.size();
         int n = matrix[0].size();
         vector<vector<int>> presum(m + 1, vector<int>(n + 1));
-        //for (int i = 1; i <= m; ++i)
-        //    presum[i][0] = matrix[i - 1][0] + presum[i - 1][0];
-
-        //for (int i = 1; i <= n; ++i)
-        //    presum[0][i] += matrix[0][i - 1] + presum[0][i - 1];
 
         for (int i = 1; i <= m; ++i)
         {
@@ -37,6 +33,45 @@ public:
                         if (sum == target)
                             ++res;
                     }
+                }
+            }
+        }
+
+        return res;
+    }
+
+    int numSubmatrixSumTarget(vector<vector<int>>& matrix, int target)
+    {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<vector<int>> presum(m + 1, vector<int>(n + 1));
+
+        for (int i = 1; i <= m; ++i)
+        {
+            for (int j = 1; j <= n; ++j)
+            {
+                presum[i][j] = matrix[i - 1][j - 1] + presum[i - 1][j] + presum[i][j - 1] - presum[i - 1][j - 1];
+            }
+        }
+
+        int res = 0;
+        for (int left = 0; left <= n; ++left)
+        {
+            for (int right = left + 1; right <= n; ++right)
+            {
+                unordered_map<int, int> counter;
+                int sum = 0;
+
+                for (int k = 1; k <= m; ++k)
+                {
+                    sum = presum[k][right] + presum[0][left] - presum[k][left] - presum[0][right];
+                    if (target == sum)
+                        ++res;
+
+                    if (counter.count(sum - target))
+                        res += counter[sum - target];
+
+                    counter[sum]++;
                 }
             }
         }
