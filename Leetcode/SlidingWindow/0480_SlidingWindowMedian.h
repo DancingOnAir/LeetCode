7827480d3d806942742c20c73fb8110677f75dd9
DancingOnAir <taotong1984@hotmail.com>
2020-02-23 13:41:19 +0800

    feat(SlidingWindow): accepted multiset & mid pointer solution for q0480

--- Leetcode/SlidingWindow/0480_SlidingWindowMedian.h
+++ Leetcode/SlidingWindow/0480_SlidingWindowMedian.h
@@ -3,6 +3,7 @@
 #include <set>
 #include <queue>
 #include <unordered_map>
+
 using namespace std;
 
 class Solution
@@ -63,7 +64,7 @@ public:
         return res;
     }
 
-    vector<double> medianSlidingWindow(vector<int>& nums, int k)
+    vector<double> medianSlidingWindow1(vector<int>& nums, int k)
     {
         if (nums.empty() || k > nums.size())
             return vector<double>();
@@ -149,5 +150,31 @@ public:
         }
 
         return res;
+    }
+
+    vector<double> medianSlidingWindow(vector<int>& nums, int k)
+    {
+        vector<double> res;
+        if (nums.empty() || k > nums.size())
+            return res;
+
+        multiset<int> ms(nums.begin(), nums.begin() + k);
+        auto mid = next(ms.begin(), k / 2);
+
+        for (int i = k; ; ++i)
+        {
+            res.emplace_back(((double)(*mid) + *prev(mid, (1 - k % 2))) * 0.5);
+
+            if (i == nums.size())
+                return res;
+
+            ms.emplace(nums[i]);
+            if (nums[i] < *mid)
+                --mid;
+
+            if (nums[i - k] <= *mid)
+                ++mid;
+            ms.erase(ms.lower_bound(nums[i - k]));
+        }
     }
 };
