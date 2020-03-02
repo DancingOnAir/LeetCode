e6fc0a46c385ddeb478cdd647a0eb37f6a8d0f81
DancingOnAir <taotong1984@hotmail.com>
2020-03-02 15:29:10 +0800

    feat(LineSweep): accepted line sweep solution for q0850

--- Leetcode/LineSweep/0850_RectangleAreaII.h
+++ Leetcode/LineSweep/0850_RectangleAreaII.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <vector>
+#include <set>
 using namespace std;
 
 class Solution
@@ -19,8 +20,76 @@ public:
         }
     };
 
+    void pushup(int start, int end, int segIndex)
+    {
+        if (covers[segIndex])
+            heights[segIndex] = nodes[end] - nodes[start];
+        else if (start + 1 == end)
+            heights[segIndex] = 0;
+        else
+            heights[segIndex] = heights[segIndex << 1] + heights[segIndex << 1 | 1];
+    }
+
+    void updateSegmentTree(int downYIndex, int upYIndex, int isIn, int start, int end, int segIndex)
+    {
+        if (downYIndex > end || upYIndex < start)
+            return;
+
+        if (downYIndex <= start && upYIndex >= end)
+        {
+            covers[segIndex] += isIn;
+            pushup(start, end, segIndex);
+            return;
+        }
+
+        if (start + 1 == end)
+            return;
+
+        int mid = start + ((end - start) >> 1);
+        if (downYIndex <= mid)
+            updateSegmentTree(downYIndex, upYIndex, isIn, start, mid, segIndex << 1);
+        if (upYIndex > mid)
+            updateSegmentTree(downYIndex, upYIndex, isIn, mid, end, segIndex << 1 | 1);
+
+        pushup(start, end, segIndex);
+    }
+
     int rectangleArea(vector<vector<int>>& rectangles)
     {
-        return 0;
+        vector<ScanLine> lines;
+        nodes.emplace_back(0);
+
+        lines.emplace_back(ScanLine(0, 0, 0, 0));
+
+        for (int i = 0; i < rectangles.size(); ++i)
+        {
+            lines.emplace_back(ScanLine(rectangles[i][0], rectangles[i][1], rectangles[i][3], 1));
+            lines.emplace_back(ScanLine(rectangles[i][2], rectangles[i][1], rectangles[i][3], -1));
+            nodes.emplace_back(rectangles[i][1]);
+            nodes.emplace_back(rectangles[i][3]);
+        }
+
+        sort(lines.begin(), lines.end());
+        sort(nodes.begin(), nodes.end());
+        unique(next(nodes.begin()), nodes.end());
+        
+        int res = 0;
+        heights.resize(nodes.size() * 2, 0);
+        covers.resize(nodes.size() * 2, 0);
+
+        for (int i = 1; i < lines.size(); ++i)
+        {
+            res =  (res + heights[1] * ((long long)lines[i].x_ - lines[i - 1].x_)) % 1000000007;
+            int left = lower_bound(next(nodes.begin()), nodes.end(), lines[i].downY_) - nodes.begin();
+            int right = lower_bound(next(nodes.begin()), nodes.end(), lines[i].upY_) - nodes.begin();
+
+            updateSegmentTree(left, right, lines[i].isIn_, 1, nodes.size() - 1, 1);
+        }
+
+        return res;
     }
+private:
+    vector<int> covers;
+    vector<int> heights;
+    vector<int> nodes;
 };
--- Leetcode/LineSweepTest/0850_RectangleAreaIITest.cpp
+++ Leetcode/LineSweepTest/0850_RectangleAreaIITest.cpp
@@ -13,3 +13,9 @@ TEST_F(RectangleAreaII, Answer3Rectangles)
     vector<vector<int>> rectangles{ {0, 0, 2, 2},{1, 0, 2, 3},{1, 0, 3, 1} };
     ASSERT_EQ(solution.rectangleArea(rectangles), 6);
 }
+
+TEST_F(RectangleAreaII, Answer3RectanglesTestOverflow)
+{
+    vector<vector<int>> rectangles{ {25, 20, 70, 27},{68, 80, 79, 100},{37, 41, 66, 76} };
+    ASSERT_EQ(solution.rectangleArea(rectangles), 6);
+}
