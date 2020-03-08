25f66b1a295f5d2da79d4233c336195db6694d6b
DancingOnAir <taotong1984@hotmail.com>
2020-03-08 15:03:28 +0800

    feat(SegmentTree): accepted segment tree solution which was implemented by pointers for q0307

--- Leetcode/SegmentTree/0307_RangeSumQueryMutable.h
+++ Leetcode/SegmentTree/0307_RangeSumQueryMutable.h
@@ -2,168 +2,318 @@
 #include <vector>
 using namespace std;
 
-class NumArray
+//class NumArray
+//{
+//private:
+//    struct stNode
+//    {
+//        int left_;
+//        int right_;
+//        int len_;
+//        int idx_;
+//        int lazy_;
+//        int sum_;
+//
+//        void reset(int idx, int left, int right)
+//        {
+//            idx_ = idx;
+//            left_ = left;
+//            right_ = right;
+//            len_ = right_ - left_ + 1;
+//            sum_ = lazy_ = 0;
+//        }
+//
+//        int leftChildIdx()
+//        {
+//            return idx_ << 1;
+//        }
+//
+//        int rightChildIdx()
+//        {
+//            return idx_ << 1 | 1;
+//        }
+//
+//        void updateByValue(int val)
+//        {
+//            lazy_ += val;
+//            sum_ = val * len_;
+//        }
+//    };
+//
+//    vector<int> nums_;
+//    vector<stNode> segTree;
+//
+//    void pushUp(int idx)
+//    {
+//        segTree[idx].sum_ = segTree[idx << 1].sum_ + segTree[idx << 1 | 1].sum_;
+//    }
+//
+//    void pushDown(int idx)
+//    {
+//        int lazy = segTree[idx].lazy_;
+//        if (lazy)
+//        {
+//            segTree[idx << 1].updateByValue(lazy);
+//            segTree[idx << 1 | 1].updateByValue(lazy);
+//            segTree[idx].lazy_ = 0;
+//        }
+//    }
+//
+//    void buildSegmentTree(int idx, int start, int end)
+//    {
+//        if (start > end)
+//            return;
+//
+//        segTree[idx].reset(idx, start, end);
+//        if (start == end)
+//        {
+//            segTree[idx].sum_ = nums_[start];
+//            return;
+//        }
+//
+//        int mid = (start + end) >> 1;
+//        buildSegmentTree(idx << 1, start, mid);
+//        buildSegmentTree(idx << 1 | 1, mid + 1, end);
+//        pushUp(idx);
+//    }
+//
+//    bool isIntersected(int start, int end, int x, int y)
+//    {
+//        if (y < start || x > end)
+//            return false;
+//        return true;
+//    }
+//
+//    bool isContained(int start, int end, int x, int y)
+//    {
+//        if (x <= start && y >= end)
+//            return true;
+//        return false;
+//    }
+//
+//    void updateSegmentTree(int idx, int start, int end, int x, int y, int val)
+//    {
+//        if (!isIntersected(start, end, x, y))
+//            return;
+//
+//        if (isContained(start, end, x, y))
+//        {
+//            segTree[idx].updateByValue(val);
+//            return;
+//        }
+//
+//        pushDown(idx);
+//
+//        int mid = (start + end) >> 1;
+//        updateSegmentTree(idx << 1, start, mid, x, y, val);
+//        updateSegmentTree(idx << 1 | 1, mid + 1, end, x, y, val);
+//
+//        pushUp(idx);
+//    }
+//
+//    void mergeQuery(int& sum, int idx)
+//    {
+//        sum += segTree[idx].sum_;
+//    }
+//
+//    void query(int idx, int start, int end, int x, int y, int& sum)
+//    {
+//        if (!isIntersected(start, end, x, y))
+//            return;
+//
+//        if (isContained(start, end, x, y))
+//        {
+//            mergeQuery(sum, idx);
+//            return;
+//        }
+//        pushDown(idx);
+//
+//        int mid = (start + end) >> 1;
+//        query(idx << 1, start, mid, x, y, sum);
+//        query(idx << 1 | 1, mid + 1, end, x, y, sum);
+//
+//        pushUp(idx);
+//    }
+//
+//    int len_;
+//public:
+//    NumArray(vector<int>& nums)
+//    {
+//        len_ = nums.size();
+//        nums_.emplace_back(0);
+//        nums_.insert(nums_.end(), nums.begin(), nums.end());
+//
+//        segTree.resize(4 * len_);
+//
+//        buildSegmentTree(1, 1, len_);
+//    }
+//
+//    void update(int i, int val)
+//    {
+//        if (i > len_)
+//            return;
+//
+//        updateSegmentTree(1, 1, len_, i + 1, i + 1, val);
+//    }
+//
+//    int sumRange(int i, int j)
+//    {
+//        if (j > len_ || j < i)
+//            return 0;
+//
+//        int sum = 0;
+//        query(1, 1, len_, i + 1, j + 1, sum);
+//
+//        return sum;
+//    }
+//};
+
+
+
+// implemented segment tree by pointer
+struct SegmentTreeNode
+{
+    int start_;
+    int end_;
+    int sum_;
+    int lazy_;
+    SegmentTreeNode* left_;
+    SegmentTreeNode* right_;
+
+    SegmentTreeNode(int start, int end) : start_(start), end_(end), sum_(0), lazy_(0), left_(nullptr), right_(nullptr)
+    {
+
+    }
+
+    void updateByValue(int val)
+    {
+        lazy_ += val;
+        sum_ = val * (end_ - start_ + 1);
+    }
+};
+
+class NumArray1
 {
 private:
-    struct stNode
+    SegmentTreeNode* root_;
+
+    void pushUp(SegmentTreeNode* root)
     {
-        int left_;
-        int right_;
-        int len_;
-        int idx_;
-        int lazy_;
-        int sum_;
-
-        void reset(int idx, int left, int right)
-        {
-            idx_ = idx;
-            left_ = left;
-            right_ = right;
-            len_ = right_ - left_ + 1;
-            sum_ = lazy_ = 0;
-        }
-
-        int leftChildIdx()
-        {
-            return idx_ << 1;
-        }
-
-        int rightChildIdx()
-        {
-            return idx_ << 1 | 1;
-        }
-
-        void updateByValue(int val)
-        {
-            lazy_ += val;
-            sum_ = val * len_;
-        }
-    };
-
-    vector<int> nums_;
-    vector<stNode> segTree;
-
-    void pushUp(int idx)
-    {
-        segTree[idx].sum_ = segTree[idx << 1].sum_ + segTree[idx << 1 | 1].sum_;
+        root->sum_ = root->left_->sum_ + root->right_->sum_;
     }
 
-    void pushDown(int idx)
+    void pushDown(SegmentTreeNode* root)
     {
-        int lazy = segTree[idx].lazy_;
+        int lazy = root->lazy_;
         if (lazy)
         {
-            segTree[idx << 1].updateByValue(lazy);
-            segTree[idx << 1 | 1].updateByValue(lazy);
-            segTree[idx].lazy_ = 0;
+            root->left_->updateByValue(lazy);
+            root->right_->updateByValue(lazy);
+            root->lazy_ = 0;
         }
     }
 
-    void buildSegmentTree(int idx, int start, int end)
+    SegmentTreeNode* buildSegmentTree(vector<int>& nums, int start, int end)
     {
         if (start > end)
-            return;
+            return nullptr;
 
-        segTree[idx].reset(idx, start, end);
+        SegmentTreeNode* root = new SegmentTreeNode(start, end);
         if (start == end)
         {
-            segTree[idx].sum_ = nums_[start];
-            return;
+            root->sum_ = nums[start];
+            return root;
         }
 
-        int mid = (start + end) >> 1;
-        buildSegmentTree(idx << 1, start, mid);
-        buildSegmentTree(idx << 1 | 1, mid + 1, end);
-        pushUp(idx);
+        int mid = start + ((end - start) >> 1);
+        root->left_ = buildSegmentTree(nums, start, mid);
+        root->right_ = buildSegmentTree(nums, mid + 1, end);
+        pushUp(root);
+        
+        return root;
     }
 
-    bool isIntersected(int start, int end, int x, int y)
+    void updateSegmentTree(int i, int val, SegmentTreeNode* root)
     {
-        if (y < start || x > end)
-            return false;
-        return true;
-    }
-
-    bool isContained(int start, int end, int x, int y)
-    {
-        if (x <= start && y >= end)
-            return true;
-        return false;
-    }
-
-    void updateSegmentTree(int idx, int start, int end, int x, int y, int val)
-    {
-        if (!isIntersected(start, end, x, y))
+        if (!root)
             return;
 
-        if (isContained(start, end, x, y))
+        if (root->start_ == i && root->end_ == i)
         {
-            segTree[idx].updateByValue(val);
+            root->sum_ = val;
             return;
         }
 
-        pushDown(idx);
+        pushDown(root);
 
-        int mid = (start + end) >> 1;
-        updateSegmentTree(idx << 1, start, mid, x, y, val);
-        updateSegmentTree(idx << 1 | 1, mid + 1, end, x, y, val);
+        int mid = root->start_ + ((root->end_ - root->start_) >> 1);
+        if (i <= mid)
+            updateSegmentTree(i, val, root->left_);
+        else
+            updateSegmentTree(i, val, root->right_);
 
-        pushUp(idx);
+        pushUp(root);
     }
 
-    void mergeQuery(int& sum, int idx)
+    void querySegmentTree(int i, int j, SegmentTreeNode* root, int& sum)
     {
-        sum += segTree[idx].sum_;
-    }
-
-    void query(int idx, int start, int end, int x, int y, int& sum)
-    {
-        if (!isIntersected(start, end, x, y))
+        if (!root)
             return;
 
-        if (isContained(start, end, x, y))
+        if (i <= root->start_ && j >= root->end_)
         {
-            mergeQuery(sum, idx);
+            sum += root->sum_;
             return;
         }
-        pushDown(idx);
 
-        int mid = (start + end) >> 1;
-        query(idx << 1, start, mid, x, y, sum);
-        query(idx << 1 | 1, mid + 1, end, x, y, sum);
+        pushDown(root);
 
-        pushUp(idx);
+        int mid = root->start_ + ((root->end_ - root->start_) >> 1);
+        if (i <= mid)
+            querySegmentTree(i, j, root->left_, sum);
+        if (j > mid)
+            querySegmentTree(i, j, root->right_, sum);
+
+        pushUp(root);
     }
 
-    int len_;
 public:
-    NumArray(vector<int>& nums)
+    NumArray1(vector<int>& nums)
     {
-        len_ = nums.size();
-        nums_.emplace_back(0);
-        nums_.insert(nums_.end(), nums.begin(), nums.end());
-
-        segTree.resize(4 * len_);
-
-        buildSegmentTree(1, 1, len_);
+        int n = nums.size();
+        root_ = buildSegmentTree(nums, 0, n - 1);
     }
 
     void update(int i, int val)
     {
-        if (i > len_)
-            return;
-
-        updateSegmentTree(1, 1, len_, i + 1, i + 1, val);
+        updateSegmentTree(i, val, root_);
     }
 
     int sumRange(int i, int j)
     {
-        if (j > len_ || j < i)
-            return 0;
-
         int sum = 0;
-        query(1, 1, len_, i + 1, j + 1, sum);
-
+        querySegmentTree(i, j, root_, sum);
+        
         return sum;
+    }
+};
+
+class NumArray
+{
+public:
+    NumArray(vector<int>& nums)
+    {
+        int n = nums.size();
+
+    }
+
+    void update(int i, int val)
+    {
+
+    }
+
+    int sumRange(int i, int j)
+    {
+
     }
 };
--- Leetcode/SegmentTreeTest/0307_RangeSumQueryMutableTest.cpp
+++ Leetcode/SegmentTreeTest/0307_RangeSumQueryMutableTest.cpp
@@ -7,45 +7,79 @@ class ARangeSumQueryMutable : public Test
 public:
     virtual void SetUp()
     {
-        nums = { 1, 3, 5 };
-        pNumArray = new NumArray(nums);
+        pEmptyNumArray = new NumArray(emptyNums);
+
+        threeNums = { 1, 3, 5 };
+        pNumArrayContainsThreeNums = new NumArray(threeNums);
+
+        fiveNums = { 0, 9, 5, 7, 3 };
+        pNumArrayContainsFiveNums = new NumArray(fiveNums);
     }
 
     virtual void TearDown()
     {
-        delete pNumArray;
+        delete pEmptyNumArray;
+        delete pNumArrayContainsThreeNums;
+        delete pNumArrayContainsFiveNums;
     }
 
 protected:
-    vector<int> nums;
-    NumArray* pNumArray;
+    vector<int> threeNums;
+    vector<int> fiveNums;
+    vector<int> emptyNums;
+    NumArray* pNumArrayContainsThreeNums;
+    NumArray* pNumArrayContainsFiveNums;
+    NumArray* pEmptyNumArray;
 };
 
 TEST_F(ARangeSumQueryMutable, AnswerEmptyNumArray)
 {
-    NumArray emptyNumArray = NumArray(vector<int>());
-    ASSERT_EQ(emptyNumArray.sumRange(0, 0), 0);
+    ASSERT_EQ(pEmptyNumArray->sumRange(0, 0), 0);
 }
 
-TEST_F(ARangeSumQueryMutable, AnswerTotalSum)
+TEST_F(ARangeSumQueryMutable, AnswerThreeNumsTotalSum)
 {
-    int n = nums.size();
+    int n = threeNums.size();
     int expectSum = 0;
-    for (int x : nums)
+    for (int x : threeNums)
         expectSum += x;
 
-    ASSERT_EQ(pNumArray->sumRange(0, n - 1), expectSum);
+    ASSERT_EQ(pNumArrayContainsThreeNums->sumRange(0, n - 1), expectSum);
 }
 
-TEST_F(ARangeSumQueryMutable, AnswerUpdateVal)
+TEST_F(ARangeSumQueryMutable, AnswerThreeNumsUpdateVal)
 {
-    int n = nums.size();
-    pNumArray->update(1, 2);
-    nums[1] = 2;
+    int n = threeNums.size();
+    pNumArrayContainsThreeNums->update(1, 2);
 
+    threeNums[1] = 2;
     int expectSum = 0;
-    for (int x : nums)
+    for (int x : threeNums)
         expectSum += x;
 
-    ASSERT_EQ(pNumArray->sumRange(0, n - 1), expectSum);
+    ASSERT_EQ(pNumArrayContainsThreeNums->sumRange(0, n - 1), expectSum);
+}
+
+TEST_F(ARangeSumQueryMutable, AnswerFiveNumsSum)
+{
+    int n = fiveNums.size();
+    int expectSum = 0;
+    for (int x : fiveNums)
+        expectSum += x;
+
+    EXPECT_EQ(pNumArrayContainsFiveNums->sumRange(2, 4), 15);
+    EXPECT_EQ(pNumArrayContainsFiveNums->sumRange(0, n - 1), expectSum);
+}
+
+TEST_F(ARangeSumQueryMutable, AnswerFiveNumsUpdateVal)
+{
+    int n = fiveNums.size();
+    pNumArrayContainsFiveNums->update(1, 2);
+
+    fiveNums[1] = 2;
+    int expectSum = 0;
+    for (int x : fiveNums)
+        expectSum += x;
+
+    ASSERT_EQ(pNumArrayContainsFiveNums->sumRange(0, n - 1), expectSum);
 }
