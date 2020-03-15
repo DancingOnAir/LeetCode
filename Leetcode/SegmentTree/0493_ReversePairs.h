879194560cf62afa08d899d9c4c732658431d83a
DancingOnAir <taotong1984@hotmail.com>
2020-03-15 10:45:20 +0800

    faet(SegmentTree): accepted merge sort solution for q0493

--- Leetcode/SegmentTree/0493_ReversePairs.h
+++ Leetcode/SegmentTree/0493_ReversePairs.h
@@ -68,7 +68,32 @@ private:
     }
 
 public:
+    int sortAndCount(vector<int>::iterator begin, vector<int>::iterator end)
+    {
+        if (end - begin <= 1)
+            return 0;
+
+        auto mid = begin + ((end - begin) >> 1);
+        int count = sortAndCount(begin, mid) + sortAndCount(mid, end);
+        for (auto i = begin, j = mid; i != mid; ++i)
+        {
+            while (j != end && *i > 2L * *j)
+                ++j;
+
+            count += j - mid;
+        }
+
+        inplace_merge(begin, mid, end);
+
+        return count;
+    }
+
     int reversePairs(vector<int>& nums)
+    {
+        return sortAndCount(nums.begin(), nums.end());
+    }
+
+    int reversePairs1(vector<int>& nums)
     {
         int n = nums.size();
         if (n < 2)
