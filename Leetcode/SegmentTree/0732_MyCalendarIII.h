a26e66e8d7ce9876f3ca06f6585e5d6feb843f05
DancingOnAir <taotong1984@hotmail.com>
2020-03-21 09:33:48 +0800

    feat(SegmentTree): accepted map solution for q0732

--- Leetcode/SegmentTree/0732_MyCalendarIII.h
+++ Leetcode/SegmentTree/0732_MyCalendarIII.h
@@ -111,3 +111,27 @@ public:
         return maxBooking_;
     }
 };
+
+class MyCalendarThree
+{
+private:
+    map<int, int> count = { {-1, 0} };
+    int res = 0;
+public:
+    MyCalendarThree()
+    {
+
+    }
+
+    int book(int s, int e)
+    {
+        auto start = count.emplace(s, (--count.upper_bound(s))->second);
+        auto end = count.emplace(e, (--count.upper_bound(e))->second);
+        for (auto i = start.first; i != end.first; ++i)
+        {
+            res = max(res, ++(i->second));
+        }
+
+        return res;
+    }
+};
