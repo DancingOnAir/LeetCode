96a6bf910446c16ea2df554fc858e757ae4f4717
DancingOnAir <taotong1984@hotmail.com>
2020-03-28 12:00:01 +0800

    feat(BuyAndSellStock): accepted reversed trans time 3d dp solution for q0123

--- Leetcode/BuyAndSellStock/0123_BestTimetoBuyandSellStockIII.h
+++ Leetcode/BuyAndSellStock/0123_BestTimetoBuyandSellStockIII.h
@@ -13,6 +13,34 @@ public:
         if (n < 2)
             return 0;
 
+        int k = 2;
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(k + 1, vector<int>(2)));
+
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = k; j >= 1; --j)
+            {
+                if (i == 0)
+                {
+                    dp[i][j][0] = 0;
+                    dp[i][j][1] = -prices[i];
+                    continue;
+                }
+
+                dp[i][j][0] = max(dp[i - 1][j][0], dp[i - 1][j][1] + prices[i]);
+                dp[i][j][1] = max(dp[i - 1][j][1], dp[i - 1][j - 1][0] - prices[i]);
+            }
+        }
+
+        return dp[n - 1][k][0];
+    }
+
+    int maxProfit1(vector<int>& prices)
+    {
+        int n = prices.size();
+        if (n < 2)
+            return 0;
+
         int k = 3;
         vector<vector<vector<int>>> dp(n, vector<vector<int>>(k, vector<int>(2)));
 
