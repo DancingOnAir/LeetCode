5f46cfe7cc1d1efc49c51694bbce239c56a0a99b
DancingOnAir <taotong1984@hotmail.com>
2020-04-02 15:17:06 +0800

    feat(BitManipulation): accepted without bit operation solution for q0137

--- Leetcode/BitManipulation/0137_SingleNumberII.h
+++ Leetcode/BitManipulation/0137_SingleNumberII.h
@@ -9,7 +9,38 @@ using namespace std;
 class SingleNumberIISolution
 {
 public:
+    // without bit operation
     int singleNumber(vector<int>& nums)
+    {
+        int n = nums.size();
+        vector<int> m(32);
+        for (int i = 0; i < n; ++i)
+        {
+            int num = nums[i];
+            for (int j = 31; j >= 0; --j)
+            {
+                m[j] += num & 1;
+                num >>= 1;
+
+                if (!num)
+                    break;
+            }
+        }
+
+        int res = 0;
+        for (int i = 31; i >= 0; --i)
+        {
+            int remainder = m[i] % 3;
+            if (remainder)
+                res += 1 << (31 - i);
+        }
+
+        return res;
+    }
+
+
+    // bit manipulation
+    int singleNumber1(vector<int>& nums)
     {
         int x1 = 0, x2 = 0, mask = 0;
         for (int num : nums)
