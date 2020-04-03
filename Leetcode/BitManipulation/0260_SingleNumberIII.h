cd799b38b672b46401729c1dcb7c63e33d1a6a02
DancingOnAir <taotong1984@hotmail.com>
2020-04-03 11:15:31 +0800

    feat(BitManipulation): accepted bit manipulation solution for q0260

--- Leetcode/BitManipulation/0260_SingleNumberIII.h
+++ Leetcode/BitManipulation/0260_SingleNumberIII.h
@@ -6,15 +6,56 @@ using namespace std;
 class SingleNumberIIISolution
 {
 public:
+    unsigned int findLastBitOfOne(int num)
+    {
+        int res = 0;
+        while ((num & 1) == 0 && res < 8 * sizeof(int))
+        {
+            num >>= 1;
+            ++res;
+        }
+
+        return res;
+    }
+
+    bool isBitOne(int num, unsigned int indexBitOfOne)
+    {
+        num >>= indexBitOfOne;
+
+        return (num & 1);
+    }
+
     vector<int> singleNumber(vector<int>& nums)
     {
-        //int res = 0;
-        //for (int num : nums)
-        //    res ^= num;
+        int n = nums.size();
+        if (n < 2)
+            return vector<int>();
 
+        vector<int> res;
+        int xorRes = 0;
+        for (int num : nums)
+            xorRes ^= num;
 
-        //return vector<int>();
+        auto lastIndexOfOne = findLastBitOfOne(xorRes);
+        int num1 = 0;
+        int num2 = 0;
 
+        for (int i = 0; i < n; ++i)
+        {
+            if (isBitOne(nums[i], lastIndexOfOne))
+                num1 ^= nums[i];
+            else
+                num2 ^= nums[i];
+        }
+
+        res.emplace_back(num1);
+        res.emplace_back(num2);
+
+        return res;
+    }
+
+    vector<int> singleNumber1(vector<int>& nums)
+    {
         int n = nums.size();
         if (n < 2)
             return vector<int>();
