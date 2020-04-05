635e2b2a1a10903de43e68e03b05242396d291d0
DancingOnAir <taotong1984@hotmail.com>
2020-04-05 16:25:16 +0800

    feat(BitManipulation): accepted optimized bit operation solution for q0191

--- Leetcode/BitManipulation/0191_Numberof1Bits.h
+++ Leetcode/BitManipulation/0191_Numberof1Bits.h
@@ -4,10 +4,26 @@
 class Numberof1BitsSolution
 {
 public:
+
+    int hammingWeight(uint32_t n)
+    {
+        // eg. for 2 digits
+        // ab - ((ab >> 1) & 0x01) = ab - a = 2 * a + b - a = a + b
+        n -= (n >> 1) & 0x55555555;
+        n = (n & 0x33333333) + ((n >> 2) & 0x33333333);
+        // here, 4bits are enough space for calculating the number of 1 bits, but 2 bits are not enough;
+        // extrme cases for 2 bits: 1111 -> 0100, for storing the num of 1bits, it needs 3 bits, not enough.
+        // extrme cases for 4 bits: 1111 1111 -> 0000 1000, for storing the num of 1bits, it needs 4 bits.
+        n = (n + (n >> 4)) & 0x0f0f0f0f;
+        n += n >> 8;
+        n += n >> 16;
+
+        return n & 0xff; 
+    }
     // explation for below bit operations
     // eg. for 2 digits
     // (ab & 0x01) + ((ab >> 1) & 0x01) = b + a
-    int hammingWeight(uint32_t n)
+    int hammingWeight1(uint32_t n)
     {
         n = (n & 0x55555555) + ((n >> 1) & 0x55555555);
         n = (n & 0x33333333) + ((n >> 2) & 0x33333333);
