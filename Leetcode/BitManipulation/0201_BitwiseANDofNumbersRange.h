60feea08b03c26315a6a4ddd311614848cce90ab
DancingOnAir <taotong1984@hotmail.com>
2020-04-07 23:40:13 +0800

    feat(BitManipulation): accepted bit manipulation with __builtin_clz solution for q0201

--- Leetcode/BitManipulation/0201_BitwiseANDofNumbersRange.h
+++ Leetcode/BitManipulation/0201_BitwiseANDofNumbersRange.h
@@ -5,6 +5,11 @@ class BitwiseANDofNumbersRangeSolution
 public:
     int rangeBitwiseAnd(int m, int n)
     {
+        return ((m ^ n) == 0)? n : (n & (~(0xffffffff >> __builtin_clz(m ^ n))));
+    }
+
+    int rangeBitwiseAnd1(int m, int n)
+    {
         int res = 0;
         while (n > m)
         {
