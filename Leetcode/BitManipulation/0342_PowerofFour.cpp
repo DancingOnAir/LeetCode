9049fb2754f87a472f0d17b390754d99588795aa
DancingOnAir <taotong1984@hotmail.com>
2020-04-10 23:28:35 +0800

    feat(BitManipulation): acceptd bit manipulation solution for q0342

--- Leetcode/BitManipulation/0342_PowerofFour.cpp
+++ Leetcode/BitManipulation/0342_PowerofFour.cpp
@@ -3,7 +3,23 @@ using namespace std;
 
 class Solution {
 public:
-    bool isPowerOfFour(int num) {
+    bool isPowerOfFour(int num)
+    {
+        if (num < 1)
+            return false;
+
+        if ((num & (num - 1)) != 0)
+            return false;
+        
+        //detect digit 1, 3, 5, 7, ...
+        if (num & 0x55555555)
+            return true;
+
+        return false;
+    }
+
+    bool isPowerOfFour1(int num)
+    {
         if (num <= 0)
             return false;
 
