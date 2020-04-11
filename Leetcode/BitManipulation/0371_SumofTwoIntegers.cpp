69ca50380e2f002ae296e08f3bd82e4d1338f95a
DancingOnAir <taotong1984@hotmail.com>
2020-04-11 17:26:17 +0800

    feat(BitManipulation): init q0371 & acceptd fixed evil left shift issue solution for q0371

---
+++ Leetcode/BitManipulation/0371_SumofTwoIntegers.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    // 0^0 = 1^1 = 0, 0^1 = 1^0 = 1
+    // 0&0 = 0&1 = 1&0 = 0, 1&1 = 1
+    int getSum(int a, int b) {
+        
+        int carry = a & b;
+        int res = a ^ b;
+
+        while (carry)
+        {
+            int temp = static_cast<unsigned int>(carry) << 1;
+            carry = temp & res;
+
+            res ^= temp;
+        }
+
+        return res;
+    }
+};
+
+void testGetSum() {
+    Solution solution;
+    cout << solution.getSum(1, 2) << endl;
+    cout << solution.getSum(-2, 3) << endl;
+    cout << solution.getSum(-1, 1) << endl;
+}
+
+int main() {
+    testGetSum();
+
+    return 0;
+}
+
