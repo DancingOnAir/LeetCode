9b5ae073f73c7ac079497faa2b478831325caa15
DancingOnAir <taotong1984@hotmail.com>
2020-04-13 23:56:55 +0800

    feat(BitManipulation): init q0397 & created unit cases for q0397

--- Leetcode/BitManipulation/0393_UTF-8Validation.cpp
+++ Leetcode/BitManipulation/0393_UTF-8Validation.cpp
@@ -4,6 +4,28 @@ using namespace std;
 
 class Solution {
 public:
+    bool validUtf8(vector<int>& data) {
+        int count  = 0;
+        for (int num : data) {
+            if (count == 0) {
+                if ((num >> 5) == 0b110)
+                    count = 1;
+                else if ((num >> 4) == 0b1110)
+                    count = 2;
+                else if ((num >> 3) == 0b11110)
+                    count = 3;
+                else if ((num >> 7))
+                    return false;
+            }else {
+                if ((num >> 6) != 0b10)
+                    return false;
+                --count;
+            }
+        }
+
+        return count == 0;
+    }
+
     int getNumofFirstOnes(int num) {
         for (int i = 7; i >= 0; --i) {
             if ((num & (1 << i)) == 0)
@@ -13,7 +35,7 @@ public:
         return 8;
     }
 
-    bool validUtf8(vector<int>& data) {
+    bool validUtf81(vector<int>& data) {
         if (data.empty())
             return true;
 
---
+++ Leetcode/BitManipulation/0397_IntegerReplacement.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int integerReplacement(int n) {
+        return 0;
+    }
+};
+
+void testIntegerReplacement() {
+    Solution solution;
+
+    cout << solution.integerReplacement(8) << endl;
+    cout << solution.integerReplacement(7) << endl;
+}
+
+int main() {
+    testIntegerReplacement();
+    
+    return 0;
+}
