8e910069f0276174ce0f396eabd68dd4069dbdf0
DancingOnAir <taotong1984@hotmail.com>
2020-04-16 10:53:35 +0800

    feat(BitManipulation): accepted bit manipulation solution for q0401

--- Leetcode/BitManipulation/0401_BinaryWatch.cpp
+++ Leetcode/BitManipulation/0401_BinaryWatch.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
 #include <vector>
 #include <string>
-
+#include <bitset>
 using namespace std;
 
 class Solution {
 public:
     vector<string> readBinaryWatch(int num) {
-        return vector<string> {};
+        vector<string> res;
+        for (int i = 0; i < 12; ++i) {
+            for (int j = 0; j < 60; ++j) {
+                if (bitset<32>(i).count() + bitset<32>(j).count() == num) {
+                    res.emplace_back(to_string(i) + ":" + (j < 10 ? "0" : "") + to_string(j));
+                }
+            }
+        }
+
+        return res;
     }
 };
 
