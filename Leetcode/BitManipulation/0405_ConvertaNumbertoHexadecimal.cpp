c17823f2eabf679ea10e935e29f189d716132177
DancingOnAir <taotong1984@hotmail.com>
2020-04-17 23:59:38 +0800

    feat(BitManipulation): init q0405 & accepted solution for q0405

---
+++ Leetcode/BitManipulation/0405_ConvertaNumbertoHexadecimal.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+    
+public:
+    string toHex(int num) {
+        string res;
+
+        const vector<char> hexes = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+        int mask = 0b1111;
+
+        for (int i = 0; i < sizeof(num) * 2 && num; ++i) {
+            res = hexes[num & mask] + res;
+            num >>= 4;
+        }
+
+        return res;
+    }
+};
+
+void testToHex() {
+    Solution solution;
+
+    cout << solution.toHex(26) << endl;
+    cout << solution.toHex(-1) << endl;
+}
+
+int main() {
+    testToHex();
+
+    return 0;
+}
