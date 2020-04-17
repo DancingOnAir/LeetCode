69be7c258c8ebb0d9f0fe1c9eb0d753991f3602a
DancingOnAir <taotong1984@hotmail.com>
2020-04-18 00:19:52 +0800

    fix(BitManipulation): added corner case for q0405

--- Leetcode/BitManipulation/0405_ConvertaNumbertoHexadecimal.cpp
+++ Leetcode/BitManipulation/0405_ConvertaNumbertoHexadecimal.cpp
@@ -7,6 +7,9 @@ class Solution {
     
 public:
     string toHex(int num) {
+        if (!num)
+            return "0";
+
         string res;
 
         const vector<char> hexes = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
