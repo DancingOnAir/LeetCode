3111a9430808a342291888da9e30b4d17699095d
DancingOnAir <taotong1984@hotmail.com>
2020-04-21 00:00:36 +0800

    feat(BitManipulation): init q0476 & created testing cases for q0476

---
+++ Leetcode/BitManipulation/0476_NumberComplement.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int findComplement(int num) {
+        return 0;
+    }
+};
+
+void testFindComplement() {
+    Solution solution;
+
+    cout << solution.findComplement(5) << endl;
+    cout << solution.findComplement(1) << endl;
+}
+
+int main() {
+    testFindComplement();
+    return 0;
+}
