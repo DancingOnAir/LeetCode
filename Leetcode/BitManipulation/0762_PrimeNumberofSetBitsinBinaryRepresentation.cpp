492fee561a11ccaf0921a8407fedfae6da31528f
DancingOnAir <taotong1984@hotmail.com>
2020-04-24 21:08:54 +0800

    feat(BitManipulation): accepted bit manipulation solution for q0762

--- Leetcode/BitManipulation/0762_PrimeNumberofSetBitsinBinaryRepresentation.cpp
+++ Leetcode/BitManipulation/0762_PrimeNumberofSetBitsinBinaryRepresentation.cpp
@@ -6,6 +6,15 @@ using namespace std;
 // for prime numbers between 0-32, contain: 2, 3, 5, 7, 11, 13, 17, 19, 23, 29
 class Solution {
 public:
+    int countPrimeSetBits(int L, int R) {
+        int res = 0;
+        while (L <= R) {
+            res += (0b100000100010100010100010101100 >> __builtin_popcount(L++)) & 1;
+        }
+
+        return res;
+    }
+
     int hammingWeight(int num) {
         num = (num & 0x55555555) + ((num >> 1) & 0x55555555);
         num = (num & 0x33333333) + ((num >> 2) & 0x33333333);
@@ -16,7 +25,7 @@ public:
         return num;
     }
 
-    int countPrimeSetBits(int L, int R) {
+    int countPrimeSetBits1(int L, int R) {
         unordered_set<int> primes {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
 
         int res = 0;
