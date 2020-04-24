671aa354de573f687e0309993cc61125f492a9cc
DancingOnAir <taotong1984@hotmail.com>
2020-04-24 19:10:55 +0800

    feat(BitManipulation): init q0762 & accepted bit manipulation solution for q0762

---
+++ Leetcode/BitManipulation/0762_PrimeNumberofSetBitsinBinaryRepresentation.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <unordered_set>
+
+using namespace std;
+
+// for prime numbers between 0-32, contain: 2, 3, 5, 7, 11, 13, 17, 19, 23, 29
+class Solution {
+public:
+    int hammingWeight(int num) {
+        num = (num & 0x55555555) + ((num >> 1) & 0x55555555);
+        num = (num & 0x33333333) + ((num >> 2) & 0x33333333);
+        num = (num & 0x0f0f0f0f) + ((num >> 4) & 0x0f0f0f0f);
+        num = (num & 0x00ff00ff) + ((num >> 8) & 0x00ff00ff);
+        num = (num & 0xffff) + ((num >> 16) & 0xffff);
+
+        return num;
+    }
+
+    int countPrimeSetBits(int L, int R) {
+        unordered_set<int> primes {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
+
+        int res = 0;
+        for (int i = L; i <= R; ++i) {
+            if (primes.count(hammingWeight(i)))
+                ++res;
+        }
+
+        return res;
+    }
+};
+
+void testCountPrimeSetBits() {
+    Solution solution;
+    cout << solution.countPrimeSetBits(6, 10) << endl;
+    cout << solution.countPrimeSetBits(10, 15) << endl;
+}
+
+int main() {
+    testCountPrimeSetBits();
+
+    return 0;
+}
