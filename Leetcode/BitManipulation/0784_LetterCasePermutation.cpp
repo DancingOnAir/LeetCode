3d61951d8fd09de9a796a5c74e30d320ef9ce6bc
DancingOnAir <taotong1984@hotmail.com>
2020-04-27 18:54:27 +0800

    feat(BitManipulation): accepted backtrack & bit manipulation solution for q0784

--- Leetcode/BitManipulation/0784_LetterCasePermutation.cpp
+++ Leetcode/BitManipulation/0784_LetterCasePermutation.cpp
@@ -6,7 +6,28 @@ using namespace std;
 
 class Solution {
 public:
+    void backtrack(string& s, int i, vector<string>& res) {
+        if (i == s.size()) {
+            res.emplace_back(s);
+            return;
+        }
+
+        backtrack(s, i + 1, res);
+        if (isalpha(s[i])) {
+            s[i] ^= 1 << 5;
+
+            backtrack(s, i + 1, res);
+            s[i] ^= 1 << 5;
+        }
+    }
+
     vector<string> letterCasePermutation(string S) {
+        vector<string> res;
+        backtrack(S, 0, res);
+        return res;
+    }
+
+    vector<string> letterCasePermutation1(string S) {
         if (S.empty())
             return vector<string>();
 
