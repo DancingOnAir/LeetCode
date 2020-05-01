7cb5669ac7a8d507dbc5a5e9ff8f9dc8f3a64db9
DancingOnAir <taotong1984@hotmail.com>
2020-05-01 14:17:26 +0800

    feat(BitManipulation):  accepted multiset solution for q0870

--- Leetcode/Array/0870_AdvantageShuffle.cpp
+++ Leetcode/Array/0870_AdvantageShuffle.cpp
@@ -1,11 +1,27 @@
 #include <vector>
 #include <iostream>
+#include <set>
 
 using namespace std;
 
 class Solution {
 public:
     vector<int> advantageCount(vector<int>& A, vector<int>& B) {
+        multiset<int> s(A.begin(), A.end());
+
+        for (int i = 0; i < B.size(); ++i) {
+            auto p = s.upper_bound(B[i]);
+            if (p == s.end())
+                p = s.begin();
+
+            A[i] = *p;
+            s.erase(p);
+        }
+
+        return A;
+    }
+
+    vector<int> advantageCount1(vector<int>& A, vector<int>& B) {
         if (A.size() != B.size())
             return vector<int>();
 
