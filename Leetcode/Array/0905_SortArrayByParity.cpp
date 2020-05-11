f290f72534f444368364d3b3a0968d906aff9db8
DancingOnAir <taotong1984@hotmail.com>
2020-05-11 23:43:03 +0800

    feat(Array): optimized solution by quick sort partition for q0905

--- Leetcode/Array/0905_SortArrayByParity.cpp
+++ Leetcode/Array/0905_SortArrayByParity.cpp
@@ -10,21 +10,21 @@ public:
         if (n < 2)
             return A;
 
-        int i = 0, j = n - 1;
-        while (i < j) {
-            while (i < n && (A[i] & 1) == 0)
-                ++i;
-            
-            if (i >= n || i >= j)
-                return A;
+        int i = -1, j = n;
+        while (true) {
+            while ((A[++i] & 1) == 0) {
+                if (i == n - 1)
+                    break;
+            }
 
-            while (j >= 0 && (A[j] & 1))
-                --j;
+            while (A[--j] & 1) {
+                if (j == 0)
+                    break;
+            }
 
-            if (j < 0 || i >= j)
-                return A;
-
-            swap(A[i++], A[j--]);
+            if (i >= j)
+                break;
+            swap(A[i], A[j]);
         }
 
         return A;
@@ -42,13 +42,13 @@ void printNums(const vector<int>& nums) {
 void testSortArrayByParity() {
     Solution solution;
 
-    // vector<int> A1 {3, 1, 2, 4};
-    // auto res1 = solution.sortArrayByParity(A1);
-    // printNums(res1);
+    vector<int> A1 {3, 1, 2, 4};
+    auto res1 = solution.sortArrayByParity(A1);
+    printNums(res1);
 
-    // vector<int> A2 {0, 2};
-    // auto res2 = solution.sortArrayByParity(A2);
-    // printNums(res2);
+    vector<int> A2 {0, 2};
+    auto res2 = solution.sortArrayByParity(A2);
+    printNums(res2);
 
     vector<int> A3 {0, 1};
     auto res3 = solution.sortArrayByParity(A3);
