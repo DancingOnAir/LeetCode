d4093e47b9ce27315bc543c291f9bb66580fc803
DancingOnAir <taotong1984@hotmail.com>
2020-05-13 11:28:24 +0800

    feat(Array): accepted montonous stack solution for q0907

--- Leetcode/Array/0907_SumofSubarrayMinimums.cpp
+++ Leetcode/Array/0907_SumofSubarrayMinimums.cpp
@@ -1,10 +1,49 @@
 #include <iostream>
 #include <vector>
+#include <stack>
 using namespace std;
 
 class Solution {
 public:
     int sumSubarrayMins(vector<int>& A) {
+        if (A.empty())
+            return 0;
+
+        int n = A.size();
+        vector<int> left(n), right(n);
+        stack<pair<int, int>> prevStk, nextStk;
+
+        for (int i = 0; i < n; ++i) {
+            left[i] = i + 1;
+            right[i] = n - i;
+        }
+
+        for (int i = 0; i < n; ++i) {
+            // for previous less
+            while (!prevStk.empty() && prevStk.top().first > A[i]) {
+                prevStk.pop();
+            }
+            left[i] = prevStk.empty() ? i + 1 : i - prevStk.top().second;
+            prevStk.emplace(make_pair(A[i], i));
+
+            while (!nextStk.empty() && nextStk.top().first > A[i]) {
+                auto x = nextStk.top();
+                nextStk.pop();
+                right[x.second] = i - x.second;
+            }
+            nextStk.emplace(make_pair(A[i], i));
+        }
+
+        int res = 0, mod = 1e9 + 7;
+        for (int i = 0; i < n; ++i) {
+            res = (res + A[i] * left[i] * right[i]) % mod;
+        }
+
+        return res;
+    }
+
+    // brute force
+    int sumSubarrayMins1(vector<int>& A) {
         if (A.empty())
             return 0;
 
@@ -32,8 +71,12 @@ public:
 
 void testSumSubarrayMins() {
     Solution solution;
+
     vector<int> A1 {3, 1, 2, 4};
     cout << solution.sumSubarrayMins(A1) << endl;
+
+    vector<int> A2 {71, 55, 82, 55};
+    cout << solution.sumSubarrayMins(A2) << endl;
 }
 
 int main() {
