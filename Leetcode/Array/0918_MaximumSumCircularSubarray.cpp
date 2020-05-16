70ca167955a1a4e77d84821a02fe1364370a20bd
DancingOnAir <taotong1984@hotmail.com>
2020-05-16 21:33:26 +0800

    feat(Array): init q0918 & presum brute force soution for q0918 but TLE

---
+++ Leetcode/Array/0918_MaximumSumCircularSubarray.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    // presum solution, but TLE
+    int maxSubarraySumCircular(vector<int>& A) {
+        int n = A.size();
+        if (n == 0)
+            return 0;
+        else if (n == 1)
+            return A[0];
+
+        int res = INT_MIN;
+        int sum = 0;
+
+        A.insert(A.end(), A.begin(), prev(A.end()));
+        vector<int> presum;
+        for (int i = 0; i < 2*n - 1; ++i) {
+            sum += A[i];
+            presum.emplace_back(sum);
+        }
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i == 0)
+                    res = max(res, presum[i + j]);
+                else
+                    res = max(res, presum[i + j] - presum[i - 1]);
+            }
+        }
+
+        return res;
+    }
+};
+
+void testMaxSubarraySumCircular() {
+    Solution solution;
+
+    vector<int> A1 {1, -2, 3, -2};
+    cout << solution.maxSubarraySumCircular(A1) << endl;
+
+    vector<int> A2 {5, -3, 5};
+    cout << solution.maxSubarraySumCircular(A2) << endl;
+
+    vector<int> A3 {3, -1, 2, -1};
+    cout << solution.maxSubarraySumCircular(A3) << endl;
+
+    vector<int> A4 {3,-2,2,-3};
+    cout << solution.maxSubarraySumCircular(A4) << endl;
+
+    vector<int> A5 {-2,-3,-1};
+    cout << solution.maxSubarraySumCircular(A5) << endl;
+
+    vector<int> A6 {3, 1, 3, 2, 6};
+    cout << solution.maxSubarraySumCircular(A6) << endl;
+}
+
+int main() {
+    testMaxSubarraySumCircular();
+
+    return 0;
+}
