029ba11507ea7cb72b311db5a6aa103fbce10d34
DancingOnAir <taotong1984@hotmail.com>
2020-05-17 13:33:11 +0800

    feat(Array): accepted deque & presum solution for q0918

--- Leetcode/Array/0918_MaximumSumCircularSubarray.cpp
+++ Leetcode/Array/0918_MaximumSumCircularSubarray.cpp
@@ -1,11 +1,38 @@
 #include <iostream>
 #include <vector>
-
+#include <deque>
 using namespace std;
 
 class Solution {
 public:
     int maxSubarraySumCircular(vector<int>& A) {
+        int n = A.size();
+        vector<int> presum(2 * n + 1);
+        for (int i = 0; i < 2 * n; ++i) {
+            presum[i + 1] = presum[i] + A[i % n];
+        }
+
+        int res = A[0];
+        deque<int> dq;
+        dq.emplace_back(0);
+
+        for (int i = 1; i <= 2 * n; ++i) {
+            // right - left <= n -> left >= right -n
+            if (i - n > dq.front())
+                dq.pop_front();
+
+            res = max(res, presum[i] - presum[dq.front()]);
+            while (!dq.empty() && presum[i] <= presum[dq.back()]) {
+                dq.pop_back();
+            }
+
+            dq.emplace_back(i);
+        }
+
+        return res;
+    }
+
+    int maxSubarraySumCircular1(vector<int>& A) {
         int sum = 0, maxSum = A[0], curMax = 0, minSum = A[0], curMin = 0;
         for (int i : A) {
             curMax = i + max(curMax, 0);
@@ -21,7 +48,7 @@ public:
     }
 
     // presum solution, but TLE
-    int maxSubarraySumCircular1(vector<int>& A) {
+    int maxSubarraySumCircular2(vector<int>& A) {
         int n = A.size();
         if (n == 0)
             return 0;
