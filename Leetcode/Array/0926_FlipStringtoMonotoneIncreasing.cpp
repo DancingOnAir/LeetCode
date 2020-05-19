c605faa2e83d0439ae6bae568963486d78f7e762
DancingOnAir <taotong1984@hotmail.com>
2020-05-19 21:35:35 +0800

    feat(Array): init q0926 & accepted solution for q0926

---
+++ Leetcode/Array/0926_FlipStringtoMonotoneIncreasing.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <numeric>
+using namespace std;
+
+class Solution {
+public:
+    int minFlipsMonoIncr(string S) {
+        int n = S.size();
+        if (n < 2)
+            return 0;
+        
+        int numOfZero = 0;
+        for (char c : S) {
+            if (c == '0')
+                ++numOfZero;
+        }
+
+        int res = INT_MAX;
+        int l = 0, r = numOfZero;
+        for (char c : S) {
+            res = min(res, l + r);
+
+            if (c == '1')
+                ++l;
+            else
+                --r;
+        }
+
+        return min(res, min(numOfZero, n - numOfZero));
+    }
+};
+
+void testMinFlipsMonoIncr() {
+    Solution solution;
+
+    string S1 = "00110";
+    cout << solution.minFlipsMonoIncr(S1) << endl;
+
+    string S2 = "010110";
+    cout << solution.minFlipsMonoIncr(S2) << endl;
+
+    string S3 = "00011000";
+    cout << solution.minFlipsMonoIncr(S3) << endl;
+    
+    string S4 = "0101100011";
+    cout << solution.minFlipsMonoIncr(S4) << endl;
+}
+
+int main() {
+    testMinFlipsMonoIncr();
+
+    return 0;
+}
