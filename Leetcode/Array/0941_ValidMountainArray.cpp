086880685d85427c8b03c163169f697c2626947a
DancingOnAir <taotong1984@hotmail.com>
2020-05-21 08:27:30 +0800

    feat(Array): init q0941 & accepted solution for q0941

---
+++ Leetcode/Array/0941_ValidMountainArray.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    bool validMountainArray(vector<int>& A) {
+        int n = A.size();
+        if (n < 3)
+            return false;
+
+        int i = 1;
+        while (i < n && A[i - 1] < A[i]) {
+            ++i;
+        }
+
+        if (i == n || i == 1 || A[i - 1] == A[i])
+            return false;
+        
+        while (i < n && A[i - 1] > A[i]) {
+            ++i;
+        }
+        
+        if (i == n)
+            return true;
+
+        return false;
+    }
+};
+
+void testValidMountainArray() {
+    Solution solution;
+
+    // vector<int> A1 {2, 1};
+    // cout << (solution.validMountainArray(A1) ? "True" : "False") << endl;
+    // vector<int> A2 {3, 5, 5};
+    // cout << (solution.validMountainArray(A2) ? "True" : "False") << endl;
+    // vector<int> A3 {0, 3, 2, 1};
+    // cout << (solution.validMountainArray(A3) ? "True" : "False") << endl;
+    // vector<int> A4 {1, 3, 2};
+    // cout << (solution.validMountainArray(A4) ? "True" : "False") << endl;
+    vector<int> A5 {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    cout << (solution.validMountainArray(A5) ? "True" : "False") << endl;
+}
+
+int main() {
+    testValidMountainArray();
+
+    return 0;
+}
