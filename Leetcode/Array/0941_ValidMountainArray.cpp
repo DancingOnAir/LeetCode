744f45b619558c69a5c149f38c6b702cc0dc0f3e
DancingOnAir <taotong1984@hotmail.com>
2020-05-21 08:41:57 +0800

    feat(Array): accepted two pointers solution for q0941

--- Leetcode/Array/0941_ValidMountainArray.cpp
+++ Leetcode/Array/0941_ValidMountainArray.cpp
@@ -7,6 +7,20 @@ class Solution {
 public:
     bool validMountainArray(vector<int>& A) {
         int n = A.size();
+        int i = 0, j = n - 1;
+        while (i < n - 1 && A[i] < A[i + 1]) {
+            ++i;
+        }
+        
+        while (j > 0 && A[j - 1] > A[j]) {
+            --j;
+        }
+
+        return (i > 0 && i == j && j < n - 1);
+    }
+
+    bool validMountainArray1(vector<int>& A) {
+        int n = A.size();
         if (n < 3 || A[0] >= A[1])
             return false;
 
