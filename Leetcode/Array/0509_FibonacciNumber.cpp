3f1e75eab7fdb62b2977547cbf2ecbf475845804
DancingOnAir <taotong1984@hotmail.com>
2020-05-30 11:25:58 +0800

    feat(Array): accepted 100% solution for q0509

--- Leetcode/Array/0509_FibonacciNumber.cpp
+++ Leetcode/Array/0509_FibonacciNumber.cpp
@@ -6,6 +6,20 @@ using namespace std;
 class Solution {
 public:
     int fib(int N) {
+        if (N < 2)
+            return N;
+        
+        int a = 0, b = 1, res = 0;
+        for (int i = 2; i <= N; ++i) {
+            res = a + b;
+            a = b;
+            b = res;
+        }
+
+        return res;
+    }
+
+    int fib1(int N) {
         if (N < 2) {
             return N;
         }
