1f059c3f03c73eb452312b6d0d6cb19f6bc7c09c
DancingOnAir <taotong1984@hotmail.com>
2020-06-01 07:56:51 +0800

    feat(Array): init q0977 & accepted solution for q0977

---
+++ Leetcode/Array/0977_SquaresofaSortedArray.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& A) {
+        if (A[0] < 0) {
+            sort(A.begin(), A.end(), [](int a, int b) { return abs(a) < abs(b); });
+        }
+        
+        vector<int> res;
+        for (int a : A) {
+            res.emplace_back(a * a);
+        }
+
+        return res;
+    }
+};
+
+void printRes(const vector<int>& nums) {
+    for (int x : nums)
+        cout << x << ", ";
+    cout << endl;
+}
+
+void testSortedSquares() {
+    Solution solution;
+
+    vector<int> A1 { -4, -1, 0, 3, 10 };
+    auto res1 = solution.sortedSquares(A1);
+    printRes(res1);
+
+    vector<int> A2 { -7, -3, 2, 3, 11 };
+    auto res2 = solution.sortedSquares(A2);
+    printRes(res2);
+}
+
+int main() {
+    testSortedSquares();
+
+    return 0;
+}
