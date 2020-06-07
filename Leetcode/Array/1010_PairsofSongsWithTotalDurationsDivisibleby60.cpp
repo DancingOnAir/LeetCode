c45fdde39c7ee40aa63bc0f311d1ad99382ffcba
DancingOnAir <taotong1984@hotmail.com>
2020-06-07 15:55:57 +0800

    feat(Array): init q1010 & acceptd solution for q1010

---
+++ Leetcode/Array/1010_PairsofSongsWithTotalDurationsDivisibleby60.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    int numPairsDivisibleBy60(vector<int>& time) {
+        int n = time.size();
+        if (n < 2)
+            return 0;
+        
+        int res = 0;
+        unordered_map<int, int> m;
+        for (int i = 0; i < n; ++i) {
+            if (m.count((60 - time[i] % 60) % 60)) {
+                res += m[(60 - time[i] % 60) % 60];
+            }
+
+            m[time[i] % 60]++;
+        }
+
+        return res;
+    }
+};
+
+void testNumPairsDivisibleBy60() {
+    Solution solution;
+    vector<int> time1 {30, 20, 150, 100, 40};
+    cout << solution.numPairsDivisibleBy60(time1) << endl;
+
+    vector<int> time2 {60, 60, 60};
+    cout << solution.numPairsDivisibleBy60(time2) << endl;
+}
+
+int main() {
+    testNumPairsDivisibleBy60();
+
+    return 0;
+}
