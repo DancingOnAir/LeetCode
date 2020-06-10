e4f397d64e75ef91bc5fa5115cc8f6abce6caaab
DancingOnAir <taotong1984@hotmail.com>
2020-06-10 11:16:40 +0800

    feat(Array): init q1014 & accepted solution for q1014

---
+++ Leetcode/Array/1014_BestSightseeingPair.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    // dp[i] = max(dp[i - 1], dp[i - 1])
+    // A[i] + A[j] + i - j, A[k]
+    int maxScoreSightseeingPair(vector<int>& A) {
+        auto n = A.size();
+
+        int maxVal = max(A[0], A[1] + 1);
+        int res = A[0] + A[1] - 1;
+        for (auto i = 2; i < n; ++i) {
+            res = max(res, maxVal + A[i] - i);
+            maxVal = max(maxVal, A[i] + i);
+        }
+
+        return res;
+    }
+};
+
+void testMaxScoreSightseeingPair() {
+    Solution solution;
+    vector<int> A1 {8, 1, 5, 2, 6};
+    cout << solution.maxScoreSightseeingPair(A1) << endl;
+}
+
+int main() {
+    testMaxScoreSightseeingPair();
+
+    return 0;
+}
