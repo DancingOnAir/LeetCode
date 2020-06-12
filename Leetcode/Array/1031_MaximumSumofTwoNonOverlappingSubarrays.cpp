d4b81b13282d785b280e36c4db4b17e4434d015e
DancingOnAir <taotong1984@hotmail.com>
2020-06-12 22:42:22 +0800

    feat(Array): init q1031 & accepted presum solution for q1031

---
+++ Leetcode/Array/1031_MaximumSumofTwoNonOverlappingSubarrays.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxSumTwoNoOverlap(vector<int>& A, int L, int M) {
+        auto n = A.size();
+
+        vector<int> presum{0};
+        for (auto a : A) {
+            presum.emplace_back(presum.back() + a);
+        }
+        
+        int res = INT_MIN;
+        for (int i = L; i <= n; ++i) {
+            int tempMaxM = INT_MIN;
+            for (int j = M; j <= i - L; ++j) {
+                tempMaxM = max(tempMaxM, presum[j] - presum[j - M]);
+            }
+
+            for (int j = i + M; j <=n; ++j) {
+                tempMaxM = max(tempMaxM, presum[j] - presum[j - M]);
+            }
+
+            res = max(res, presum[i] - presum[i - L] + tempMaxM);
+        }
+
+        return res;
+    }
+};
+
+void testMaxSumTwoNoOverlap() {
+    Solution solution;
+
+    vector<int> A1 {0,6,5,2,2,5,1,9,4};
+    int L1 = 1, M1 = 2;
+    cout << solution.maxSumTwoNoOverlap(A1, L1, M1) << endl;
+
+    vector<int> A2 {3,8,1,3,2,1,8,9,0};
+    int L2 = 3, M2 = 2;
+    cout << solution.maxSumTwoNoOverlap(A2, L2, M2) << endl;
+
+    vector<int> A3 {2,1,5,6,0,9,5,0,3,8};
+    int L3 = 4, M3 = 3;
+    cout << solution.maxSumTwoNoOverlap(A3, L3, M3) << endl;
+}
+
+int main() {
+    testMaxSumTwoNoOverlap();
+
+    return 0;
+}
