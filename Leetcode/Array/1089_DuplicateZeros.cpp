600e75eb0bde35584795594b351e06a76ef74940
DancingOnAir <taotong1984@hotmail.com>
2020-06-19 08:44:11 +0800

    feat(Array): accepted solution for q1089

--- Leetcode/Array/1089_DuplicateZeros.cpp
+++ Leetcode/Array/1089_DuplicateZeros.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
 #include <vector>
-#include <list>
+#include <algorithm>
 using namespace std;
 
 class Solution {
 public:
     void duplicateZeros(vector<int>& arr) {
+        int n = arr.size();
+        int j = n + count(arr.begin(), arr.end(), 0);
+
+        for (int i = n - 1; i >= 0; --i) {
+            if (--j < n) {
+                arr[j] = arr[i];
+            }
+
+            if (arr[i] == 0 && --j < n) {
+                arr[j] = 0;
+            }
+        }
+    }
+
+    void duplicateZeros1(vector<int>& arr) {
         int n = arr.size();
         if (n < 2)
             return;
