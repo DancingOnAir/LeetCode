0e3d16d7f4adf9d1b53d58167e897a3aa29b0a94
DancingOnAir <taotong1984@hotmail.com>
2020-07-12 11:26:05 +0800

    feat(Array): init q1217 & accepted solution for q1217

---
+++ Leetcode/Array/1217_PlaywithChips.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+class Solution {
+public:
+    int minCostToMoveChips(vector<int>& chips) {
+        map<int, int> m;
+
+        for (int chip : chips) {
+            m[chip]++;
+        }
+
+        int minPos = -1, sum = 0;
+        for (auto& iter : m) {
+            if (minPos == -1) {
+                minPos = iter.first;
+            }
+
+            if (((iter.first - minPos) & 1) == 0) {
+                sum += iter.second;
+            }
+        }
+
+        return min(sum, (int)chips.size() - sum);
+    }
+};
+
+void testMinCostToMoveChips() {
+    Solution solution;
+
+    vector<int> chips1 = {1, 2, 3};
+    cout << solution.minCostToMoveChips(chips1) << endl;
+
+    vector<int> chips2 = {2, 2, 2, 3, 3};
+    cout << solution.minCostToMoveChips(chips2) << endl;
+
+    vector<int> chips3 = {1, 2, 2, 2, 2};
+    cout << solution.minCostToMoveChips(chips3) << endl;
+
+}
+
+int main() {
+    testMinCostToMoveChips();
+
+    return 0;
+}
