1b0bcd786782a52403f452e790a2cb7ee8f4d27f
DancingOnAir <taotong1984@hotmail.com>
2020-07-17 16:02:44 +0800

    feat(Array): init q1267 & accetped solution for q1267

---
+++ Leetcode/Array/1267_CountServersthatCommunicate.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <numeric>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    int countServers(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        unordered_map<int, vector<int>> counts;
+        int res = 0;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j]) {
+                    counts[j].emplace_back(i);
+                    ++res;
+                }
+            }
+        }
+
+        for (auto& iter : counts) {
+            if (iter.second.size() == 1) {
+                vector<int> tempRow = grid[iter.second[0]];
+                if (count(tempRow.begin(), tempRow.end(), 1) == 1) {
+                    --res;
+                } 
+            }
+        }
+        return res;
+    }
+};
+
+void testCountServers() {
+    Solution solution;
+
+    vector<vector<int>> grid1 = {{1, 0},{0, 1}};
+    cout << solution.countServers(grid1) << endl;
+
+    vector<vector<int>> grid2 = {{1, 0},{1, 1}};
+    cout << solution.countServers(grid2) << endl;
+
+    vector<vector<int>> grid3 = {{1, 1, 0, 0},{0, 0, 1, 0},{0, 0, 1, 0},{0, 0, 0, 1}};
+    cout << solution.countServers(grid3) << endl;
+
+    vector<vector<int>> grid4 = {{0, 0, 0, 0, 0, 0, 0, 0},
+                                {0, 0, 0, 0, 0, 0, 0, 0},
+                                {0, 0, 0, 0, 0, 0, 0, 0},
+                                {0, 0, 0, 0, 0, 0, 1, 0},
+                                {0, 0, 1, 0, 0, 0, 0, 0},
+                                {0, 0, 1, 0, 0, 1, 0, 0}};
+    cout << solution.countServers(grid4) << endl;
+}
+
+int main() {
+    testCountServers();
+
+    return 0;
+}
