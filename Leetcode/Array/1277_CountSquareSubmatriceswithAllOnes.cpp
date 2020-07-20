72273cf4e3577469534e7fbfddfa320419f5f148
DancingOnAir <taotong1984@hotmail.com>
2020-07-20 15:49:43 +0800

    feat(Array): accepted dp solution for q1277

--- Leetcode/Array/1277_CountSquareSubmatriceswithAllOnes.cpp
+++ Leetcode/Array/1277_CountSquareSubmatriceswithAllOnes.cpp
@@ -6,6 +6,19 @@ using namespace std;
 class Solution {
 public:
     int countSquares(vector<vector<int>>& matrix) {
+        int res = 0;
+        for (int i = 0; i < matrix.size(); ++i) {
+            for (int j = 0; j < matrix[0].size(); res += matrix[i][j++]) {
+                if (i && j && matrix[i][j]) {
+                    matrix[i][j] = min(matrix[i - 1][j - 1], min(matrix[i - 1][j], matrix[i][j - 1])) + 1;
+                }
+            }
+        }
+
+        return res;
+    }
+
+    int countSquares1(vector<vector<int>>& matrix) {
 
         int m = matrix.size(), n = matrix[0].size();
         int res = 0;
@@ -49,11 +62,6 @@ void testCountSquares() {
                                 {1,1,0},
                                 {1,1,0}};
     cout << solution.countSquares(matrix2) << endl;
-
-    // vector<vector<int>> matrix3 ={{0,1,1,1},
-    //                             {1,1,1,1},
-    //                             {0,1,1,1}};
-    // cout << solution.countSquares(matrix3) << endl;
 }
 
 int main() {
