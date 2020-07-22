00fb5b328b091c3920a7da68371f0a97d730aadb
DancingOnAir <taotong1984@hotmail.com>
2020-07-22 10:14:06 +0800

    feat(Array): init q1295 & accepted solution for q1295

---
+++ Leetcode/Array/1295_FindNumberswithEvenNumberofDigits.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+class Solution {
+public:
+    int getDigit(int num) {
+        int res = 0;
+        while (num) {
+            num /= 10;
+            ++res;
+        }
+
+        return res;
+    }
+
+    int findNumbers(vector<int>& nums) {
+        int res = 0;
+        for (int num : nums) {
+            if ((getDigit(num) & 1) == 0)
+                ++res;
+        }
+
+        return res;
+    }
+};
+
+void testFindNumbers() {
+    Solution solution;
+
+    vector<int> nums1 {12, 345, 2, 6, 7896};
+    cout << solution.findNumbers(nums1) << endl;
+
+    vector<int> nums2 {555, 901, 482, 1771};
+    cout << solution.findNumbers(nums2) << endl;
+}
+
+int main() {
+    testFindNumbers();
+
+    return 0;
+}
