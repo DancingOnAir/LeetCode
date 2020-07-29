1a287c2f90dd973bbf7f2f7d7abd6e3454d8bf6d
DancingOnAir <taotong1984@hotmail.com>
2020-07-29 08:24:49 +0800

    feat(Array): init q1346 & accepted hash table solution for q1346

---
+++ Leetcode/Array/1346_CheckIfNandItsDoubleExist.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        unordered_set<int> s;
+        for (int a : arr) {
+            if ((a & 1) == 0 && s.count(a / 2)) {
+                return true;
+            }
+
+            if (s.count(a * 2))
+                return true;
+
+            s.emplace(a);
+        }
+
+        return false;
+    }
+};
+
+void testCheckIfExist() {
+    Solution solution;
+
+    vector<int> arr1 {10, 2, 5, 3};
+    cout << (solution.checkIfExist(arr1) ? "True" : "False") << endl;
+
+    vector<int> arr2 {7, 1, 14, 11};
+    cout << (solution.checkIfExist(arr2) ? "True" : "False") << endl;
+
+    vector<int> arr3 {3, 1, 7, 11};
+    cout << (solution.checkIfExist(arr3) ? "True" : "False") << endl;
+}
+
+int main() {
+    testCheckIfExist();
+
+    return 0;
+}
