3a007ecce360e3d30804a862b309b7f4a8e7560c
DancingOnAir <taotong1984@hotmail.com>
2021-04-29 13:30:40 +0800

    feat(String): init q1451 & accepted solution for q1451

---
+++ Leetcode/String/1451_RearrangeWordsinaSentence.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+using std::stringstream;
+using std::stable_sort;
+
+class Solution {
+public:
+    string arrangeWords(string text) {
+        text[0] += ('a' - 'A');
+
+        vector<string> words;
+        stringstream ss(text);
+        string word;
+
+        while (ss >> word) {
+            words.emplace_back(word);
+        }
+
+        stable_sort(words.begin(), words.end(), [](const string& s1, const string& s2) -> bool { return s1.size() < s2.size(); });
+        string res = "";
+        for (auto& w : words) {
+            res += w + " ";
+        }
+        res.pop_back();
+        res[0] -= ('a' - 'A');
+
+        return res;
+    }
+};
+
+void testArrangeWords() {
+    Solution solution;
+
+    assert(solution.arrangeWords("Leetcode is cool") == "Is cool leetcode");
+    assert(solution.arrangeWords("Keep calm and code on") == "On and keep calm code");
+    assert(solution.arrangeWords("To be or not to be") == "To be or to be not");
+}
+
+
+int main() {
+    testArrangeWords();
+
+    return 0;
+}
