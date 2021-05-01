707b00c10c2d16212cc2b8036641d8a0b3c416d2
DancingOnAir <taotong1984@hotmail.com>
2021-05-01 13:33:25 +0800

    feat(String): accepted sliding window solution for q1461

--- Leetcode/String/1461_CheckIfaStringContainsAllBinaryCodesofSizeK.cpp
+++ Leetcode/String/1461_CheckIfaStringContainsAllBinaryCodesofSizeK.cpp
@@ -1,11 +1,28 @@
 #include <unordered_set>
 #include <string>
+#include <deque>
 
 using namespace std;
 
 class Solution {
 public:
+    // sliding window
     bool hasAllCodes(string s, int k) {
+        unordered_set<string> seen;
+        deque<char> q;
+
+        for (char c : s) {
+            q.push_back(c);
+            if (q.size() == k) {
+                seen.insert({q.begin(), q.end()});
+                q.pop_front();
+            }
+        }
+
+        return seen.size() == pow(2, k);
+    }
+
+    bool hasAllCodes1(string s, int k) {
         unordered_set<string> seen;
         for (int i = 0; i + k <= s.size(); ++i) {
             seen.insert(s.substr(i, k));
@@ -17,10 +34,10 @@ public:
 void testHasAllCodes() {
     Solution solution;
 
-    assert(solution.hasAllCodes("00110110", 2));
-    assert(solution.hasAllCodes("00110", 2));
-    assert(solution.hasAllCodes("0110", 1));
-    assert(!solution.hasAllCodes("0110", 2));
+    // assert(solution.hasAllCodes("00110110", 2));
+    // assert(solution.hasAllCodes("00110", 2));
+    // assert(solution.hasAllCodes("0110", 1));
+    // assert(!solution.hasAllCodes("0110", 2));
     assert(!solution.hasAllCodes("0000000001011100", 4));
 }
 
