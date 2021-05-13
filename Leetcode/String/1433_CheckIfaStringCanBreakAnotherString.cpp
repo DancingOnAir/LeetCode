182cb0effb3e7b8205a3e503d3b253a0919f798b
DancingOnAir <taotong1984@hotmail.com>
2021-05-13 23:30:49 +0800

    feat(String): accepted count solution for q1433

--- Leetcode/String/1433_CheckIfaStringCanBreakAnotherString.cpp
+++ Leetcode/String/1433_CheckIfaStringCanBreakAnotherString.cpp
@@ -1,11 +1,36 @@
 #include <string>
+#include <vector>
 
 using namespace std;
 
 class Solution {
 public:
-    // sort and compare: O(nlogn)
+    // use vector to count characters
     bool checkIfCanBreak(string s1, string s2) {
+        vector<int> v(26);
+        for (int i = 0; i < s1.size(); ++i) {
+            ++v[s1[i] - 'a'];
+            --v[s2[i] - 'a'];
+        }
+
+        bool flag1 = true, flag2 = true;
+        int total = 0;
+        for (int val : v) {
+            total += val;
+            if (total < 0)
+                flag1 = false;
+            if (total > 0)
+                flag2 = false;
+
+            if (!flag1 && !flag2)
+                return false;
+        }
+
+        return true;
+    }
+
+    // sort and compare: O(nlogn)
+    bool checkIfCanBreak1(string s1, string s2) {
         sort(s1.begin(), s1.end());
         sort(s2.begin(), s2.end());
 
