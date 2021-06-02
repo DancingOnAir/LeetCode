3c76fee350e2bcdabc90c1c8e92e964862a60785
DancingOnAir <taotong1984@hotmail.com>
2021-06-02 09:50:51 +0800

    feat(String): accepted sliding window cpp solution for q1876

---
+++ Leetcode/String/1876_SubstringsofSizeThreewithDistinctCharacters.cpp
@@ -0,0 +1,39 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+class Solution {
+public:
+    int countGoodSubstrings(string s) {
+        vector<int> cnt(26);
+        int res = 0;
+        int repeat = 0;
+        for (auto i = 0; i < s.size(); ++i) {
+            if (cnt[s[i] - 'a']++ == 1)
+                ++repeat;
+            
+            if (i >= 3) {
+                if (cnt[s[i - 3] - 'a']-- == 2)
+                    --repeat;
+            }
+
+            if (i >= 2 && repeat == 0)
+                ++res;
+        }
+
+        return res;
+    }
+};
+
+void testCountGoodSubstrings() {
+    Solution solution;
+
+    assert(solution.countGoodSubstrings("xyzzaz") == 1);
+    assert(solution.countGoodSubstrings("aababcabc") == 4);
+}
+
+int main() {
+    testCountGoodSubstrings();
+
+    return 0;
+}
