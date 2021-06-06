d3fa0b68403a62ad0f684b403d1a77ffe3c99078
DancingOnAir <taotong1984@hotmail.com>
2021-06-06 10:29:52 +0800

    feat(String): init q1108 & acceptd string replace cpp solution for q1108

---
+++ Leetcode/String/1108_DefanginganIPAddress.cpp
@@ -0,0 +1,32 @@
+#include <string>
+
+using namespace std;
+class Solution {
+private:
+    void replaceString(const string& target, const string& rep, string& text) {
+        auto i = 0;
+        while ((i = text.find(target, i)) != string::npos) {
+            text.replace(i, target.size(), rep);
+            i += rep.size();
+        }
+    }
+
+public:
+    string defangIPaddr(string address) {
+        replaceString(".", "[.]", address);
+        return address;
+    }
+};
+
+void testDefangIPAddr() {
+    Solution solution;
+
+    assert(solution.defangIPaddr("1.1.1.1") == "1[.]1[.]1[.]1");
+    assert(solution.defangIPaddr("255.100.50.0") == "255[.]100[.]50[.]0");
+}
+
+int main() {
+    testDefangIPAddr();
+
+    return 0;
+}
