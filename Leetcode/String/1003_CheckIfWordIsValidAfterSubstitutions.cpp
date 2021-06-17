bc383777d218c995231183a5a6e422ec304e3748
DancingOnAir <taotong1984@hotmail.com>
2021-06-17 11:55:36 +0800

    feat(String): accepted two pointers cpp solution for q1003

--- Leetcode/String/1003_CheckIfWordIsValidAfterSubstitutions.cpp
+++ Leetcode/String/1003_CheckIfWordIsValidAfterSubstitutions.cpp
@@ -4,7 +4,21 @@
 using namespace std;
 class Solution {
 public:
+    // two pointers solution
     bool isValid(string s) {
+        int j = 0;
+        for (int i = 0; i < s.size(); ++i) {
+            if (s[i] == 'c' && j > 1 && s[j - 1] == 'b' && s[j - 2] == 'a')
+                j -= 2;
+            else
+                s[j++] = s[i];
+        }
+
+        return j == 0;
+    }
+
+    // stack solution
+    bool isValid1(string s) {
         stack<char> stk;
         for (char& c : s) {
             if (c == 'c') {
