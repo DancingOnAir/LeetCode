d29c3d93a935ecfd9f9ab3c9355a493f61b1a7dd
DancingOnAir <taotong1984@hotmail.com>
2021-06-19 13:59:20 +0800

    feat(String): accepted stable sort cpp solution for q937

--- Leetcode/String/0937_ReorderDatainLogFiles.cpp
+++ Leetcode/String/0937_ReorderDatainLogFiles.cpp
@@ -2,9 +2,31 @@
 #include <string>
 
 using namespace std;
+bool myCompare(string a, string b) {
+    int i = a.find(' ');
+    int j = b.find(' ');
+
+    if (!isdigit(a[i + 1]) && !isdigit(b[j + 1])) {
+        if (a.substr(i + 1) == b.substr(j + 1))
+            return a < b;
+        return a.substr(i + 1) < b.substr(j + 1);
+    }
+    else if (!isdigit(a[i + 1])) {
+        return true;
+    }
+
+    return false;
+}
+
 class Solution {
 public:
+    
     vector<string> reorderLogFiles(vector<string>& logs) {
+        stable_sort(logs.begin(), logs.end(), myCompare);
+        return logs;
+    }
+
+    vector<string> reorderLogFiles1(vector<string>& logs) {
         vector<string> res;
         vector<string> digits;
         vector<pair<string, string>> letters;
