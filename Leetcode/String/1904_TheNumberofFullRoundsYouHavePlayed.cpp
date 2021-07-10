a3203e4a179e379feff9633d334881524f5a1340
DancingOnAir <taotong1984@hotmail.com>
2021-07-10 19:59:35 +0800

    feat(String): accepted 100 faster cpp solution for q1904

--- Leetcode/String/1904_TheNumberofFullRoundsYouHavePlayed.cpp
+++ Leetcode/String/1904_TheNumberofFullRoundsYouHavePlayed.cpp
@@ -3,7 +3,16 @@
 using namespace std;
 class Solution {
 public:
-    int numberOfRounds(string startTime, string finishTime) {
+
+    int numberOfRounds(string s, string f) {
+        int ts = 60 * stoi(s.substr(0, 2)) + stoi(s.substr(3));
+        int tf = 60 * stoi(f.substr(0, 2)) + stoi(f.substr(3));
+        if (ts > tf)
+            tf += 60 * 24;
+        return max(0, tf / 15 - (ts + 14) / 15);
+    }
+
+    int numberOfRounds1(string startTime, string finishTime) {
         int ts = 60 * stoi(startTime.substr(0, 2)) + stoi(startTime.substr(3));
         int tf = 60 * stoi(finishTime.substr(0, 2)) + stoi(finishTime.substr(3));
 
