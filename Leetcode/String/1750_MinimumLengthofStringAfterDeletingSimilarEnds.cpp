da05cefbc26c3a835bb2c70c19cf8a1bc2ee0943
DancingOnAir <taotong1984@hotmail.com>
2021-07-15 11:10:51 +0800

    feat(String): init q1750 & accepted two pointers cpp solution for q1750

---
+++ Leetcode/String/1750_MinimumLengthofStringAfterDeletingSimilarEnds.cpp
@@ -0,0 +1,32 @@
+#include <string>
+
+using namespace std;
+class Solution {
+public:
+    int minimumLength(string s) {
+        int lo = 0, hi = s.size() - 1;
+        while (lo < hi && s[lo] == s[hi]) {
+            char c = s[lo];
+            while (lo <= hi && c == s[lo])
+                ++lo;
+            while (lo <= hi && c == s[hi])
+                --hi;
+        }
+
+        return hi - lo + 1;
+    }
+};
+
+void testMinimumLength() {
+    Solution solution;
+
+    assert(solution.minimumLength("ca") == 2);
+    assert(solution.minimumLength("cabaabac") == 0);
+    assert(solution.minimumLength("aabccabba") == 3);
+}
+
+int main() {
+    testMinimumLength();
+
+    return 0;
+}
