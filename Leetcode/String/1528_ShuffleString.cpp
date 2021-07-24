ef44b5684e0d4653d069a90fa5bc70039e649c9d
DancingOnAir <taotong1984@hotmail.com>
2021-07-24 09:45:09 +0800

    feat(String): init q1528 & accepted cyclic sort cpp solution for q1528

---
+++ Leetcode/String/1528_ShuffleString.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+        for (auto i = 0; i < indices.size(); ++i) {
+            while (indices[i] != i) {
+                swap(s[i], s[indices[i]]);
+                swap(indices[i], indices[indices[i]]);
+            }
+        }
+
+        return s;
+    }
+};
+
+void testRestoreString() {
+    Solution solution;
+
+    vector<int> indices1{4,5,6,7,0,2,1,3};
+    assert(solution.restoreString("codeleet", indices1) == "leetcode");
+
+    vector<int> indices2{4,0,2,6,7,3,1,5};
+    assert(solution.restoreString("aaiougrt", indices2) == "arigatou");
+}
+
+int main() {
+    testRestoreString();
+
+    return 0;
+}
