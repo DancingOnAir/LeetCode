03e447d8911a0dad5ea56dcfb64ecaaeac2a17d5
DancingOnAir <taotong1984@hotmail.com>
2021-08-01 22:50:23 +0800

    feat(String): init q0942 & accepted greedy cpp solution for q0942

---
+++ Leetcode/String/0942_DIStringMatch.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+class Solution {
+public:
+    vector<int> diStringMatch(string s) {
+        vector<int> res;
+        for (int lo = 0, hi = s.size(), i = 0; i <= s.size(); ++i) {
+            res.push_back(i == s.size() || s[i] == 'I' ? lo++ : hi--);
+        }
+        return res;
+    }
+};
+
+void testDiStringMatch() {
+    Solution solution;
+
+    vector<int> res1 {0,4,1,3,2};
+    assert(solution.diStringMatch("IDID") == res1);
+
+    vector<int> res2 {0,1,2,3};
+    assert(solution.diStringMatch("III") == res2);
+
+    vector<int> res3 {3,2,0,1};
+    assert(solution.diStringMatch("DDI") == res3);
+}
+
+int main() {
+    testDiStringMatch();
+
+    return 0;
+}
