ecf8947d503014d1b024b8b6082975ca071c8514
DancingOnAir <taotong1984@hotmail.com>
2021-08-07 23:09:02 +0800

    feat(String): accepted single-interval scheduling maximization shorter cpp solution for q1520

--- Leetcode/String/1520_MaximumNumberofNonOverlappingSubstrings.cpp
+++ Leetcode/String/1520_MaximumNumberofNonOverlappingSubstrings.cpp
@@ -6,6 +6,36 @@ using namespace std;
 class Solution {
 public:
     vector<string> maxNumOfSubstrings(string s) {
+        int n = s.size();
+        vector<int> left(26, INT_MAX), right(26, INT_MIN);
+
+        for (int i = 0; i < n; ++i) {
+            left[s[i] - 'a'] = min(left[s[i] - 'a'], i);
+            right[s[i] - 'a'] = i;
+        }
+
+        vector<string> res;
+        for (int i = 0, pre = -1; i < n; ++i) {
+            if (i == right[s[i] - 'a']) {
+                int l = left[s[i] - 'a'];
+                int r = right[s[i] - 'a'];
+
+                for (int j = r - 1; j > l && l > pre && r == i; --j) {
+                    l = min(l, left[s[j] - 'a']);
+                    r = max(r, right[s[j] - 'a']);
+                }
+
+                if (l > pre && r == i) {
+                    res.push_back(s.substr(l, r - l + 1));
+                    pre = r;
+                }
+            }
+        }
+
+        return res;
+    }
+
+    vector<string> maxNumOfSubstrings1(string s) {
         vector<int> left(26, INT_MAX), right(26, INT_MIN), skip(26);
 
         for (int i = 0; i < s.size(); ++i) {
