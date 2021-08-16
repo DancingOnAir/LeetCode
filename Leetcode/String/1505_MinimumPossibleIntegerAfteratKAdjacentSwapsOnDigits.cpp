ceacf4d16b924b9ccb655ead262122fb82b249bd
DancingOnAir <taotong1984@hotmail.com>
2021-08-16 11:16:40 +0800

    feat(String): init q1505 & treid a dfs /usr/bin/env /bin/sh /var/folders/p1/lc1wsk_x0s988hpnrf8k4mpc0000gn/T/Microsoft-MIEngine-Cmd-ok5tbx1m.dp5  solution for q1505

---
+++ Leetcode/String/1505_MinimumPossibleIntegerAfteratKAdjacentSwapsOnDigits.cpp
@@ -0,0 +1,41 @@
+#include <string>
+#include <algorithm>
+
+using namespace std;
+class Solution {
+public:
+    string minInteger(string num, int k) {
+        if (k <= 0)
+            return num;
+
+        int n = num.size();
+        if (k >= n * (n - 1) / 2) {
+            sort(num.begin(), num.end());
+            return num;
+        }
+
+        for(int i = 0; i < 10; ++i){
+			int idx = num.find(to_string(i));
+			if(idx >= 0 && idx <= k)
+				return num[idx] + minInteger(num.substr(0, idx) + num.substr(idx + 1), k - idx);
+		}
+
+        return num;
+    }
+};
+
+void testMinInteger() {
+    Solution solution;
+
+    assert(solution.minInteger("4321", 4) == "1342");
+    assert(solution.minInteger("100", 1) == "010");
+    assert(solution.minInteger("36789", 1000) == "36789");
+    assert(solution.minInteger("22", 22) == "22");
+    assert(solution.minInteger("9438957234785635408", 23) == "0345989723478563548");
+}
+
+int main() {
+    testMinInteger();
+
+    return 0;
+}
