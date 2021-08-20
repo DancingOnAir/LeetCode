91f53b549a1b5b8e7916fc3327e88273e9dee83b
DancingOnAir <taotong1984@hotmail.com>
2021-08-20 15:01:02 +0800

    feat(String): init q1754 & accepted greedy cpp solution for q1754

--- Leetcode/String/1754_LargestMergeOfTwoStrings.cpp
+++ Leetcode/String/1754_LargestMergeOfTwoStrings.cpp
@@ -5,6 +5,21 @@ class Solution {
 public:
     string largestMerge(string word1, string word2) {
         string res;
+        int i = 0, j = 0;
+        while (i < word1.size() && j < word2.size()) {
+            if (word1.compare(i, word1.size() - i, word2, j, word2.size() - j) > 0) {
+                res += word1[i++];
+            }
+            else {
+                res += word2[j++];
+            }
+        }
+
+        return res + word1.substr(i) + word2.substr(j);
+    }
+
+    string largestMerge1(string word1, string word2) {
+        string res;
 
         while (!word1.empty() && !word2.empty()) {
             if (word1 >= word2) {
