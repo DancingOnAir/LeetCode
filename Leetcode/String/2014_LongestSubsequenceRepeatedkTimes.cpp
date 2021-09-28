ccdeefc7e532f88ee8a38a77b81f5cd979bd46e5
DancingOnAir <taotong1984@hotmail.com>
2021-09-28 23:35:55 +0800

    feat(String): init q2014 & accepted map+next_permutation cpp solution for q2014

--- Leetcode/String/2014_LongestSubsequenceRepeatedkTimes.cpp
+++ Leetcode/String/2014_LongestSubsequenceRepeatedkTimes.cpp
@@ -1,4 +1,6 @@
 #include <string>
+#include <map>
+#include <algorithm>
 
 using namespace std;
 class Solution {
@@ -39,10 +41,41 @@ public:
         }
     }
 
-    string longestSubsequenceRepeatedK(string s, int k) {
+    string longestSubsequenceRepeatedK1(string s, int k) {
         longestSubsequence = "";
         dfs("", s, k);
         return longestSubsequence;
+    }
+
+
+    string longestSubsequenceRepeatedK(string s, int k) {
+        map<char, int> freq;
+        for (char& c : s) {
+            ++freq[c];
+        }
+
+        string cand;
+        for (auto& it: freq) {
+            for (int i = 0; i < it.second / k; ++i) {
+                cand += it.first;
+            }
+        }
+
+        string res;
+        for (int i = cand.size(); i > 0; --i) {
+            string temp(cand);
+            do {
+                string t = temp.substr(0, i);
+                if (isRepeatTimesEqualOrGreatThanK(t, s, k)) {
+                    res = max(res, t);
+                }
+            }
+            while (next_permutation(temp.begin(), temp.end()));
+            
+            if (!res.empty())
+                return res;
+        }
+        return res;
     }
 };
 
