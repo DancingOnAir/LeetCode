75888254d37ef0e9a2f82a4187798562d1c7f5ef
DancingOnAir <taotong1984@hotmail.com>
2023-01-13 17:03:39 +0800

    feat(BitManipulation): init q2429 & accepted bit manipulation cpp solution for q2429

---
+++ Leetcode/BitManipulation/2429_MinimizeXOR.cpp
@@ -0,0 +1,35 @@
+#include <assert.h>
+
+class Solution {
+public:
+    int minimizeXor(int num1, int num2) {
+        int a = __builtin_popcount(num1);
+        int b = __builtin_popcount(num2);
+        int res = num1;
+
+        for (int i = 0; i < 32; ++i) {
+            if (a > b && ((1 << i) & num1) > 0) {
+                res ^= (1 << i);
+                --a;
+            }
+
+            if (a < b && ((1 << i) & num1) == 0) {
+                res ^= (1 << i);
+                ++a;
+            }
+        }
+
+        return res;
+    }
+};
+
+void testMinimiszeXor() {
+    Solution solution;
+    assert(solution.minimizeXor(3, 5) == 3);
+    assert(solution.minimizeXor(1, 12) == 3);
+}
+
+int main() {
+    testMinimiszeXor();
+    return 0;
+}
