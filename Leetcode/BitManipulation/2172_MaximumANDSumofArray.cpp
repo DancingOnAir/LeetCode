b43dfbbf5681dd8fc2dd8419244027537031bda2
DancingOnAir <taotong1984@hotmail.com>
2023-01-19 17:17:19 +0800

    feat(BitManipulation): init q2172 & accepted base-3 bit manipulation cpp solution for q2172

--- Leetcode/BitManipulation/2172_MaximumANDSumofArray.cpp
+++ Leetcode/BitManipulation/2172_MaximumANDSumofArray.cpp
@@ -1,11 +1,36 @@
 #include <vector>
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 class Solution {
 public:
+    // base - 3
+    int dp(int i, int mask, int numSlots, vector<int>& nums, vector<int>& memo) {
+        if (memo[mask] > 0)
+            return memo[mask];
+
+        if (i < 0)
+            return 0;
+
+        for (int slot = 1, bit = 1; slot <= numSlots; ++slot, bit *= 3) {
+            // int b = pow(3, (slot - 1));
+            if (mask / bit % 3 > 0) {
+                memo[mask] = max(memo[mask], (slot & nums[i]) + dp(i - 1, mask - bit, numSlots, nums, memo));
+            }
+        }
+
+        return memo[mask];
+    }
 
     int maximumANDSum(vector<int>& nums, int numSlots) {
+        int mask = pow(3, numSlots) - 1;
+        vector<int> memo(mask);
+        return dp(nums.size() - 1, mask, numSlots, nums, memo);
+    }
+
+    // base - 2
+    int maximumANDSum1(vector<int>& nums, int numSlots) {
         vector<int> f((1 << (numSlots * 2)));
         int res = 0;
         for (int i = 0; i < f.size(); ++i) {
