9819458cbd5c44e65fbafe2981ac53f3f80f2316
DancingOnAir <taotong1984@hotmail.com>
2023-01-27 09:05:04 +0800

    feat(BitManipulation): accepted count cpp solution for q2546

--- Leetcode/BitManipulation/2546_ApplyBitwiseOperationstoMakeStringsEqual.cpp
+++ Leetcode/BitManipulation/2546_ApplyBitwiseOperationstoMakeStringsEqual.cpp
@@ -5,6 +5,10 @@ using namespace std;
 class Solution {
 public:
     bool makeStringsEqual(string s, string target) {
+        return (count(s.begin(), s.end(), '1') > 0) == (count(target.begin(), target.end(), '1') > 0);
+    }
+
+    bool makeStringsEqual1(string s, string target) {
         return (s.find('1') != string::npos) == (target.find('1') != string::npos);
     }
 };
