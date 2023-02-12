582316e9c0934dfd36ace3f87fb667c15c2ed4ec
DancingOnAir <taotong1984@hotmail.com>
2023-02-12 21:31:04 +0800

    feat(BinarySearch): accepted binary search cpp solution for q2554

--- Leetcode/BinarySearch/2554_MaximumNumberofIntegerstoChooseFromaRangeI.cpp
+++ Leetcode/BinarySearch/2554_MaximumNumberofIntegerstoChooseFromaRangeI.cpp
@@ -1,12 +1,47 @@
 #include <iostream>
 #include <vector>
 #include <unordered_set>
+#include <numeric>
+#include <algorithm>
 
 using namespace std;
 class Solution {
 public:
-    // hash set
+    // https://labuladong.github.io/algo/di-yi-zhan-da78c/shou-ba-sh-48c1d/wo-xie-le--9c7a4/
+    // 寻找右边边界，找到刚好超出maxSum的数，再减1得到值
     int maxCount(vector<int>& banned, int n, int maxSum) {
+        vector<int> ps(100001);
+        for (int x : banned) {
+            ps[x] = x;
+        }
+        partial_sum(ps.begin(), ps.end(), ps.begin());
+
+        int left = 1, right = n;
+        int cnt = 0;
+        while (left <= right) {
+            int mid = left + ((right - left) >> 1);
+            int cur  = (1 + mid) * mid / 2 - ps[mid];
+            if (cur > maxSum) {
+                right = mid - 1;
+            }
+            else if (cur < maxSum) {
+                left = mid + 1;
+            }
+            else if (cur == maxSum) {
+                left = mid + 1;
+            }
+        }
+
+        --left;
+        for (int i = 0; i < left; ++i) {
+            cnt += ps[i] != ps[i + 1];
+        }
+
+        return left - cnt;
+    }
+
+    // hash set
+    int maxCount1(vector<int>& banned, int n, int maxSum) {
         unordered_set<int> st(banned.begin(), banned.end());
         int res = 0;
         for (int i = 1; i <= n && maxSum >= 0; ++i) {
