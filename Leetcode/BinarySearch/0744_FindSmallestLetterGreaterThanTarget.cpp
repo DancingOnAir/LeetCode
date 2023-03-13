fad83760cc72206faddacb3b4664377387416524
DancingOnAir <taotong1984@hotmail.com>
2023-03-14 01:02:27 +0800

    feat(BinarySearch): init q0744 & accepted binary search cpp solution for q0744

---
+++ Leetcode/BinarySearch/0744_FindSmallestLetterGreaterThanTarget.cpp
@@ -0,0 +1,37 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        int left = 0, right = letters.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (letters[mid] <= target) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+        
+        if (left < letters.size())
+            return letters[left];
+        return letters[0];
+    }
+};
+
+void testNextGreatestLetter() {
+    Solution solution;
+    vector<char> letters1 {'c', 'f', 'j'};
+    cout << solution.nextGreatestLetter(letters1, 'a') << endl;
+    cout << solution.nextGreatestLetter(letters1, 'c') << endl;
+    vector<char> letters2 {'x', 'x', 'y', 'y'};
+    cout << solution.nextGreatestLetter(letters2, 'z') << endl;
+}
+
+int main() {
+    testNextGreatestLetter();
+    return 0;
+}
