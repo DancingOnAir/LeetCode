e4065861cc4af637a7be80858b1b7c590fb46515
DancingOnAir <taotong1984@hotmail.com>
2023-04-05 21:15:17 +0500

    feat(Array): init q2562 & accepted log cpp solution for q2562

--- Leetcode/Array/2562_FindtheArrayConcatenationValue.cpp
+++ Leetcode/Array/2562_FindtheArrayConcatenationValue.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <cmath>
 
 using namespace std;
 class Solution {
 public:
     long long findTheArrayConcVal(vector<int>& nums) {
+        long long res = 0;
+        for (int i = 0, j = nums.size() - 1; i <= j; ++i, --j) {
+            if (i < j) {
+                res += nums[i] * pow(10, (int)log10(nums[j]) + 1) + nums[j];
+            }
+            else {
+                res += nums[i];
+            }
+        }
+        return res;
+    }
+
+    long long findTheArrayConcVal1(vector<int>& nums) {
         long long res = 0;
         for (int i = 0, j = nums.size() - 1; i <= j; ++i, --j) {
             if (i < j) {
