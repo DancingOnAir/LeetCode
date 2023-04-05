7e1fd5dab4a7e61f586f0c0bccc87a7d6c5e5d3a
DancingOnAir <taotong1984@hotmail.com>
2023-04-05 21:11:52 +0500

    feat(Array): init q2562 & accepted cpp solution for q2562

---
+++ Leetcode/Array/2562_FindtheArrayConcatenationValue.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+class Solution {
+public:
+    long long findTheArrayConcVal(vector<int>& nums) {
+        long long res = 0;
+        for (int i = 0, j = nums.size() - 1; i <= j; ++i, --j) {
+            if (i < j) {
+                res += stoll(to_string(nums[i]) + to_string(nums[j]));
+            }
+            else {
+                res += nums[i];
+            }
+        }
+        return res;
+    }
+};
+
+void testFindTheArrayConcVal() {
+    Solution solution;
+    vector<int> nums1 {7, 52, 2, 4};
+    cout << solution.findTheArrayConcVal(nums1) << endl;
+    vector<int> nums2 {5, 14, 13, 8, 12};
+    cout << solution.findTheArrayConcVal(nums2) << endl;
+}
+
+int main() {
+    testFindTheArrayConcVal();
+    return 0;
+}
