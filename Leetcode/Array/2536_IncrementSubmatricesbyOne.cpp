64ec148eec00756e9eedd9c17fb2458210d6750e
DancingOnAir <taotong1984@hotmail.com>
2023-04-26 10:20:33 +0800

    feat(Array):init q2536 & accepted 2d range caching cpp solution for q2536

--- Leetcode/Array/2536_IncrementSubmatricesbyOne.cpp
+++ Leetcode/Array/2536_IncrementSubmatricesbyOne.cpp
@@ -7,6 +7,39 @@ public:
     vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& queries) {
         vector<vector<int>> res(n, vector<int>(n));
         for (auto& q : queries) {
+            ++res[q[0]][q[1]];
+            if (q[2] + 1 < n) {
+                --res[q[2] + 1][q[1]];
+            }
+
+            if (q[3] + 1 < n) {
+                --res[q[0]][q[3] + 1];
+            }
+
+            if ((q[2] + 1 < n) && (q[3] + 1 < n)) {
+                ++res[q[2] + 1][q[3] + 1];
+            }
+        }
+        // sweep line by row
+        for (int r = 1; r < n; ++r) {
+            for (int c = 0; c < n; ++c) {
+                res[r][c] += res[r - 1][c];
+            }
+        }
+        // sweep line by col
+        for (int r = 0; r < n; ++r) {
+            for (int c = 1; c < n; ++c) {
+                res[r][c] += res[r][c - 1];
+            }
+        }
+        return res;
+    }
+
+    // # https://leetcode.com/problems/increment-submatrices-by-one/solutions/3052675/python3-sweep-line-range-addition-w-visualization-clean-concise/
+    // # Time complexity: O(n2+nq) ≈ O(nq) if q≫n; 这里q是queries的长度
+    vector<vector<int>> rangeAddQueries1(int n, vector<vector<int>>& queries) {
+        vector<vector<int>> res(n, vector<int>(n));
+        for (auto& q : queries) {
             for (int r = q[0]; r < q[2] + 1; ++r) {
                 ++res[r][q[1]];
                 if (q[3] + 1 < n) {
